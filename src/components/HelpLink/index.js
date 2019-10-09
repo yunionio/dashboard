00c22819cb3d41395cac1aa91b91152cbfe7d62d
houjiazong <houjiazong@gmail.com>
2019-10-09 19:30:18 +0800

    Add HelpLink

---
+++ src/components/HelpLink/index.js
@@ -0,0 +1,25 @@
+export default {
+  name: 'HelpLink',
+  functional: true,
+  props: {
+    blank: {
+      type: Boolean,
+      default: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+  render (h, ctx) {
+    const { blank, href } = ctx.props
+    const target = blank ? '_blank' : '_self'
+    const slots = ctx.slots()
+    return (
+      <a href={ href } target={ target }>
+        { slots.default ? h('span', slots.default) : null }
+        { blank ? <icon class='ml-1' name='blank' /> : null }
+      </a>
+    )
+  },
+}
