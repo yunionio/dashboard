77bec06614480da7828cce06b45f8dce59488e69
wangpeng <wangpeng@yunionyun.com>
2019-10-10 16:17:54 +0800

    全局报错添加请求信息

--- src/sections/DialogManager/components/Error.vue
+++ src/sections/DialogManager/components/Error.vue
@@ -2,16 +2,22 @@
   <base-dialog @cancel="cancelDialog">
     <div slot="header">错误信息</div>
     <div class="error-body" slot="body">
-      <div class="d-flex mb-3">
+      <div class="d-flex pb-2">
         <div class="label">错误消息: </div>
         <div class="error-color ml-3">
           <div>{{ params.error.class }}</div>
           <div>{{ params.error.detail }}</div>
         </div>
       </div>
-      <div class="d-flex">
+      <div class="d-flex pb-2 mt-2 border-bottom">
         <div class="label">错误源信息: </div>
         <pre class="error-color ml-3">{{ details }}</pre>
+      </div>
+      <div class="d-flex mt-3">
+        <div class="label">请求: </div>
+        <div class="ml-3">
+          <pre>{{ request }}</pre>
+        </div>
       </div>
     </div>
     <div slot="footer">
@@ -29,8 +35,10 @@ export default {
   mixins: [DialogMixin, WindowsMixin],
   data () {
     const details = JSON.stringify(this.params.error.resource, null, 2)
+    const request = JSON.stringify(this.params.request, null, 2)
     return {
       details,
+      request,
     }
   },
 }
