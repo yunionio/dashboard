f5e9249772454e105a9f56a2986f04cb12ed018c
wangpeng <wangpeng@yunionyun.com>
2019-10-12 14:31:05 +0800

    添加list-cell-body-wrap组件支持复制和编辑,添加可用区和区域

---
+++ components/ListBodyCellWrap/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div v-on="events">
+    <span v-if="!hideField" :class="{ 'text-weak': field === 'description' }">{{ row[field] || '-' }}</span>
+    <slot />
+    <div class="d-flex">
+      <edit
+        slot="edit"
+        v-if="showEdit"
+        @update="update"
+        :label="labelCn"
+        :input-rules="inputRules"
+        :visible.sync="editVisible"
+        :defaultValue="row[field]" />
+      <copy
+        slot="copy"
+        class="ml-1"
+        v-if="showCopy"
+        :message="row[field]" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListBodyCellWrap',
+  props: {
+    alwaysShowCopyBtn: {
+      type: Boolean,
+      default: false,
+    },
+    alwaysShowEditBtn: {
+      type: Boolean,
+      default: false,
+    },
+    row: { // 当前行数据
+      type: Object,
+      required: true,
+    },
+    field: {
+      type: String,
+      default: 'name',
+    },
+    label: {
+      type: String,
+    },
+    steadyStatus: {
+      type: Array,
+    },
+    list: {
+      type: Object,
+    },
+    copy: {
+      type: Boolean,
+      default: false,
+    },
+    edit: {
+      type: Boolean,
+      default: false,
+    },
+    hideField: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data () {
+    return {
+      showBtn: false,
+      editVisible: false, // edit form 的显隐
+    }
+  },
+  computed: {
+    labelCn () {
+      if (this.label) return this.label
+      const fieldMap = {
+        name: '名称',
+        description: '备注',
+      }
+      if (fieldMap[this.field]) {
+        return fieldMap[this.field]
+      }
+      return ''
+    },
+    showCopy () {
+      if (this.alwaysShowCopyBtn) return true
+      if (this.copy && this.showBtn) return true
+      return false
+    },
+    showEdit () {
+      if (this.alwaysShowEditBtn) return true
+      if (this.editVisible) return true
+      if (this.edit && this.showBtn) return true
+      return false
+    },
+    inputRules () {
+      if (this.field === 'description') return []
+      return null
+    },
+  },
+  created () {
+    this.events = {}
+    this.events.mouseenter = this.handleMouseenter
+    this.events.mouseleave = this.handleMouseleave
+  },
+  methods: {
+    update (formData) {
+      const steadyStatus = this.steadyStatus || this.list.steadyStatus
+      this.list && this.list.singleUpdate(this.row.id, { [this.field]: formData.input }, steadyStatus)
+    },
+    handleMouseenter (e) {
+      e.stopPropagation()
+      e.preventDefault()
+      if (!this.showBtn) this.showBtn = true
+    },
+    handleMouseleave (e) {
+      e.stopPropagation()
+      e.preventDefault()
+      if (this.showBtn) this.showBtn = false
+    },
+  },
+}
+</script>
