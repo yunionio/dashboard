a292a15144a49be8555c986e8ba1ab5f42d6681c
wangpeng <wangpeng@yunionyun.com>
2019-10-16 17:39:19 +0800

    调度标签、调度策略、动态调度标签 done

--- router/index.js
+++ router/index.js
@@ -3,6 +3,9 @@ import Cloudaccount from '@Cloudenv/views/cloudaccount'
 import CloudaccountCreate from '@Cloudenv/views/cloudaccount/create'
 import Cloudregion from '@Cloudenv/views/cloudregion'
 import Zone from '@Cloudenv/views/zone'
+import Schedtag from '@Cloudenv/views/schedtag'
+import Schedpolicy from '@Cloudenv/views/schedpolicy'
+import Dynamicschedtag from '@Cloudenv/views/dynamicschedtag'
 
 export default {
   index: 7,
@@ -68,6 +71,55 @@ export default {
               name: 'CloudaccountCreate',
               path: 'create',
               component: CloudaccountCreate,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      meta: {
+        label: '调度',
+      },
+      submenus: [
+        {
+          path: '/schedtag',
+          meta: {
+            label: '调度标签',
+          },
+          component: Layout,
+          children: [
+            {
+              name: 'Schedtag',
+              path: '',
+              component: Schedtag,
+            },
+          ],
+        },
+        {
+          path: '/schedpolicy',
+          meta: {
+            label: '调度策略',
+          },
+          component: Layout,
+          children: [
+            {
+              name: 'Schedpolicy',
+              path: '',
+              component: Schedpolicy,
+            },
+          ],
+        },
+        {
+          path: '/dynamicschedtag',
+          meta: {
+            label: '动态调度标签',
+          },
+          component: Layout,
+          children: [
+            {
+              name: 'Dynamicschedtag',
+              path: '',
+              component: Dynamicschedtag,
             },
           ],
         },
---
+++ sections/StrategyRadio/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-form-item class="mb-0">
+    <a-radio-group v-decorator="decorator">
+      <a-radio-button
+        v-for="item in opts"
+        :value="item.key"
+        :key="item.key">{{ item.label }}</a-radio-button>
+    </a-radio-group>
+  </a-form-item>
+</template>
+
+<script>
+// description：偏好设置 组件
+import { STRATEGY_OPT } from '@Cloudenv/constants/sched'
+
+export default {
+  name: 'SrategyRadio',
+  props: {
+    decorator: {
+      type: Array,
+      required: true,
+    },
+  },
+  data () {
+    return {
+      opts: STRATEGY_OPT,
+    }
+  },
+}
+</script>
