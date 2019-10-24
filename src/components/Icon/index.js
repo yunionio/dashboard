b655326bc592fdd9d6b41a4f4ab2b8df18f81860
houjiazong <houjiazong@gmail.com>
2019-10-24 16:33:20 +0800

    Update icons

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,7 +1,11 @@
+import { Icon as AntIcon } from 'ant-design-vue'
+import { mergeProps } from 'ant-design-vue/lib/_util/props-util'
 
-function getImgs () {
+const customCache = new Set()
+
+function getLocalImages () {
   let paths = []
-  const filepathMap = {}
+  const localImagesMap = {}
   const imgs = require.context('./assets', false, /\.\/\w+\.svg/)
   imgs.keys().forEach(dir => {
     paths = paths.concat(imgs(dir))
@@ -10,55 +14,63 @@ function getImgs () {
     const pathArr = path.split('/')
     const filenameHash = pathArr[pathArr.length - 1]
     const filename = filenameHash.split('.')[0]
-    filepathMap[filename] = path
+    localImagesMap[filename] = path
   })
-  return filepathMap
+  return localImagesMap
 }
-const filepathMap = getImgs()
+const localImagesMap = getLocalImages()
 
-export default {
-  name: 'Icon',
-  functional: true,
-  props: {
-    name: {
-      type: String,
-      required: true,
+function create (options) {
+  const { scriptUrl, extraCommonProps = {} } = options
+  /**
+   * DOM API required.
+   * Make sure in browser environment.
+   * The Custom Icon will create a <script/>
+   * that loads SVG symbols and insert the SVG Element into the document body.
+   */
+  if (
+    typeof document !== 'undefined' &&
+    typeof window !== 'undefined' &&
+    typeof document.createElement === 'function' &&
+    typeof scriptUrl === 'string' &&
+    scriptUrl.length &&
+    !customCache.has(scriptUrl)
+  ) {
+    const script = document.createElement('script')
+    script.setAttribute('src', scriptUrl)
+    script.setAttribute('data-namespace', scriptUrl)
+    customCache.add(scriptUrl)
+    document.body.appendChild(script)
+  }
+  const Icon = {
+    functional: true,
+    name: 'Icon',
+    props: AntIcon.props,
+    render (h, context) {
+      const { props, slots, listeners, data } = context
+      const { type, ...restProps } = props
+      const localePath = localImagesMap[type]
+      if (localePath) {
+        const imgSize = (data.style && data.style.fontSize) || '16px'
+        return <img src={localePath} alt={type} width={imgSize} height={imgSize} />
+      }
+      const slotsMap = slots()
+      const children = slotsMap.default
+      // component > children > type
+      let content = null
+      if (type) {
+        content = <use {...{ attrs: { 'xlink:href': `#oc-${type}` } }} />
+      }
+      if (children) {
+        content = children
+      }
+      const iconProps = mergeProps(extraCommonProps, data, { props: restProps, on: listeners })
+      return <AntIcon {...iconProps}>{content}</AntIcon>
     },
-    fill: {
-      type: String,
-      default: 'currentColor',
-    },
-    width: {
-      type: String,
-      default: '1em',
-    },
-    height: {
-      type: String,
-      default: '1em',
-    },
-    class: {
-      type: String,
-    },
-  },
-  render (h, ctx) {
-    let locale = false // 是否是本地图标
-    const localePath = filepathMap[ctx.props.name]
-    const { name, ...rest } = ctx.props
-    const link = `#oc-${name}`
-    const svgProps = {
-      attrs: {
-        ...rest,
-      },
-    }
-    if (localePath) locale = true
-    const remoteIcon = (<i class='oc-icon'>
-      <svg aria-hidden='true' { ...svgProps }>
-        <use xlinkHref={ link } />
-      </svg>
-    </i>)
-    const localeIcon = <img src={localePath} alt={ctx.props.name} { ...svgProps } />
-    return (
-      locale ? localeIcon : remoteIcon
-    )
-  },
+  }
+  return Icon
 }
+
+export default create({
+  scriptUrl: '//at.alicdn.com/t/font_1347390_bgii1veo2zl.js',
+})
