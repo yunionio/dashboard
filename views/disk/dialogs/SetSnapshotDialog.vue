825079cd3fa92b1482023d501ffe958c0b0774de
郝程宇 <haochengyu@kuaifawu.com>
2019-11-08 19:10:22 +0800

    迁移硬盘模块到新项目

---
+++ views/disk/dialogs/SetSnapshotDialog.vue
@@ -0,0 +1,214 @@
+<template>
+  <base-dialog @cancel="cancelDialog">
+    <div slot="header">设置自动快照</div>
+    <div slot="body">
+      <a-form
+        :form="form.fc">
+        <a-form-item label="自动快照" v-bind="formItemLayout">
+          <a-radio-group v-decorator="decorators.isOpen">
+            <a-radio value="open">
+              开启
+            </a-radio>
+            <a-radio value="close">
+              关闭
+            </a-radio>
+          </a-radio-group>
+        </a-form-item>
+        <template v-if="enable">
+          <a-form-item label="策略名称" v-bind="formItemLayout">
+            <a-select v-decorator="decorators.snapshotpolicy">
+              <a-select-option v-for="item in snapshotpolicyOptions" :key="item.id">
+                {{getPolicyLabel(item)}}
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item label="备份日期" v-bind="formItemLayout">
+            <span class="ant-form-text">
+              {{ weekTips }}
+            </span>
+          </a-form-item>
+          <a-form-item label="备份时间" v-bind="formItemLayout">
+            <span class="ant-form-text">
+              {{ timeTips }}
+            </span>
+          </a-form-item>
+          <a-form-item label="保留时间" v-bind="formItemLayout">
+            <span class="ant-form-text">
+              {{ dayTips }}
+            </span>
+          </a-form-item>
+        </template>
+      </a-form>
+    </div>
+    <div slot="footer">
+      <a-button type="primary" @click="handleConfirm" :loading="loading">{{ $t('dialog.ok') }}</a-button>
+      <a-button @click="cancelDialog">{{ $t('dialog.cancel') }}</a-button>
+    </div>
+  </base-dialog>
+</template>
+
+<script>
+import * as R from 'ramda'
+import { weekOptions, timeOptions } from '../../../constants'
+import DialogMixin from '@/mixins/dialog'
+import WindowsMixin from '@/mixins/windows'
+
+export default {
+  name: 'SetSnapshotDialog',
+  mixins: [DialogMixin, WindowsMixin],
+  data () {
+    return {
+      loading: false,
+      form: {
+        fc: this.$form.createForm(this, {
+          onValuesChange: (props, values) => {
+            if (values.hasOwnProperty('snapshotpolicy')) {
+              this.assembly(values.snapshotpolicy)
+            }
+            if (values.hasOwnProperty('isOpen')) {
+              this.enable = !this.enable
+            }
+          },
+        }),
+      },
+      decorators: {
+        isOpen: [
+          'isOpen',
+          {
+            initialValue: 'close',
+          },
+        ],
+        snapshotpolicy: [
+          'snapshotpolicy',
+          {
+            rules: [
+              { required: true },
+            ],
+          },
+        ],
+      },
+      formItemLayout: {
+        wrapperCol: {
+          span: 8,
+        },
+        labelCol: {
+          span: 3,
+        },
+      },
+      snapshotpolicyOptions: [],
+      attchedPolices: this.params.data[0].snapshotpolicies,
+      weekTips: '',
+      timeTips: '',
+      dayTips: '',
+      enable: false,
+    }
+  },
+  created () {
+    this.fetchSnaphotpolicy()
+    this.manager = new this.$Manager('snapshotpolicies')
+  },
+  methods: {
+    validateForm () {
+      return new Promise((resolve, reject) => {
+        this.form.fc.validateFields((err, values) => {
+          if (!err) {
+            resolve(values)
+          } else {
+            reject(err)
+          }
+        })
+      })
+    },
+    detachPolicy (id) {
+      return this.manager.delete({
+        id,
+        ctx: [['disks', this.params.data[0]['id']]],
+      })
+    },
+    detachPolices () {
+      const promiseArr = this.attchedPolices.map(item => {
+        return this.detachPolicy(item.id)
+      })
+      return Promise.all(promiseArr)
+    },
+    async handleConfirm () {
+      this.loading = true
+      try {
+        let values = await this.validateForm()
+        this.loading = true
+        if (this.enable) {
+          await this.detachPolices()
+          await this.manager.performAction({
+            id: values.snapshotpolicy,
+            action: '',
+            data: null,
+            ctx: [['disks', this.params.data[0]['id']]],
+          })
+        } else {
+          if (this.attchedPolices.length) {
+            await this.detachPolices()
+          }
+        }
+        this.loading = false
+        this.params.list.refresh()
+        this.cancelDialog()
+      } catch (error) {
+        this.loading = false
+      }
+    },
+    getPolicyLabel (item) {
+      let text = item.name
+      if (item.repeat_weekdays && item.repeat_weekdays.length) {
+        text += ' -- 每' + item.repeat_weekdays.map(item => weekOptions[item - 1]).join('、')
+      }
+      if (item.time_points && item.time_points.length) {
+        text += '，' + item.time_points.map(item => timeOptions[item]).join('、')
+      }
+      if (item.retention_days === -1) {
+        text += '，永久'
+      } else if (item.retention_days) {
+        text += `，${item.retention_days}天`
+      }
+      if (text.length > 24) {
+        return text.substring(0, 24) + '...'
+      }
+      return text
+    },
+    async fetchSnaphotpolicy () {
+      const manager = new this.$Manager('snapshotpolicies')
+      let params = {
+        scope: 'system',
+      }
+      try {
+        const res = await manager.list({ params })
+        const data = (res.data.data || []).filter(item => {
+          return !this.attchedPolices.find(a => a.id === item.id)
+        })
+        if (data && data.length) this.snapshotpolicyOptions = data
+        if (this.snapshotpolicyOptions.length) {
+          this.form.fc.setFieldsValue({ snapshotpolicy: this.snapshotpolicyOptions[0].id })
+        }
+      } catch (error) {}
+    },
+    assembly (id) {
+      const snapshotpolicyObj = R.indexBy(R.prop('id'), this.snapshotpolicyOptions)
+      const selectItem = snapshotpolicyObj[id]
+      this.weekTips = (
+        selectItem.repeat_weekdays &&
+        selectItem.repeat_weekdays.map(item => weekOptions[item - 1]).join('、')
+      ) || '-'
+      this.timeTips = (
+        selectItem.time_points &&
+        selectItem.time_points.map(item => timeOptions[item]).join('、')
+      ) || '-'
+      let ret = '-'
+      if (selectItem.retention_days === -1) {
+        ret = '永久'
+      } else if (selectItem.retention_days) {
+        ret = `${selectItem.retention_days}天`
+      }
+      this.dayTips = ret
+    },
+  },
+}
+</script>
