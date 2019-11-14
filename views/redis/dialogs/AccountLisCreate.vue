188e1b3b0bb516afdd89bb51daaf01b0853548ac
csq <caosq123>
2019-11-14 19:13:17 +0800

    【2.12】feature: red

--- views/redis/dialogs/AccountLisCreate.vue
+++ views/redis/dialogs/AccountLisCreate.vue
@@ -57,6 +57,10 @@ export default {
         const ALL_LETTERS = /[a-z]/g
         const REG = /^[a-z][a-z0-9_]*$/
         if (value && value.length <= 16 && new RegExp('_').test(value) && REG.test(value) && ALL_DIGITS.test(value) && ALL_LETTERS.test(value)) {
+          console.log(value, value[value.length])
+          if (value[value.length - 1] === '_') {
+            _callback('不可以下划线结尾')
+          }
           _callback()
         } else {
           _callback('以字母开头，由小写字母，数字、下划线组成。长度不超过16个字符')
