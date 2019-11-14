66971226615cbd1c72d14f1a7289b53f30e4135f
houjiazong <houjiazong@gmail.com>
2019-11-14 19:55:19 +0800

    Remove promiseValidateForm util

--- plugins/validate.js
+++ plugins/validate.js
@@ -1,21 +1,8 @@
 import Vue from 'vue'
 import validateForm from '@/utils/validate'
 
-const promiseValidateForm = function (fc) {
-  return new Promise((resolve, reject) => {
-    fc.validateFields((err, values) => {
-      if (!err) {
-        resolve(values)
-      } else {
-        reject(err)
-      }
-    })
-  })
-}
-
 export default {
   install () {
     Vue.prototype.$validate = validateForm
-    Vue.prototype.$promiseValidateForm = promiseValidateForm
   },
 }
