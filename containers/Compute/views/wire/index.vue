ed8e48188b8a79e1f29cc34bf36e3c6c700aa44b
郝程宇 <haochengyu@kuaifawu.com>
2019-11-27 11:34:03 +0800

    迁移二层网络模块

---
+++ containers/Compute/views/wire/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <page-header title="二层网络" />
+    <page-body>
+      <wire-list />
+    </page-body>
+  </div>
+</template>
+
+<script>
+import WireList from './components/List'
+
+export default {
+  name: 'WireIndex',
+  components: {
+    WireList,
+  },
+}
+</script>
