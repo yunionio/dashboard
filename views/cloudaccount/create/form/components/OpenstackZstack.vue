b0b61cb4119cd00299668ec3b6b5a492b2e21409
wangpeng <wangpeng@yunionyun.com>
2019-11-29 18:30:39 +0800

    新建云账号域和项目添加默认值

--- views/cloudaccount/create/form/components/OpenstackZstack.vue
+++ views/cloudaccount/create/form/components/OpenstackZstack.vue
@@ -104,6 +104,7 @@ export default {
         domain: [
           'domain',
           {
+            initialValue: this.$store.getters.userInfo.projectDomainId,
             rules: [
               { validator: isRequired(), message: '请选择域', trigger: 'change' },
             ],
@@ -112,6 +113,7 @@ export default {
         project: [
           'project',
           {
+            initialValue: this.$store.getters.userInfo.projectId,
             rules: [
               { validator: isRequired(), message: '请选择项目', trigger: 'change' },
             ],
