27945e932cf549e5da0f1b83ca39fff27e3320db
Mingjiu Gao <gaomingjiu@yunionyun.com>
2019-12-04 17:44:02 +0800

    add 硬盘快照列表及回滚硬盘

---
+++ views/snapshot/DiskSnapshot.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <page-header title="硬盘快照" />
+    <page-body>
+      <snapshot-list :list="list" />
+    </page-body>
+  </div>
+</template>
+
+<script>
+import SnapshotList from './components/List'
+
+export default {
+  name: 'DiskIndex',
+  components: {
+    SnapshotList,
+  },
+  data () {
+    return {
+      list: this.$list.createList(this, {
+        id: 'VmSnapshotsList',
+        resource: 'snapshots',
+        getParams: {
+          details: true,
+          with_meta: true,
+          is_instance_snapshot: false,
+        },
+        filterOptions: {
+          name: {
+            label: '名称',
+            filter: true,
+            formatter: val => {
+              return `name.contains(${val})`
+            },
+          },
+        },
+      }),
+    }
+  },
+}
+</script>
