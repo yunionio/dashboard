660162f33199bc158d7cbfadcaf3ed7c2ae57c23
wangpeng <wangpeng@yunionyun.com>
2019-12-07 11:06:52 +0800

    新建IDC虚机时，网络应是“默认”，且指定IP子网时应有下拉框。

--- components/BaseSelect/OptionLabel.js
+++ components/BaseSelect/OptionLabel.js
@@ -1,10 +1,13 @@
 const resourceMode = {
-  networks: (vm, h) => ( // IP子网
-    <div>
-      <span style="float: left">{ vm.getLabel() }</span>
-      <span style="float: right; color: #8492a6; font-size: 13px">可用: { vm.data.ports - vm.data.ports_used }</span>
-    </div>
-  ),
+  networks: (vm, h) => {
+    const text = vm.getLabel()
+    return ( // IP子网
+      <div class='d-flex'>
+        <span class='text-truncate flex-fill mr-2' title={ text }>{ text }</span>
+        <span style="color: #8492a6; font-size: 13px">可用: { vm.data.ports - vm.data.ports_used }</span>
+      </div>
+    )
+  },
 }
 
 export default {
