23b2fbb909a56184aba7a73ba36c624590ef1b8c
houjiazong <houjiazong@gmail.com>
2019-12-09 13:12:07 +0800

    fix: github actions

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 echo "🗃 Initializing git repository..."
 
-git clone git@github.com:yunionio/dashboard-common.git src
-git clone git@github.com:yunionio/dashboard-module-cloudenv.git containers/Cloudenv
-git clone git@github.com:yunionio/dashboard-module-compute.git containers/Compute
+git clone https://github.com/yunionio/dashboard-common.git src
+git clone https://github.com/yunionio/dashboard-module-cloudenv.git containers/Cloudenv
+git clone https://github.com/yunionio/dashboard-module-compute.git containers/Compute
 cd src
 yarn
 cd ../containers/Compute
