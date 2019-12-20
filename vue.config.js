7b5797ecf01e311bf5b8692d1d4ec1334ae27d40
wangpeng <wangpeng@yunionyun.com>
2019-12-20 10:32:16 +0800

    feature：vue.config.js 添加 watchOptions

--- vue.config.js
+++ vue.config.js
@@ -66,6 +66,9 @@ module.exports = {
       warnings: true,
       errors: true,
     },
+    watchOptions: {
+      ignored: ['src/node_modules', 'containers/**/node_modules', 'node_modules'],
+    },
     open: process.platform === 'darwin',
     port: 8080,
     proxy: {
