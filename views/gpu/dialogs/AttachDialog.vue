cd0d997f756bf35ac75323cade780b475dd0094c
Mingjiu Gao <gaomingjiu@yunionyun.com>
2019-12-26 11:46:49 +0800

    feat #1113 透传设备关联主机增加提示信息

--- views/gpu/dialogs/AttachDialog.vue
+++ views/gpu/dialogs/AttachDialog.vue
@@ -2,7 +2,7 @@
   <base-dialog @cancel="cancelDialog">
     <div slot="header">关联主机</div>
     <div slot="body">
-      <dialog-selected-tips :count="params.data.length" action="关联主机" />
+      <dialog-selected-tips name="GPU" :count="params.data.length" action="关联主机" />
       <vxe-grid class="mb-2" :data="params.data" :columns="params.columns.slice(0, 3)" />
       <a-form
         :form="form.fc">
