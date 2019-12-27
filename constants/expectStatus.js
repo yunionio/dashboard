77bc6a2770ac0dc7cb13bffadb52f8b6ee641697
Mingjiu Gao <gaomingjiu@yunionyun.com>
2019-12-27 15:36:50 +0800

    feat VMware主机支持挂起与恢复操作

--- constants/expectStatus.js
+++ constants/expectStatus.js
@@ -2,7 +2,7 @@
 export default {
   server: {
     success: ['running'],
-    info: ['ready', 'deallocated', 'unknown'],
+    info: ['ready', 'deallocated', 'unknown', 'suspend'],
     // danger: [new RegExp('fail')] // 这条会在 base组件 中默认存在
   },
   network: {
