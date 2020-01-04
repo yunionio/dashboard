fc233a52aad27766862ca1f3800c767da2f5e22b
郝程宇 <haochengyu@kuaifawu.com>
2020-01-04 23:53:26 +0800

    修复公共组件指定标签样式

--- sections/SchedtagPolicy/index.vue
+++ sections/SchedtagPolicy/index.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="d-flex">
     <a-form-item
-      class="mb-0 mr-1">
+      class="mb-0 mr-1 w-50">
       <base-select
+        class="w-100"
         v-decorator="decorators.schedtag"
         resource="schedtags"
         :need-params="true"
         :params="schedtagParams"
         :select-props="{ allowClear: true, placeholder: '请选择调度标签' }" />
     </a-form-item>
-    <a-form-item class="mb-0">
+    <a-form-item class="mb-0 w-50">
       <base-select
+        class="w-100"
         v-decorator="decorators.policy"
         :options="policyOpts"
         :select-props="{ allowClear: true, placeholder: '请选择调度策略' }" />
