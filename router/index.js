07521af28f5892a88032cbea526d85b991eb57e8
houjiazong <houjiazong@gmail.com>
2020-01-14 19:09:57 +0800

    fix: remove keypair permission

--- router/index.js
+++ router/index.js
@@ -366,7 +366,6 @@ export default {
           path: '/keypair',
           meta: {
             label: '密钥',
-            permission: 'keypairs_list',
           },
           component: Layout,
           children: [
