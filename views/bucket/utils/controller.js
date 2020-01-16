e56309779a6e88997cba9015f03153b83f0ef0c2
曹赛琪 <caosaiqi@yunionyun.com>
2020-01-16 14:27:05 +0800

    feat: 修改文件列表分页

--- views/bucket/utils/controller.js
+++ views/bucket/utils/controller.js
@@ -1,42 +1,52 @@
 import { Manager } from '@/utils/manager'
-const bucketsManager = new Manager('buckets', 'v2')
 
 export const objectsModel = {
-  bucketsManager,
   async getAcl (resName, key) {
+    let bucketsManager = new Manager('buckets', 'v2')
     try {
       const { data = {} } = await bucketsManager.getSpecific({ id: resName, spec: 'acl', params: { key } })
       return data
     } catch (err) {
       throw err
+    } finally {
+      bucketsManager = null
     }
   },
-  async getUrl (row, resName) {
+  async getUrl (row, resName, accessUrl) {
     const { key } = row
     const { acl } = await this.getAcl(resName, key)
-    if (acl === 'private') {
-      const params = {
-        key,
-        method: 'GET',
-      }
-      // 链接有效时间 秒
-      if (row.expire_seconds) {
-        params['expire_seconds'] = row.expire_seconds
-      }
-      const { data = {} } = await bucketsManager.performAction({
-        id: resName,
-        action: 'temp-url',
-        data: params,
-      })
-      const { url } = data
-      if (url) {
-        if (!url.startsWith('http')) {
-          return `http://${url}`
+    let bucketsManager = new Manager('buckets', 'v2')
+    try {
+      if (acl === 'private') {
+        const params = {
+          key,
+          method: 'GET',
         }
-        return url
+        // 链接有效时间 秒
+        if (row.expire_seconds) {
+          params['expire_seconds'] = row.expire_seconds
+        }
+        const { data = {} } = await bucketsManager.performAction({
+          id: resName,
+          action: 'temp-url',
+          data: params,
+        })
+        const { url } = data
+        if (url) {
+          if (!url.startsWith('http')) {
+            return `http://${url}`
+          }
+          return url
+        }
+        return null
+      } else if (accessUrl) {
+        return `http://${accessUrl}/${key}`
       }
       return null
+    } catch (err) {
+      throw err
+    } finally {
+      bucketsManager = null
     }
-    return null
   },
 }
