86956b40ef3548dada44e459749637840605ec81
lengband <lengband@163.com>
2020-02-07 15:19:04 +0800

    feat ServerPassword 添加测试用例

--- sections/ServerPassword/__tests__/index.js
+++ sections/ServerPassword/__tests__/index.js
@@ -1,15 +1,14 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import { LOGIN_TYPES_MAP } from '@Compute/constants'
 import VueRouter from 'vue-router'
-import Base from 'ant-design-vue/lib/base'
+import BaseSelect from '@tests/stubs/BaseSelect'
+import Icon from '@tests/stubs/Icon'
 import ServerPassword from '../index'
 
 const localVue = createLocalVue()
 localVue.use(VueRouter)
-localVue.use(Base)
 localVue.directive('decorator', {})
-// eslint-disable-next-line no-unused-vars
-const router = new VueRouter() // 暂时无法解决 BaseSelect value is required
+const router = new VueRouter()
 
 let decorator = null
 let form = null
@@ -66,21 +65,26 @@ describe('ServerPassword loginTypeMap', () => {
   })
 
   // 暂时无法解决 BaseSelect value is required
-  // it('check loginTypeMap initValue is the first of loginTypes', () => {
-  //   let loginTypes = ['keypair', 'password']
-  //   const wrapper = shallowMount(ServerPassword, {
-  //     propsData: {
-  //       decorator,
-  //       loginTypes,
-  //       form,
-  //     },
-  //     localVue,
-  //     router,
-  //   })
-  //   expect(wrapper.vm.loginTypeMap).toEqual({
-  //     keypair: LOGIN_TYPES_MAP.keypair,
-  //     password: LOGIN_TYPES_MAP.password,
-  //   })
-  //   expect(wrapper.vm.form.fc.setFieldsValue.mock.calls[0][0]).toEqual({ loginType: LOGIN_TYPES_MAP.keypair.key })
-  // })
+  it('check loginTypeMap initValue is the first of loginTypes', () => {
+    let loginTypes = ['keypair', 'password']
+    const wrapper = shallowMount(ServerPassword, {
+      propsData: {
+        decorator,
+        loginTypes,
+        form,
+      },
+      stubs: {
+        'base-select': BaseSelect,
+        'icon': Icon,
+      },
+      localVue,
+      router,
+    })
+    expect(wrapper.vm.loginTypeMap).toEqual({
+      keypair: LOGIN_TYPES_MAP.keypair,
+      password: LOGIN_TYPES_MAP.password,
+    })
+    expect(wrapper.vm.form.fc.setFieldsValue.mock.calls[0][0]).toEqual({ loginType: LOGIN_TYPES_MAP.keypair.key })
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
 })
