14741b7eb68fe8f4d31bf03b17211b8a1b6ce82e
郝程宇 <haochengyu@kuaifawu.com>
2020-02-09 15:37:46 +0800

    修改镜像列表刷新期望值状态问题

--- views/host-image/components/List.vue
+++ views/host-image/components/List.vue
@@ -11,6 +11,7 @@
 import { sizestr } from '@/utils/utils'
 import { getStatusTableColumn, getNameDescriptionTableColumn, getProjectTableColumn, isPublicTableColumn, getTimeTableColumn } from '@/utils/common/tableColumn'
 import { getTenantFilter, getStatusFilter } from '@/utils/common/tableFilter'
+import expectStatus from '@/constants/expectStatus'
 import SystemIcon from '@/sections/SystemIcon'
 import BaseDropList from '@/sections/DropList'
 import WindowsMixin from '@/mixins/windows'
@@ -53,6 +54,7 @@ export default {
         resource: 'guestimages',
         apiVersion: 'v1',
         getParams: this.getParam,
+        steadyStatus: Object.values(expectStatus.image).flat(),
         filterOptions: {
           name: {
             label: '名称',
