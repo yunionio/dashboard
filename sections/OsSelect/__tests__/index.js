e6cd139af4addd76a533c13fbf59299b996f8970
lengband <lengband@163.com>
2020-02-13 10:49:17 +0800

    feat 添加OsSelect和ImageSelect的单元测试

---
+++ sections/OsSelect/__tests__/index.js
@@ -0,0 +1,66 @@
+import { shallowMount } from '@vue/test-utils'
+import OsSelect from '../index'
+import { IMAGES_TYPE_MAP } from '@/constants/compute'
+
+let setFieldsValue = null
+let decorator = null
+
+let $route = {
+  query: {
+  },
+}
+
+beforeEach(() => {
+  setFieldsValue = jest.fn()
+  decorator = {
+    os: [
+      'os',
+      {
+        initialValue: '',
+        rules: [
+          { required: true, message: '请选择操作系统' },
+        ],
+      },
+    ],
+    image: [
+      'image',
+      {
+        initialValue: { key: '', label: '' },
+      },
+    ],
+    imageType: [
+      'imageType',
+      {
+        initialValue: 'standard',
+      },
+    ],
+  }
+})
+
+describe('OsSelect', () => {
+  it('IDC base', () => {
+    const type = 'idc'
+    const hypervisor = 'kvm'
+    const wrapper = shallowMount(OsSelect, {
+      provide () {
+        return {
+          form: {
+            fc: {
+              setFieldsValue,
+            },
+          },
+        }
+      },
+      propsData: {
+        decorator,
+        type,
+        hypervisor,
+      },
+      mocks: {
+        $route,
+      },
+    })
+    expect(wrapper.vm.mirrorTypeOptions).toEqual([IMAGES_TYPE_MAP.standard, IMAGES_TYPE_MAP.customize, IMAGES_TYPE_MAP.iso, IMAGES_TYPE_MAP.host, IMAGES_TYPE_MAP.snapshot])
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+})
