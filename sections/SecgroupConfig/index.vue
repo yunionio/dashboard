10244a59acb7b397e717917f672eb9069f8d6222
lengband <lengband@163.com>
2020-02-14 19:26:33 +0800

    fix(SecgroupConfig) [2.13] zstack仅能选择一个安全组

--- sections/SecgroupConfig/index.vue
+++ sections/SecgroupConfig/index.vue
@@ -10,7 +10,7 @@
     </a-form-item>
     <a-form-item class="mb-0" v-if="isBind">
       <div slot="extra">
-        {{ `最多支持选择${ (isAzure || isUCloud) ? 1 : 5 }个安全组。没有想要的安全组？可以前往` }}
+        {{ `最多支持选择${ (isAzure || isUCloud || isZstack) ? 1 : 5 }个安全组。没有想要的安全组？可以前往` }}
         <help-link :href="href"> 新建安全组</help-link>
       </div>
       <base-select
@@ -56,9 +56,9 @@ export default {
   },
   data () {
     const validateSecgroups = (rule, value, callback) => {
-      let maxError = (this.isAzure || this.isUCloud) ? 'Azure和Ucloud最多关联一个安全组' : '最多关联五个安全组'
+      let maxError = (this.isAzure || this.isUCloud || this.isZstack) ? 'Azure和Ucloud最多关联一个安全组' : '最多关联五个安全组'
       let minError = '最少关联一个'
-      let max = (this.isAzure || this.isUCloud) ? 1 : 5
+      let max = (this.isAzure || this.isUCloud || this.isZstack) ? 1 : 5
       if (value.length > max) {
         return callback(maxError)
       }
@@ -102,6 +102,9 @@ export default {
     },
     isUCloud () {
       return this.provider.toLowerCase() === HYPERVISORS_MAP.ucloud.provider.toLowerCase()
+    },
+    isZstack () {
+      return this.provider.toLowerCase() === HYPERVISORS_MAP.zstack.provider.toLowerCase()
     },
   },
   watch: {
