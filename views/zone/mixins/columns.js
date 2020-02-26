2c387e5ce1929c607eda6d584ae921c1203eeb31
wangpeng <wangpeng@yunionyun.com>
2020-02-26 16:39:46 +0800

    feat [master] 可用区list与抽屉解耦完成

---
+++ views/zone/mixins/columns.js
@@ -0,0 +1,40 @@
+import {
+  getNameDescriptionTableColumn,
+} from '@/utils/common/tableColumn'
+
+export default {
+  created () {
+    this.columns = [
+      getNameDescriptionTableColumn({
+        vm: this,
+        hideField: true,
+        slotCallback: row => {
+          return (
+            <side-page-trigger onTrigger={() => this.handleOpenSidepage(row)}>{ row.name_cn ? `${row.name}(${row.name_cn})` : row.name }</side-page-trigger>
+          )
+        },
+      }),
+      {
+        field: 'hosts',
+        title: '物理机/可用物理机',
+        width: 140,
+        formatter: ({ row }) => {
+          return `${row.hosts}/${row.hosts_enabled}`
+        },
+      },
+      {
+        field: 'baremetals',
+        title: '受管物理机/可用受管物理机',
+        width: 180,
+        formatter: ({ row }) => {
+          return `${row.baremetals}/${row.baremetals_enabled}`
+        },
+      },
+      {
+        field: 'wires',
+        title: '二层网络',
+        width: 70,
+      },
+    ]
+  },
+}
---
+++ views/zone/mixins/singleActions.js
@@ -0,0 +1,25 @@
+export default {
+  created () {
+    this.singleActions = [
+      {
+        label: '删除',
+        action: obj => {
+          this.createDialog('DeleteResDialog', {
+            data: [obj],
+            columns: this.columns,
+            title: '删除',
+            onManager: this.onManager,
+            success: () => {
+              this.destroySidePages()
+            },
+          })
+        },
+        meta: obj => {
+          return {
+            validate: obj.can_delete,
+          }
+        },
+      },
+    ]
+  },
+}
