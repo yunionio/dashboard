c4b269d86641ffedff23fb32f7156463b4cf647d
郝程宇 <haochengyu@kuaifawu.com>
2020-03-05 13:32:52 +0800

    全局替换vxe-grid组件

--- views/cloudaccount/dialogs/SetAutoSync.vue
+++ views/cloudaccount/dialogs/SetAutoSync.vue
@@ -3,7 +3,7 @@
     <div slot="header">设置自动同步</div>
     <div slot="body">
       <dialog-selected-tips :count="params.data.length" action="设置自动同步" />
-      <vxe-grid class="mb-2" :data="params.data" :columns="params.columns.slice(0, 3)" />
+      <dialog-table :data="params.data" :columns="params.columns.slice(0, 3)" />
       <a-form
         :form="form.fc">
         <a-form-item label="自动同步" v-bind="formItemLayout">
