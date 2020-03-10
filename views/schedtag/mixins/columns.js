570357d6bb4fa7b037a71211cce5b76afc59d020
Mingjiu Gao <gaomingjiu@yunionyun.com>
2020-03-10 17:14:26 +0800

    feat 调度及标签详情页增加信息

--- views/schedtag/mixins/columns.js
+++ views/schedtag/mixins/columns.js
@@ -1,9 +1,13 @@
-import { STRATEGY_CN } from '@Cloudenv/constants/sched'
+import {
+  getDefaultStrategyTableColumn,
+  getResourceTypeTableColumn,
+  getResourceCountTableColumn,
+  getDynamicSchedtagCountTableColumn,
+  getSchedpolicyCountTableColumn,
+} from '../utils/columns'
 import {
   getNameDescriptionTableColumn,
 } from '@/utils/common/tableColumn'
-
-const RES_TYPES = { hosts: '宿主机、物理机', storages: '存储', networks: '网络' }
 
 export default {
   created () {
@@ -17,47 +21,11 @@ export default {
           )
         },
       }),
-      {
-        field: 'default_strategy',
-        title: '偏好',
-        width: 80,
-        formatter: ({ row }) => {
-          return STRATEGY_CN[row.default_strategy] || '无'
-        },
-      },
-      {
-        field: 'resource_type',
-        title: '资源类型',
-        width: 120,
-        formatter: ({ row }) => {
-          return RES_TYPES[row.resource_type] || '无'
-        },
-      },
-
-      {
-        field: 'resource_count',
-        title: '资源数量',
-        width: 80,
-        formatter: ({ row }) => {
-          return row.host_count || row.storage_count || row.network_count || '0'
-        },
-      },
-      {
-        field: 'dynamic_schedtag_count',
-        title: '关联动态调度标签',
-        width: 120,
-        formatter: ({ row }) => {
-          return row.dynamic_schedtag_count || '0'
-        },
-      },
-      {
-        field: 'schedpolicy_count',
-        title: '关联调度策略',
-        width: 120,
-        formatter: ({ row }) => {
-          return row.schedpolicy_count || '0'
-        },
-      },
+      getDefaultStrategyTableColumn(),
+      getResourceTypeTableColumn(),
+      getResourceCountTableColumn(),
+      getDynamicSchedtagCountTableColumn(),
+      getSchedpolicyCountTableColumn(),
     ]
   },
 }
