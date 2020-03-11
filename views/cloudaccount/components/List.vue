2443912d6bb0c2a2c30fd567c6d8bdef5821367c
曹赛琪 <caosaiqi@yunionyun.com>
2020-03-11 13:49:28 +0800

    merge

--- views/cloudaccount/components/List.vue
+++ views/cloudaccount/components/List.vue
@@ -308,10 +308,7 @@ export default {
         },
         {
           label: '更新账单文件',
-<<<<<<< HEAD
           permission: 'cloudaccounts_perform_update_credential',
-=======
->>>>>>> db3ccd63bd005f5381bcb4f0a02603fe6f2fe239
           action: obj => {
             this.$router.push({
               name: 'CloudaccountUpdateBill',
