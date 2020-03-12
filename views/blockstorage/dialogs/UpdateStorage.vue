1a7115f8e85c5fde4bf8c58c606924127f08d4ab
曹赛琪 <caosaiqi@yunionyun.com>
2020-03-12 10:55:21 +0800

    feat: 修改属性添加介质类型

--- views/blockstorage/dialogs/UpdateStorage.vue
+++ views/blockstorage/dialogs/UpdateStorage.vue
@@ -3,11 +3,16 @@
     <div slot="header">{{this.params.title}}</div>
     <div slot="body">
       <dialog-selected-tips :count="params.data.length" :action="this.params.title" name="块存储" />
-      <vxe-grid class="mb-2" :data="params.data" :columns="params.columns.slice(0, 3)" />
+      <dialog-table :data="params.data" :columns="columns" />
       <a-form :form="form.fc" v-bind="formItemLayout">
         <form-items :storage_type="storage_type" />
          <a-form-item label="超售比">
           <a-input-number :step="0.1" v-decorator="decorators.commit_bound" :min="0" />
+        </a-form-item>
+        <a-form-item label="介质类型">
+          <a-select v-decorator="decorators.medium_type" style="width: 200px">
+            <a-select-option v-for="(val, key) in MEDIUM_TYPES"  :key="key" :value="key">{{val}}</a-select-option>
+          </a-select>
         </a-form-item>
       </a-form>
     </div>
@@ -19,8 +24,9 @@
 </template>
 
 <script>
-import { formItemLayout } from '@Storage/constants/index.js'
+import { formItemLayout, MEDIUM_TYPES } from '@Storage/constants/index.js'
 import FormItems from '@Storage/views/blockstorage/components/FormItems'
+import { getCopyWithContentTableColumn } from '@/utils/common/tableColumn'
 import DialogMixin from '@/mixins/dialog'
 import WindowsMixin from '@/mixins/windows'
 
@@ -32,11 +38,29 @@ export default {
   mixins: [DialogMixin, WindowsMixin],
   data () {
     return {
+      MEDIUM_TYPES,
       loading: false,
       formItemLayout,
       form: {
         fc: this.$form.createForm(this),
       },
+      columns: [
+        getCopyWithContentTableColumn(),
+        {
+          field: 'commit_bound',
+          title: '超售比',
+          formatter: ({ row }) => {
+            return row.commit_bound ? row.commit_bound.toFixed(1) : '-'
+          },
+        },
+        {
+          field: 'medium_type',
+          title: '介质类型',
+          formatter: ({ row }) => {
+            return MEDIUM_TYPES[row.medium_type] || row.medium_type
+          },
+        },
+      ],
     }
   },
   provide () {
@@ -56,10 +80,16 @@ export default {
         commit_bound: [
           'commit_bound',
           {
-            initialValue: this.params.data.commit_bound || 1,
+            initialValue: this.params.data[0].commit_bound || 1,
             rules: [
               { required: true, message: '请设置超售比' },
             ],
+          },
+        ],
+        medium_type: [
+          'medium_type',
+          {
+            initialValue: this.params.data[0].medium_type,
           },
         ],
       }
