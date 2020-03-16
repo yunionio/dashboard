1cf1cac1286497171f1d4c2a30dd40c545fe1630
郝程宇 <haochengyu@kuaifawu.com>
2020-03-16 20:41:47 +0800

    修改主机模块3.1bug

--- views/snapshotpolicy/dialogs/AttachDisk.vue
+++ views/snapshotpolicy/dialogs/AttachDisk.vue
@@ -3,7 +3,7 @@
     <div slot="header">关联硬盘</div>
     <div slot="body">
       <a-alert class="mb-2" type="warning" :message="message" />
-      <dialog-selected-tips :count="params.data.length" action="关联硬盘" />
+      <dialog-selected-tips :count="params.data.length" action="关联硬盘" name="策略" />
       <dialog-table :data="params.data" :columns="params.columns.slice(0, 3)" />
       <loader loading v-if="!(bindedDisksLoaded && disksInitLoaded)" />
       <a-form :form="form.fc" hideRequiredMark v-show="bindedDisksLoaded && disksInitLoaded">
