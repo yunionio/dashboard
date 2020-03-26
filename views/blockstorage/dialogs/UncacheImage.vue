83fffe41369fc5a91d0325f20d13c45ac884ab62
郝程宇 <haochengyu@kuaifawu.com>
2020-03-26 15:23:06 +0800

    修改存储模块弹窗文案

--- views/blockstorage/dialogs/UncacheImage.vue
+++ views/blockstorage/dialogs/UncacheImage.vue
@@ -2,7 +2,7 @@
   <base-dialog @cancel="cancelDialog">
     <div slot="header">{{this.params.title}}</div>
     <div slot="body">
-      <dialog-selected-tips :count="params.data.length" :action="this.params.title" name="镜像" />
+      <dialog-selected-tips :count="params.data.length" :action="this.params.title" :name="$t('dictionary.image')" />
       <dialog-table :data="params.data" :columns="params.columns.slice(0, 3)" />
     </div>
     <div slot="footer">
