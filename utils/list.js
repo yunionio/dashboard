082dd2b1604ca1e9ed6aa7fbb09604435b2ef887
侯佳宗 <houjiazong@yunionyun.com>
2020-04-03 16:33:47 +0800

    fix: remove selectItems getter

--- utils/list.js
+++ utils/list.js
@@ -174,7 +174,7 @@ class CreateList {
     this.total = 0
     this.nextMarker = ''
     // 选择数据
-    this._selectedItems = []
+    this.selectedItems = []
     this.selected = []
     // 指定作为id的属性key值
     this.idKey = idKey
@@ -197,18 +197,6 @@ class CreateList {
     this.responseData = responseData
     // 初始化 params
     this.params = {}
-  }
-  // 重写selectedItems getter和setter
-  get selectedItems () {
-    const items = []
-    R.forEach(id => {
-      items.push(this.data[id]['data'])
-    }, this.selected)
-    this._selectedItems = items
-    return items
-  }
-  set selectedItems (items) {
-    this._selectedItems = items
   }
   /**
    * @description 获取列表配置，如果没有则创建
