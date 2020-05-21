e3a76d7e01660d566d56d3858d7ab2c60931b7d2
Mingjiu Gao <gaomingjiu@yunionyun.com>
2020-05-21 18:58:42 +0800

    feat 调整主机模块详情页展示

--- views/image/sidepage/Detail.vue
+++ views/image/sidepage/Detail.vue
@@ -33,7 +33,7 @@ export default {
       baseInfo: [
         {
           field: 'project_domain',
-          title: '部门',
+          title: this.$t('dictionary.domain'),
           formatter: ({ row }) => {
             if (!row.domain_id) return '-'
             return <side-page-trigger permission="domains_get" name="DomainSidePage" id={row.domain_id} vm={this}>{ row.project_domain }</side-page-trigger>
