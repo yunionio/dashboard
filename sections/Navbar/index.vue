f891291515d70523b395d40a148edeaad1e41c0d
侯佳宗 <houjiazong@yunionyun.com>
2020-05-25 15:26:48 +0800

    fix: check work order popover service

--- sections/Navbar/index.vue
+++ sections/Navbar/index.vue
@@ -100,7 +100,7 @@
     <!-- 消息中心 -->
     <notify-popover class="navbar-item" />
     <!-- 工单 -->
-    <work-order-popover class="navbar-item" />
+    <work-order-popover class="navbar-item" v-if="itsmServiceEnable" />
     <!-- 帮助 -->
     <help-popover class="navbar-item" />
     <!-- 用户 -->
@@ -275,6 +275,10 @@ export default {
     },
     licenseClosable () {
       return this.computeStatus.prohibited || this.computeStatus.exceeded
+    },
+    itsmServiceEnable () {
+      const itsm = (this.userInfo.services || []).find(v => v.type === 'itsm' && v.status === true)
+      return !!itsm
     },
   },
   watch: {
