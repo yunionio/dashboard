e8d0b6d02248bf27f9b5feaf192c9a82d376b9ca
侯佳宗 <houjiazong@yunionyun.com>
2020-05-26 18:03:57 +0800

    fix: eslint camelcase

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
+    'camelcase': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'import/order': ['error', {
       'groups': ['builtin', 'external', 'parent', 'sibling', 'index'],
     }],
