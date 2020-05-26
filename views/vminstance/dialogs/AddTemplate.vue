9fea594c103d5d942d77cfdef467a668f42d82b8
曹赛琪 <caosaiqi@yunio>
2020-05-26 14:33:12 +0800

    主机添加操作-保存主机模版

---
+++ views/vminstance/dialogs/AddTemplate.vue
@@ -0,0 +1,82 @@
+<template>
+  <base-dialog @cancel="cancelDialog">
+    <div slot="header">保存主机模版</div>
+    <div slot="body">
+      <dialog-selected-tips :name="$t('dictionary.server')" :count="params.data.length" action="保存主机模版" />
+      <dialog-table :data="params.data" :columns="columns" />
+      <a-form :form="form.fc"  v-bind="formItemLayout">
+        <a-form-item :label="$t('common.name')">
+          <a-input v-decorator="decorators.name" placeholder="请输入名称" />
+        </a-form-item>
+      </a-form>
+    </div>
+    <div slot="footer">
+      <a-button type="primary" @click="handleConfirm" :loading="loading">{{ $t('dialog.ok') }}</a-button>
+      <a-button @click="cancelDialog">{{ $t('dialog.cancel') }}</a-button>
+    </div>
+  </base-dialog>
+</template>
+
+<script>
+import DialogMixin from '@/mixins/dialog'
+import WindowsMixin from '@/mixins/windows'
+
+export default {
+  name: 'VmAddTemplateDialog',
+  mixins: [DialogMixin, WindowsMixin],
+  data () {
+    return {
+      loading: false,
+      form: {
+        fc: this.$form.createForm(this),
+      },
+      decorators: {
+        name: [
+          'name',
+          {
+            rules: [
+              { required: true, message: '请输入名称' },
+            ],
+          },
+        ],
+      },
+      formItemLayout: {
+        wrapperCol: {
+          span: 20,
+        },
+        labelCol: {
+          span: 4,
+        },
+      },
+    }
+  },
+  computed: {
+    columns () {
+      const fields = ['name', 'instance_type', 'brand']
+      return this.params.columns.filter(item => {
+        const { field } = item
+        return fields.indexOf(field) > -1
+      })
+    },
+  },
+  methods: {
+    async handleConfirm () {
+      this.loading = true
+      try {
+        const values = await this.form.fc.validateFields()
+        await this.params.onManager('performAction', {
+          id: this.params.data[0].id,
+          steadyStatus: ['running', 'ready'],
+          managerArgs: {
+            action: 'save-template',
+            data: values,
+          },
+        })
+        this.cancelDialog()
+      } finally {
+        this.loading = false
+      }
+    },
+  },
+}
+</script>
