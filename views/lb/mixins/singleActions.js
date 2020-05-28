22956fe2cc6affebd9f152fc0204b486abfc4ab9
曹赛琪 <caosaiqi@yunio>
2020-05-28 17:02:33 +0800

    设置启用禁用操作

--- views/lb/mixins/singleActions.js
+++ views/lb/mixins/singleActions.js
@@ -1,62 +1,63 @@
 import _ from 'lodash'
 import { PROVIDER_MAP } from '@/constants'
 import expectStatus from '@/constants/expectStatus'
+import { getEnabledSwitchActions } from '@/utils/common/tableActions'
 const notSupportEnable = ['huawei', 'aws', 'qcloud']
 const notSupportDisable = ['huawei', 'aws', 'qcloud']
 
 export default {
   created () {
     this.singleActions = [
-      {
-        label: '启用',
-        action: (obj) => {
-          this.onManager('performAction', {
-            steadyStatus: Object.values(expectStatus.lb).flat(),
-            id: obj.id,
-            managerArgs: {
-              action: 'status',
-              data: {
-                id: obj.id,
-                status: 'enabled',
+      ...getEnabledSwitchActions(this, undefined, undefined, {
+        actions: [
+          (obj) => {
+            this.onManager('performAction', {
+              steadyStatus: Object.values(expectStatus.lb).flat(),
+              id: obj.id,
+              managerArgs: {
+                action: 'status',
+                data: {
+                  id: obj.id,
+                  status: 'enabled',
+                },
               },
-            },
-          })
-        },
-        meta: (obj) => {
-          const notSupport = notSupportEnable.includes(obj.provider.toLowerCase())
-          const label = notSupport ? _.get(PROVIDER_MAP, `[${obj.provider}].label`) : ''
-          const tooltip = label ? `【${label}】暂不支持该操作` : ''
-          return {
-            validate: !notSupport && obj.status === 'disabled',
-            tooltip,
-          }
-        },
-      },
-      {
-        label: '禁用',
-        action: (obj) => {
-          this.onManager('performAction', {
-            steadyStatus: Object.values(expectStatus.lb).flat(),
-            id: obj.id,
-            managerArgs: {
-              action: 'status',
-              data: {
-                id: obj.id,
-                status: 'disabled',
+            })
+          },
+          (obj) => {
+            this.onManager('performAction', {
+              steadyStatus: Object.values(expectStatus.lb).flat(),
+              id: obj.id,
+              managerArgs: {
+                action: 'status',
+                data: {
+                  id: obj.id,
+                  status: 'disabled',
+                },
               },
-            },
-          })
-        },
-        meta: (obj) => {
-          const notSupport = notSupportDisable.includes(obj.provider.toLowerCase())
-          const label = notSupport ? _.get(PROVIDER_MAP, `[${obj.provider}].label`) : ''
-          const tooltip = label ? `【${label}】暂不支持该操作` : ''
-          return {
-            validate: !notSupport && obj.status === 'enabled',
-            tooltip,
-          }
-        },
-      },
+            })
+          },
+        ],
+        metas: [
+          (obj) => {
+            const notSupport = notSupportEnable.includes(obj.provider.toLowerCase())
+            const label = notSupport ? _.get(PROVIDER_MAP, `[${obj.provider}].label`) : ''
+            const tooltip = label ? `【${label}】暂不支持该操作` : ''
+            return {
+              validate: !notSupport && obj.status === 'disabled',
+              tooltip,
+            }
+          },
+          (obj) => {
+            const notSupport = notSupportDisable.includes(obj.provider.toLowerCase())
+            const label = notSupport ? _.get(PROVIDER_MAP, `[${obj.provider}].label`) : ''
+            const tooltip = label ? `【${label}】暂不支持该操作` : ''
+            return {
+              validate: !notSupport && obj.status === 'enabled',
+              tooltip,
+            }
+          },
+        ],
+      }),
       {
         label: '删除',
         permission: 'lb_loadbalancers_delete',
