503353e65123f667740552e4f60c6d174f09f68a
houjiazong <houjiazong@gmail.com>
2020-06-04 11:18:18 +0800

    feat: instance group add function introduction

--- views/instance-group/index.vue
+++ views/instance-group/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <page-header :title="$t('dictionary.instancegroup')" />
+    <a-alert :message="$t('compute.instance_group_desc')" type="info" class="mt-2" />
     <page-body>
       <instance-group-list :id="listId" />
     </page-body>
