b20915543e2733ea23e8947029c38fd5f54a29ea
曹赛琪 <caosaiqi@yunio>
2020-07-01 17:06:55 +0800

    添加系统引导路由配置

--- router/index.js
+++ router/index.js
@@ -3,6 +3,7 @@ import commonalertsCreate from '@Monitor/views/commonalert/create'
 import commonalertsUpdate from '@Monitor/views/commonalert/update'
 import Explorer from '@Monitor/views/explorer'
 import Layout from '@/layouts/RouterView'
+import { hasSetupKey } from '@/utils/auth'
 
 export default {
   meta: {
@@ -15,6 +16,7 @@ export default {
       meta: {
         label: 'Metrics Explorer',
         t: 'dictionary.explorer',
+        hidden: () => !hasSetupKey(['onestack', 'private', 'public', 'vmware']),
       },
       component: Layout,
       children: [
@@ -31,6 +33,7 @@ export default {
         label: '报警策略',
         t: 'dictionary.commonalert',
         permission: 'commonalerts_list',
+        hidden: () => !hasSetupKey(['onestack', 'private', 'public', 'vmware']),
       },
       component: Layout,
       children: [
