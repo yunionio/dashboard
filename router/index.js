11f9a5bf75c837c31f8003a2ce28c89c99ae5790
houjiazong <houjiazong@gmail.com>
2020-07-04 13:39:53 +0800

    improvement: catch router replace error

--- router/index.js
+++ router/index.js
@@ -8,8 +8,12 @@ import Router from 'vue-router'
 import routes from './routes'
 
 const originalPush = Router.prototype.push
+const originalReplace = Router.prototype.replace
 Router.prototype.push = function push (location) {
   return originalPush.call(this, location).catch(err => err)
+}
+Router.prototype.replace = function replace (location) {
+  return originalReplace.call(this, location).catch(err => err)
 }
 
 Vue.use(Router)
