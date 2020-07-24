970cd678ffabf88c013c1ee1ad3d4a73c5fc98ff
mj <gaomingjiu@yunion.cn>
2020-07-24 17:26:06 +0800

    fix #5386 共享的安全组不能新建、编辑规则

--- views/secgroup/components/List.vue
+++ views/secgroup/components/List.vue
@@ -97,7 +97,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['isAdminMode', 'isDomainMode', 'userInfo']),
+    ...mapGetters(['isAdminMode', 'isDomainMode', 'userInfo', 'isProjectMode']),
     groupActions () {
       const _frontGroupActions = this.frontGroupActions ? this.frontGroupActions.bind(this)() || [] : []
       return _frontGroupActions.concat(
@@ -238,9 +238,12 @@ export default {
         manager = null
       }
     },
-    openEditRulesDialog (obj) {
+    openEditRulesDialog (rule, row) {
+      if (this.isProjectMode && this.userInfo.projectId !== row.tenant_id) {
+        return
+      }
       this.createDialog('EditRulesDialog', {
-        data: [obj],
+        data: [rule],
         title: 'edit',
         columns: this.columns,
         refresh: () => {
--- views/secgroup/sidepage/InDirection.vue
+++ views/secgroup/sidepage/InDirection.vue
@@ -4,11 +4,12 @@
       message="提示：安全组未设置任何自定义放行规则，会导致无法访问云服务器端口，若需访问请添加安全组规则放行对应的端口"
       type="warning"
       style="margin-bottom: 20px" />
-    <direction-list :type="getParams.type" :id="getParams.id" />
+    <direction-list :type="getParams.type" :id="getParams.id" :isRead="isRead" />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import DirectionList from '../components/Direction'
 import WindowsMixin from '@/mixins/windows'
 
@@ -22,6 +23,17 @@ export default {
     getParams: {
       type: Object,
       required: true,
+    },
+    resId: String,
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapGetters(['isAdminMode', 'isDomainMode', 'isProjectMode', 'userInfo']),
+    isRead () {
+      return this.isProjectMode && this.userInfo.projectId !== this.data.tenant_id
     },
   },
 }
