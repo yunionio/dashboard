7b96d8ea9bf72920f099e49c41111dbdd3bbf592
houjiazong <houjiazong@gmail.com>
2020-07-28 11:05:18 +0800

    fix: props

--- extends/NumberCard/components/K8s.vue
+++ extends/NumberCard/components/K8s.vue
@@ -19,7 +19,7 @@
         <a-form-item :label="$t('dashboard.text_6')">
           <a-input v-decorator="decorators.name" />
         </a-form-item>
-        <k8s-config :fc="form.fc" :decorators="decorators" usage-:label="$t('dashboard.text_20')" @update:usage_key="setDefaultName" />
+        <k8s-config :fc="form.fc" :decorators="decorators" :usage-label="$t('dashboard.text_20')" @update:usage_key="setDefaultName" />
       </a-form>
     </base-drawer>
   </div>
