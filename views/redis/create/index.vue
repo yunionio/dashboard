acc461ba93538689889dac8da34f4aa0e2a76b5a
曹赛琪 <caosaiqi@yunio>
2020-07-29 19:24:13 +0800

    删除基础配置

--- views/redis/create/index.vue
+++ views/redis/create/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="redis-create-index">
     <page-header title="新建" />
-    <a-divider orientation="left">基础配置</a-divider>
     <a-form
       class="mt-3"
       :form="form.fc">
