58bd125eb686c0aef998a8540de086d148212c91
lengband <lengband@163.com>
2020-08-01 22:48:20 +0800

    fix [3.3] 宿主机回收为物理机 添加判断 is_baremetal && guests === 0

--- views/host/mixins/singleActions.js
+++ views/host/mixins/singleActions.js
@@ -202,7 +202,7 @@ export default {
                         }
                       }
                       return {
-                        validate: true,
+                        validate: obj.is_baremetal && obj.guests === 0,
                       }
                     },
                   },
