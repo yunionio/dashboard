9f43f6f58c124dc6f0f9a40703391df435fa2e80
侯佳宗 <houjiazong@yunionyun.com>
2020-08-05 15:43:31 +0800

    fix: i18n message

--- scope/layouts/Navbar/index.vue
+++ scope/layouts/Navbar/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar-wrap d-flex align-items-center" @click.stop.prevent="handleCloseSidebar">
     <template v-if="authInfoLoaded">
-      <a-tooltip :title="$t('common_209')" placement="right">
+      <a-tooltip title="导航菜单" placement="right">
         <div class="d-flex align-items-center navbar-item-trigger justify-content-center global-map-btn ml-1 flex-shrink-0 flex-grow-0" @click.stop.prevent="handleToggleSidebar">
           <icon type="menu" style="font-size: 24px;" />
         </div>
