c3d06f50c3351d4ba950b5267bec6754dc995e42
houjiazong <houjiazong@gmail.com>
2020-08-19 17:42:39 +0800

    feat: set default hidden columns

--- views/cloudaccount/components/List.vue
+++ views/cloudaccount/components/List.vue
@@ -67,6 +67,7 @@ export default {
           share_mode: getPublicFilter(),
           project_domains: getDomainFilter(),
         },
+        hiddenColumns: ['guest_count', 'host_count', 'enable_auto_sync', 'probe_at', 'last_auto_sync'],
       }),
       exportDataOptions: {
         items: [
