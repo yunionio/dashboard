c69d352adb1827103852f0c0aa83b220bb2f0a90
mj <gaomingjiu@yunion.cn>
2020-08-29 18:05:23 +0800

    feat 增加域名解析详情

---
+++ views/dns-zone/sidepage/Detail.vue
@@ -0,0 +1,42 @@
+<template>
+  <detail
+    :on-manager="onManager"
+    :data="data"
+    :base-info="baseInfo"
+    resource="dns_zones"
+    statusModule="dnszone"
+    :hiddenKeys="['tenant']"
+    :nameProps="{edit: false}" />
+</template>
+
+<script>
+import {
+  getZoneTypeTableColumns,
+  getVpcCountTableColumns,
+  getDnsRecordsetCountTableColumns,
+
+} from '../utils/columns'
+
+export default {
+  name: 'DnsZoneDetail',
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+    onManager: {
+      type: Function,
+      required: true,
+    },
+  },
+  data () {
+    return {
+      baseInfo: [
+        getZoneTypeTableColumns(),
+        getDnsRecordsetCountTableColumns(),
+        getVpcCountTableColumns(),
+      ],
+    }
+  },
+}
+</script>
