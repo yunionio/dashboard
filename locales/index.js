2f3bef59fb8064df521479bba4bd88962cb7e079
houjiazong <houjiazong@gmail.com>
2020-09-04 15:19:17 +0800

    feat: brand use i18n value

--- locales/index.js
+++ locales/index.js
@@ -5,17 +5,19 @@ import zhCN from './zh-CN'
 import helpEN from './help-en'
 import helpZhCN from './help-zh-CN'
 import { getLanguage } from '@/utils/common/cookie'
+import setting from '@/config/setting'
 
 Vue.use(VueI18n)
 
 const messages = {
-  en: Object.assign(en, helpEN),
-  'zh-CN': Object.assign(zhCN, helpZhCN),
+  en: Object.assign(en, helpEN, { brand: setting.brand.en }),
+  'zh-CN': Object.assign(zhCN, helpZhCN, { brand: setting.brand['zh-CN'] }),
 }
 
 const i18n = new VueI18n({
   locale: getLanguage(),
   messages,
+  fallbackLocale: 'zh-CN',
   // silentTranslationWarn: true,
 })
 
