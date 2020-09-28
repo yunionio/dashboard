38b2dafb257722d276328f72f9104cafa15e58ba
mj <gaomingjiu@yunion.cn>
2020-09-28 16:39:54 +0800

    feat 调整服务组件参数

--- views/kube-component/components/Monitor/index.vue
+++ views/kube-component/components/Monitor/index.vue
@@ -7,6 +7,9 @@
     <a-form-model-item :label="$t('k8s.text_274')" prop="grafana.adminPassword">
       <a-input-password v-model="formData.grafana.adminPassword" :placeholder="$t('k8s.text_275')" />
     </a-form-model-item>
+    <a-form-model-item :label="$t('k8s.text_405')" prop="grafana.publicAddress">
+      <a-input v-model="formData.grafana.publicAddress" addon-before="https://" addon-after="/grafana" :placeholder="$t('k8s.text_404')" />
+    </a-form-model-item>
     <storage v-model="formData.grafana.storage" prop="grafana.storage" />
     <a-divider content-position="left">Loki</a-divider>
     <storage v-model="formData.loki.storage" prop="loki.storage" />
@@ -23,6 +26,7 @@
 import * as R from 'ramda'
 import mixin from '../../mixins'
 import Storage from './Storage'
+import { validate } from '@/utils/validate'
 
 export default {
   name: 'KubeComponentMonitor',
@@ -31,10 +35,33 @@ export default {
   },
   mixins: [mixin],
   data () {
+    const { host } = window.location
+
+    const checkPublicAddress = (rule, value, callback) => {
+      const arr = value.split(':')
+      if (arr.length > 1) {
+        const ip = arr[0]
+        const port = arr[1]
+        if (validate(ip, 'IPv4') === false || validate(ip, 'IPv4').result === false ||
+          validate(port, 'ports') === false || validate(port, 'ports').result === false) {
+          callback(new Error(this.$t('k8s.text_406')))
+        } else {
+          callback()
+        }
+      } else {
+        if ((validate(value, 'IPv4') === false || validate(value, 'IPv4').result === false) &&
+          (validate(value, 'domain') === false || validate(value, 'domain').result === false)) {
+          callback(new Error(this.$t('k8s.text_406')))
+        } else {
+          callback()
+        }
+      }
+    }
     let formData = {
       grafana: {
         adminUser: 'admin',
         adminPassword: 'prom-operator',
+        publicAddress: host,
         storage: {
           enabled: false,
           sizeMB: 1,
@@ -69,6 +96,10 @@ export default {
         ],
         'grafana.adminPassword': [
           { required: true, message: this.$t('k8s.text_277'), trigger: 'blur' },
+        ],
+        'grafana.publicAddress': [
+          { required: true, message: this.$t('k8s.text_404'), trigger: 'blur' },
+          { validator: checkPublicAddress },
         ],
         'grafana.storage': [
           { required: true, message: this.$t('k8s.text_278'), trigger: 'blur' },
