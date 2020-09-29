a092a6cbbdd23664d462f969f34a2ef28ac18c85
houjiazong <houjiazong@gmail.com>
2020-09-29 14:41:38 +0800

    fix: add openstack filter

--- views/lb/constants/index.js
+++ views/lb/constants/index.js
@@ -1,5 +1,5 @@
 import i18n from '@/locales'
-export const surpportLb = ['onecloud', 'aliyun', 'qcloud', 'huawei', 'aws']
+export const surpportLb = ['onecloud', 'aliyun', 'qcloud', 'huawei', 'aws', 'openstack']
 
 export const CHARGE_TYPE = {
   postpaid: i18n.t('network.text_256'),
