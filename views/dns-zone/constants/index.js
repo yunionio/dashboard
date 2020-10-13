68510e9f13ef531020236817eca1063df186809e
mj <gaomingjiu@yunion.cn>
2020-10-13 11:26:21 +0800

    feat #2360 新建DNS解析时，解析域类型加下提示

--- views/dns-zone/constants/index.js
+++ views/dns-zone/constants/index.js
@@ -1,10 +1,14 @@
+import i18n from '@/locales'
+
 export const zoneTypes = [
   {
     label: 'PrivateZone',
     value: 'PrivateZone',
+    desc: i18n.t('network.text_738'),
   },
   {
     label: 'PublicZone',
     value: 'PublicZone',
+    desc: i18n.t('network.text_739'),
   },
 ]
