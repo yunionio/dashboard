c43645ef8da130d3a2840d69cbbb24a571aa8f62
lengband <lengband@163.com>
2020-10-31 17:50:55 +0800

    fix [3.5] complete getSignature

--- utils/crypto.js
+++ utils/crypto.js
@@ -2,5 +2,10 @@ import sha256 from 'crypto-js/sha256'
 import stringify from 'fast-json-stable-stringify'
 
 export const getSignature = data => {
-  return sha256(stringify(data)).toString()
+  if (!data.signature) {
+    return sha256(stringify(data)).toString()
+  } else {
+    console.error(`crypto: ${data} already has "signature"`)
+    return data
+  }
 }
