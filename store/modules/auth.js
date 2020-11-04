95163ec935b02a137db17a24e27e3eb324307ccb
houjiazong <houjiazong@gmail.com>
2020-11-04 18:33:07 +0800

    feat(3.6): sso 登录不进行记录

--- store/modules/auth.js
+++ store/modules/auth.js
@@ -135,6 +135,8 @@ export default {
     UPDATE_LOGGED_USERS (state, payload) {
       // 如果是cas登录则不保存信息
       if (state.info && state.info.idp_driver === 'cas') return
+      // 如果是sso登录则不保存信息
+      if (state.auth && state.auth.auth && state.auth.auth.is_sso) return
       const newVal = { ...state.loggedUsers }
       if (payload.action === 'delete') {
         delete newVal[payload.key]
