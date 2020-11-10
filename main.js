639b5c462f0419ce277363ff4dae8c3717028418
houjiazong <houjiazong@gmail.com>
2020-11-10 17:41:51 +0800

    feat(3.6): 剥离vue config

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import './config/vue.config'
 import Vue from 'vue'
 import Antd from 'ant-design-vue'
 import 'xe-utils'
@@ -22,9 +23,6 @@ import './directives'
 import './plugins'
 import './permission'
 import './filters'
-
-Vue.config.productionTip = false
-Vue.config.devtools = process.env.NODE_ENV === 'development'
 
 Vue.use(Antd)
 Vue.use(VXETable, {
