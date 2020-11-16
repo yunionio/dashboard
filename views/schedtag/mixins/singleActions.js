b77de49a31151419bac35a4db644b35cd013761a
mj <gaomingjiu@yunion.cn>
2020-11-16 17:05:21 +0800

    Feat 3.5 #2139 调度标签支持关联资源

--- views/schedtag/mixins/singleActions.js
+++ views/schedtag/mixins/singleActions.js
@@ -6,74 +6,92 @@ export default {
   created () {
     this.singleActions = [
       {
-        label: i18n.t('cloudenv.text_423'),
+        label: i18n.t('cloudenv.text_454'),
         action: obj => {
-          this.createDialog('SetOwnerDialog', {
+          this.createDialog('AssociatedResourcesDialog', {
             data: [obj],
             columns: this.columns,
-            title: i18n.t('cloudenv.text_423'),
-            tipName: i18n.t('cloudenv.text_18'),
-            onManager: this.onManager,
-            tipname: this.$t('dictionary.schedtag'),
-            action: i18n.t('cloudenv.text_423'),
-          })
-        },
-        meta: () => {
-          const ret = {
-            validate: true,
-            tooltip: null,
-          }
-          if (!this.isAdminMode) {
-            ret.validate = false
-          }
-          return ret
-        },
-      },
-      {
-        label: i18n.t('cloudenv.text_422'),
-        action: obj => {
-          this.createDialog('SetStrategyDialog', {
-            data: [obj],
-            columns: this.columns,
-            title: i18n.t('cloudenv.text_422'),
             onManager: this.onManager,
           })
         },
-        meta: () => {
-          const ret = {
-            validate: true,
-            tooltip: null,
-          }
-          if (!this.isAdminMode) {
-            ret.validate = false
-          }
-          return ret
-        },
       },
       {
-        label: i18n.t('cloudenv.text_108'),
-        action: obj => {
-          this.createDialog('DeleteResDialog', {
-            vm: this,
-            data: [obj],
-            columns: this.columns,
-            title: i18n.t('cloudenv.text_108'),
-            name: this.$t('dictionary.schedtag'),
-            onManager: this.onManager,
-            success: () => {
-              this.destroySidePages()
+        label: i18n.t('cloudenv.text_311'),
+        actions: obj => {
+          return [
+
+            {
+              label: i18n.t('cloudenv.text_423'),
+              action: obj => {
+                this.createDialog('SetOwnerDialog', {
+                  data: [obj],
+                  columns: this.columns,
+                  title: i18n.t('cloudenv.text_423'),
+                  tipName: i18n.t('cloudenv.text_18'),
+                  onManager: this.onManager,
+                  tipname: this.$t('dictionary.schedtag'),
+                  action: i18n.t('cloudenv.text_423'),
+                })
+              },
+              meta: () => {
+                const ret = {
+                  validate: true,
+                  tooltip: null,
+                }
+                if (!this.isAdminMode) {
+                  ret.validate = false
+                }
+                return ret
+              },
             },
-          })
-        },
-        meta: obj => {
-          const ret = {
-            validate: obj.can_delete,
-            tooltip: null,
-          }
-          if (!this.isAdminMode) {
-            ret.validate = false
-          }
-          return ret
+            {
+              label: i18n.t('cloudenv.text_422'),
+              action: obj => {
+                this.createDialog('SetStrategyDialog', {
+                  data: [obj],
+                  columns: this.columns,
+                  title: i18n.t('cloudenv.text_422'),
+                  onManager: this.onManager,
+                })
+              },
+              meta: () => {
+                const ret = {
+                  validate: true,
+                  tooltip: null,
+                }
+                if (!this.isAdminMode) {
+                  ret.validate = false
+                }
+                return ret
+              },
+            },
+            {
+              label: i18n.t('cloudenv.text_108'),
+              action: obj => {
+                this.createDialog('DeleteResDialog', {
+                  vm: this,
+                  data: [obj],
+                  columns: this.columns,
+                  title: i18n.t('cloudenv.text_108'),
+                  name: this.$t('dictionary.schedtag'),
+                  onManager: this.onManager,
+                  success: () => {
+                    this.destroySidePages()
+                  },
+                })
+              },
+              meta: obj => {
+                const ret = {
+                  validate: obj.can_delete,
+                  tooltip: null,
+                }
+                if (!this.isAdminMode) {
+                  ret.validate = false
+                }
+                return ret
+              },
+            },
+          ]
         },
       },
     ]
