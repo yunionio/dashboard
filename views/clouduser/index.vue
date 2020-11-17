075205a260e43a44b68a8407c0177a8403186e85
houjiazong <houjiazong@gmail.com>
2020-11-17 17:10:15 +0800

    feat(3.5): saml user

--- views/clouduser/index.vue
+++ views/clouduser/index.vue
@@ -1,128 +1,92 @@
 <template>
   <div>
-    <page-header :title="$t('common_325')" />
-    <page-body>
-      <page-list
-        :list="list"
-        :columns="columns"
-        :single-actions="singleActions"
-        :export-data-options="exportDataOptions" />
-    </page-body>
+    <template v-if="!loaded">
+      <p class="text-center"><a-spin /></p>
+    </template>
+    <template v-else>
+      <template v-if="tabs.length === 0">
+        <p class="text-center">{{ $t('common.notData') }}</p>
+      </template>
+      <template v-else>
+        <page-header :title="$t('common_325')" :tabs="tabs" :current-tab.sync="currentTab" />
+        <page-body>
+          <component :is="currentTab" />
+        </page-body>
+      </template>
+    </template>
   </div>
 </template>
 
 <script>
-// import * as R from 'ramda'
-import PasswordFetcher from '@Compute/sections/PasswordFetcher'
-import { getBrandTableColumn } from '@/utils/common/tableColumn'
-// import { getBrandTableColumn, getNameDescriptionTableColumn } from '@/utils/common/tableColumn'
-import { getNameFilter, getBrandFilter } from '@/utils/common/tableFilter'
-import WindowsMixin from '@/mixins/windows'
-import ListMixin from '@/mixins/list'
+import ClouduserList from './components/ClouduserList'
+import SamluserList from './components/SamluserList'
 
 export default {
-  name: 'ClouduserForUser',
-  mixins: [WindowsMixin, ListMixin],
+  name: 'User',
+  components: {
+    ClouduserList,
+    SamluserList,
+  },
   data () {
     return {
-      list: this.$list.createList(this, {
-        id: 'ClouduserForUser',
-        resource: 'cloudusers',
-        apiVersion: 'v1',
-        getParams: {
-          user: this.$store.getters.userInfo.id,
-        },
-        filterOptions: {
-          name: getNameFilter(),
-          provider: getBrandFilter('cloud_id_brands'),
-        },
-      }),
-      exportDataOptions: {
-        items: [
-          { label: 'ID', key: 'id' },
-          { label: this.$t('table.title.name'), key: 'name' },
-          { label: this.$t('table.title.brand'), key: 'brand' },
-        ],
-      },
-      singleActions: [
-        {
-          label: this.$t('common_326'),
-          action: (obj) => {
-            this.createDialog('CloudgroupListForClouduserForUserDialog', {
-              data: [obj],
-            })
-          },
-        },
-      ],
-      columns: [
-        {
-          field: 'name',
-          title: this.$t('common_327'),
-          showOverflow: 'title',
-        },
-        {
-          field: 'password',
-          title: this.$t('common_328'),
-          width: 50,
-          slots: {
-            default: ({ row }) => {
-              return [<PasswordFetcher serverId={ row.id } resourceType='cloudusers' />]
-            },
-          },
-        },
-        getBrandTableColumn(),
-        {
-          field: 'iam_login_url',
-          title: this.$t('common_329'),
-          showOverflow: 'title',
-          slots: {
-            default: ({ row }) => {
-              if (!row.iam_login_url) return '-'
-              return [<help-link href={ row.iam_login_url } />]
-            },
-          },
-        },
-        {
-          field: 'cloudgroups',
-          title: this.$t('common_460', [this.$t('dictionary.cloudgroup')]),
-          // type: 'expand',
-          slots: {
-            default: ({ row }) => {
-              return this.$t('common_323', [(row.cloudgroups && row.cloudgroups.length) || 0])
-            },
-            // content: ({ row }) => {
-            //   if (R.isNil(row.cloudgroups) || R.isEmpty(row.cloudgroups)) return this.$t('common_708', [this.$t('dictionary.cloudgroup')])
-            //   return [
-            //     <vxe-grid
-            //       showOverflow='title'
-            //       data={ row.cloudgroups }
-            //       columns={[
-            //         getNameDescriptionTableColumn({
-            //           onManager: this.onManager,
-            //           hideField: true,
-            //           showDesc: false,
-            //           edit: false,
-            //           slotCallback: row => {
-            //             return (
-            //               <side-page-trigger permission='cloudgroup_get' name='CloudgroupSidePage' id={row.id} vm={this}>{ row.name }</side-page-trigger>
-            //             )
-            //           },
-            //         }),
-            //         {
-            //           field: 'id',
-            //           title: 'ID',
-            //           formatter: ({ cellValue }) => cellValue || '-',
-            //         },
-            //       ]} />,
-            //   ]
-            // },
-          },
-        },
-      ],
+      loaded: false,
+      tabs: [],
+      currentTab: '',
     }
   },
   created () {
-    this.list.fetchData()
+    this.genTabInfos()
+  },
+  methods: {
+    async genTabInfos () {
+      try {
+        const [cloudusers, samlusers] = await Promise.all([this.fetchCloudusers(), this.fetchSamlusers()])
+        if (cloudusers.length > 0) {
+          this.tabs.push({ key: 'clouduser-list', label: this.$t('personal_user_tab.cloud') })
+        }
+        if (samlusers.length > 0) {
+          this.tabs.push({ key: 'samluser-list', label: this.$t('personal_user_tab.saml') })
+        }
+        if (this.tabs.length > 0) {
+          this.currentTab = this.tabs[0].key
+        }
+        this.loaded = true
+      } catch (error) {
+        throw error
+      }
+    },
+    async fetchCloudusers () {
+      let manager = new this.$Manager('cloudusers', 'v1')
+      try {
+        const response = await manager.list({
+          params: {
+            user: this.$store.getters.userInfo.id,
+            scope: this.$store.getters.scope,
+            limit: 1,
+          },
+        })
+        const data = response.data.data || []
+        return data
+      } finally {
+        manager = null
+      }
+    },
+    async fetchSamlusers () {
+      let manager = new this.$Manager('samlusers', 'v1')
+      try {
+        const response = await manager.list({
+          params: {
+            user: this.$store.getters.userInfo.id,
+            scope: this.$store.getters.scope,
+            limit: 1,
+          },
+        })
+        const data = response.data.data || []
+        return data
+      } finally {
+        manager = null
+      }
+    },
   },
 }
 </script>
