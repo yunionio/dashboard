df5dc961c2975b17e8fb8cf7b47c2081f28072f0
houjiazong <houjiazong@gmail.com>
2020-11-23 15:29:20 +0800

    build(3.6/script): add k8s, db, helm module

--- scripts/sync-modules.sh
+++ scripts/sync-modules.sh
@@ -31,9 +31,9 @@ sync 'dashboard-module-compute' ../containers/Compute
 sync 'dashboard-module-dashboard' ../containers/Dashboard
 sync 'dashboard-module-network' ../containers/Network
 sync 'dashboard-module-storage' ../containers/Storage
-# sync 'dashboard-module-k8s' ../containers/K8S
-# sync 'dashboard-module-helm' ../containers/Helm
-# sync 'dashboard-module-db' ../containers/DB
+sync 'dashboard-module-k8s' ../containers/K8S
+sync 'dashboard-module-helm' ../containers/Helm
+sync 'dashboard-module-db' ../containers/DB
 sync 'dashboard-module-monitor' ../containers/Monitor
 
 exit 0
