9e7b6ce0173d8a3e83c799b37bd55a8a704c0d04
郝程宇 <haochengyu@kuaifawu.com>
2020-12-10 10:11:23 +0800

    feat(3.5/sku): 套餐支持公有云筛选

--- views/sku/mixins/columns.js
+++ views/sku/mixins/columns.js
@@ -2,6 +2,7 @@ import {
   getEnabledTableColumn,
   getNameDescriptionTableColumn,
   getStatusTableColumn,
+  getBrandTableColumn,
 } from '@/utils/common/tableColumn'
 import { sizestr } from '@/utils/utils'
 import i18n from '@/locales'
@@ -19,6 +20,7 @@ export default {
           )
         },
       }),
+      getBrandTableColumn({ field: 'provider' }),
       {
         field: 'cpu_core_count',
         title: i18n.t('compute.text_1051'),
