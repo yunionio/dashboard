616a3e0b0f71b1743e8b5fedb9a442fd66177b5f
mj <gaomingjiu@yunion.cn>
2021-03-30 20:43:23 +0800

    feat: add logout socket

--- utils/notificationListener/Client.js
+++ utils/notificationListener/Client.js
@@ -6,7 +6,9 @@
  * @class Clinet
  */
 import io from 'socket.io-client'
-import { message } from 'ant-design-vue'
+import { message, notification } from 'ant-design-vue'
+import router from '@/router'
+import { genReferRouteQuery } from '@/utils/utils'
 
 class Client {
   constructor (store, options = {}) {
@@ -35,6 +37,25 @@ class Client {
 
     this.socket.on('message', (payload) => {
       try {
+        if (payload === 'wslogout') {
+          this.socket.close()
+          store.dispatch('auth/logout').then(() => {
+            notification.warning({
+              message: '提示',
+              description: '当前用户已在其它设备登录，登录后从其它设备退出登录',
+            })
+            if (!router.currentRoute.meta.authPage) {
+              router.push({
+                path: '/auth/login',
+                query: genReferRouteQuery(router.currentRoute),
+              })
+            }
+          }).catch((error) => {
+            console.log('logout error: ' + error)
+          })
+          return
+        }
+
         const obj = JSON.parse(payload)
         if (!obj.success || !obj.action || obj.ignore_alert) {
           return
