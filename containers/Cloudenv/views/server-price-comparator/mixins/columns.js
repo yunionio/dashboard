23c66a2056eef7bfbe90e79bfe495d1fcb9dd279
tb365 <tangbin@yunion.cn>
2021-05-21 15:45:10 +0800

    add month & year price

--- containers/Cloudenv/views/server-price-comparator/mixins/columns.js
+++ containers/Cloudenv/views/server-price-comparator/mixins/columns.js
@@ -6,6 +6,25 @@ import {
 import i18n from '@/locales'
 import { BUY_DURATIONS_OPTIONS } from '@Compute/constants'
 
+const parseDuration = (s) => {
+  const m = /(\d)+(M)/
+  const y = /(\d)+(Y)/
+  let duration = 1
+  if (m.exec(s)) {
+    duration = parseInt(m.exec(s)[1])
+  } else if (y.exec(s)) {
+    duration = parseInt(y.exec(s)[1]) * 12
+  }
+  return duration
+}
+
+const parseFee = (fee) => {
+  const m = new RegExp(/(\D)(\d*\.?\d*)/).exec(fee)
+  const currency = m && m[1] ? m[1] : ''
+  const price = m && m[2] ? parseFloat(m[2]) : 0
+  return [currency, price]
+}
+
 export default {
   created () {
     const columns = [
@@ -100,6 +119,36 @@ export default {
           default: ({ row }, h) => {
             return [
               <div style={{ color: '#f5222d', fontSize: '14px' }}>{ row.fee }</div>,
+            ]
+          },
+        },
+      },
+      {
+        field: 'fee',
+        title: i18n.t('cloudenv.month_expense'),
+        minWidth: 50,
+        slots: {
+          default: ({ row }, h) => {
+            const d = parseDuration(row.duration)
+            const [c, p] = parseFee(row.fee)
+            const fee = row.billing_type === 'postpaid' ? p * 24 * 30 : p / d
+            return [
+              <div style={{ color: '#f5222d', fontSize: '14px' }}>{ `${c}${fee.toFixed(2)}` }</div>,
+            ]
+          },
+        },
+      },
+      {
+        field: 'fee',
+        title: i18n.t('cloudenv.year_expense'),
+        minWidth: 50,
+        slots: {
+          default: ({ row }, h) => {
+            const d = parseDuration(row.duration)
+            const [c, p] = parseFee(row.fee)
+            const fee = row.billing_type === 'postpaid' ? p * 24 * 30 * 12 : p * 12 / d
+            return [
+              <div style={{ color: '#f5222d', fontSize: '14px' }}>{ `${c}${fee.toFixed(2)}` }</div>,
             ]
           },
         },
