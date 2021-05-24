4aae932cde23e248bd84a5efa0fa51fe74763d30
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2021-05-24 19:52:20 +0800

    feat(Compute): vm transfer add forcast

--- containers/Compute/views/vminstance/dialogs/Transfer.vue
+++ containers/Compute/views/vminstance/dialogs/Transfer.vue
@@ -5,29 +5,44 @@
       <dialog-selected-tips :name="$t('dictionary.server')" :count="params.data.length" :action="$t('compute.text_1127')" />
       <dialog-table :data="params.data" :columns="params.columns.slice(0, 3)" />
       <a-form :form="form.fc" hideRequiredMark v-bind="formItemLayout">
-        <a-form-item :label="$t('compute.text_111')" :extra="$t('compute.text_1164')">
+        <!-- 强制迁移 -->
+        <a-form-item :label="$t('compute.text_1261')" v-if="isSingle" :extra="$t('compute.text_1262')">
+          <a-switch
+            :checkedChildren="$t('compute.text_115')"
+            :unCheckedChildren="$t('compute.text_116')"
+            v-decorator="decorators.rescue_mode"
+            @change="rescueModeChangeHandle" />
+        </a-form-item>
+        <!-- 自动启动 -->
+        <a-form-item :label="$t('compute.text_494')" v-if="isSingle && firstData.status === 'ready'" :extra="$t('compute.text_1263')">
+          <a-switch
+            :checkedChildren="$t('compute.text_115')"
+            :unCheckedChildren="$t('compute.text_116')"
+            v-decorator="decorators.auto_start" />
+        </a-form-item>
+        <!-- 跳过CPU检查 -->
+        <a-form-item :label="$t('compute.live_migrate.skip_cpu_check')" v-if="isSingle && firstData.status === 'running'" :extra="$t('compute.live_migrate.skip_cpu_check.explain')">
+          <a-switch
+            :checkedChildren="$t('compute.text_115')"
+            :unCheckedChildren="$t('compute.text_116')"
+            v-decorator="decorators.skip_cpu_check"
+            @change="skipCpuCheckChangeHandle" />
+        </a-form-item>
+        <a-form-item
+          :label="$t('compute.text_111')"
+          :validate-status="hostValidateStatus"
+          :help="hostValidateMsg">
           <base-select
             v-decorator="decorators.host"
-            resource="hosts"
             remote
             :remote-fn="q => ({ filter: `name.contains(${q})` })"
-            :params="hostsParams"
-            :mapper="hostsMapper"
+            :options="hostsOptions"
             :select-props="{ allowClear: true, placeholder: $t('compute.text_314') }" />
-        </a-form-item>
-        <a-form-item :label="$t('compute.text_1261')" v-if="isSingle" :extra="$t('compute.text_1262')">
-          <a-switch :checkedChildren="$t('compute.text_115')" :unCheckedChildren="$t('compute.text_116')" v-decorator="decorators.rescue_mode" />
-        </a-form-item>
-        <a-form-item :label="$t('compute.text_494')" v-if="isSingle && firstData.status === 'ready'" :extra="$t('compute.text_1263')">
-          <a-switch :checkedChildren="$t('compute.text_115')" :unCheckedChildren="$t('compute.text_116')" v-decorator="decorators.auto_start" />
-        </a-form-item>
-        <a-form-item :label="$t('compute.live_migrate.skip_cpu_check')" v-if="isSingle && firstData.status === 'running'" :extra="$t('compute.live_migrate.skip_cpu_check.explain')">
-          <a-switch :checkedChildren="$t('compute.text_115')" :unCheckedChildren="$t('compute.text_116')" v-decorator="decorators.skip_cpu_check" />
         </a-form-item>
       </a-form>
     </div>
     <div slot="footer">
-      <a-button type="primary" @click="handleConfirm" :loading="loading">{{ $t('dialog.ok') }}</a-button>
+      <a-button type="primary" @click="handleConfirm" :loading="loading" :disabled="handleConfirmDisabled">{{ $t('dialog.ok') }}</a-button>
       <a-button @click="cancelDialog">{{ $t('dialog.cancel') }}</a-button>
     </div>
   </base-dialog>
@@ -47,6 +62,8 @@ export default {
       form: {
         fc: this.$form.createForm(this),
       },
+      forcastData: null,
+      hosts: [],
       decorators: {
         host: [
           'host',
@@ -111,14 +128,41 @@ export default {
       }
       return ret
     },
+    hostsOptions () {
+      const hostIds = this.forcastData?.filtered_candidates?.map(v => v.id) || []
+      if (this.forcastData?.can_create === false) {
+        return []
+      }
+      return this.hosts.filter(v => {
+        return !hostIds.includes(v.id) && v.id !== this.firstData.host_id
+      }).map(v => {
+        return {
+          key: v.id,
+          label: v.name,
+        }
+      })
+    },
+    hostValidateStatus () {
+      if (this.forcastData && this.hostsOptions?.length === 0) {
+        return 'error'
+      }
+      return 'success'
+    },
+    hostValidateMsg () {
+      if (this.forcastData && this.hostsOptions?.length === 0) {
+        return this.$t('compute.transfer_host')
+      }
+      return this.$t('compute.text_1384')
+    },
+    handleConfirmDisabled () {
+      return this.forcastData && this.hostsOptions?.length === 0
+    },
+  },
+  created () {
+    this.isSingle && this.queryForcastData()
+    this.queryHosts()
   },
   methods: {
-    hostsMapper (data) {
-      if (this.isSingle) {
-        data = data.filter(item => item.id !== this.firstData.host_id)
-      }
-      return data
-    },
     doSingleTransfer (ids, values) {
       let action = 'migrate'
       const data = {
@@ -176,6 +220,52 @@ export default {
       } finally {
         this.loading = false
       }
+    },
+    doForecast (live_migrate = true, skip_cpu_check = false, prefer_host_id) {
+      const manager = new this.$Manager('servers')
+      const params = {
+        live_migrate,
+        skip_cpu_check,
+      }
+      if (prefer_host_id) {
+        params.prefer_host_id = prefer_host_id
+      }
+      return manager.performAction({
+        id: this.params.data[0].id,
+        action: 'migrate-forecast',
+        data: params,
+      })
+    },
+    queryForcastData (skip_cpu_check, prefer_host_id) {
+      const live_migrate = this.firstData.status === 'running'
+      this.doForecast(live_migrate, skip_cpu_check, prefer_host_id).then((res) => {
+        this.forcastData = res.data
+      }).catch((err) => {
+        console.log(err)
+        throw err
+      })
+    },
+    rescueModeChangeHandle (v) {
+      this.form.fc.setFieldsValue({ host: '' })
+      if (v) {
+        this.forcastData = null
+      } else {
+        this.queryForcastData(false)
+      }
+    },
+    skipCpuCheckChangeHandle (v) {
+      const rescue_mode = this.form.fc.getFieldValue('rescue_mode')
+      if (rescue_mode) return
+      this.queryForcastData(v)
+    },
+    queryHosts () {
+      const hostsManager = new this.$Manager('hosts')
+      hostsManager.list({ params: this.hostsParams }).then((res) => {
+        this.hosts = res.data.data || []
+      }).catch((err) => {
+        console.log(err)
+        throw err
+      })
     },
   },
 }
