50b97b87ab113d405d92ec9284710fa18982b3b5
tb365 <tangbin@yunion.cn>
2021-05-25 15:01:14 +0800

    common alert add default chart name

--- containers/Monitor/views/commonalert/components/alert/form/index.vue
+++ containers/Monitor/views/commonalert/components/alert/form/index.vue
@@ -691,7 +691,12 @@ export default {
       this.$refs.filtersRef.reset()
     },
     getTitle (mertricItem) {
-      let label = this.metricKeyItem.label || '-'
+      let padding = ' '
+      if (this.$store.getters.setting.language === 'zh-CN') {
+        padding = ''
+      }
+      let label = this.metricKeyItem && this.metricKeyItem.metric_res_type ? this.$t(`dictionary.${this.metricKeyItem.metric_res_type}`) + padding : ''
+      label += this.metricKeyItem && this.metricKeyItem.label ? this.metricKeyItem.label : '-'
       const metricLabel = _.get(mertricItem, 'description.display_name')
       if (metricLabel) {
         label += `(${metric_zh[metricLabel] ? metric_zh[metricLabel] : metricLabel})`
