3f2251453f4758ac301429be602fcfd22771fe3c
tb365 <tangbin@yunion.cn>
2021-07-21 11:38:51 +0800

    import vue config from scope dictionary

--- src/config/vue.config.js
+++ src/config/vue.config.js
@@ -2,3 +2,7 @@ import Vue from 'vue'
 
 Vue.config.productionTip = false
 Vue.config.devtools = process.env.NODE_ENV === 'development'
+
+// import scope vue全局配置
+const scopeConfig = require.context('../scope', false, /.\/vue.scope.config.js/)
+scopeConfig.keys().forEach(scopeConfig)
