87b4f765d0f609348a79765b89a2ce42cde23254
mj <gaomingjiu@yunion.cn>
2021-07-27 15:07:47 +0800

    feat: 优化下拉选择框在内容过多时无法有效查看信息

--- src/components/SearchBox/AutoCompleter.vue
+++ src/components/SearchBox/AutoCompleter.vue
@@ -26,10 +26,10 @@
               :key="item.key">
               <span>
                 <a-checkbox
-                  class="w-100 text-truncate"
+                  class="w-100"
                   :checked="selectValue && selectValue.includes(item.key)"
                   :value="item.key"
-                  @change="handleValueChange">{{ item.label }}</a-checkbox>
+                  @change="handleValueChange"><span class="text-wrap text-break" :title="item.label">{{ item.label }}</span></a-checkbox>
               </span>
             </li>
           </template>
@@ -415,10 +415,9 @@ export default {
   padding: 0;
   > li {
     > span {
-      line-height: 30px;
-      height: 30px;
+      // height: 30px;
       display: block;
-      padding: 0 10px;
+      padding: 6px 10px;
       cursor: pointer;
       &.empty {
         cursor: default;
