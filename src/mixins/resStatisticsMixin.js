3380b684bddfd2e2fff63ed588fbe6f6a6cc634f
mj <gaomingjiu@yunion.cn>
2021-08-05 15:54:51 +0800

    feat: 支持数据自定义处理

--- src/mixins/resStatisticsMixin.js
+++ src/mixins/resStatisticsMixin.js
@@ -1,3 +1,4 @@
+import * as R from 'ramda'
 import ResStatusTab from '@/sections/ResStatusTab'
 
 export default {
@@ -20,40 +21,46 @@ export default {
     this.resStaticsManager = null
   },
   methods: {
-    fetchResStatistics (params) {
+    fetchResStatistics (params, callback) {
       this.errorFilterStatus = []
       this.otherFilterStatus = []
       this.resStaticsManager.list({ params }).then(res => {
-        const { ready = {}, running = {} } = res.data
-        // 统计
-        let total = 0
-        let error = 0
-        let other = 0
-        for (const k in res.data) {
-          total += res.data[k].total_count
-          if (new RegExp('fail').test(k)) {
-            this.errorFilterStatus.push(k)
-            error += res.data[k].total_count
-          } else {
-            if (!['running', 'ready'].includes(k)) {
-              this.otherFilterStatus.push(k)
-              other += res.data[k].total_count
-            }
-          }
-        }
-        const statusOpts = [
-          { title: this.$t('compute.text_576'), type: 'total', num: total },
-          { title: this.$t('compute.text_574'), type: 'running', num: running?.total_count || 0 },
-          { title: this.$t('compute.text_273'), type: 'ready', num: ready?.total_count || 0 },
-          { title: this.$t('common_623', [this.$t('scope.text_61')]), type: 'error', num: error },
-          { title: this.$t('compute.text_674'), type: 'other', num: other },
-        ]
-        this.statusOpts = statusOpts
+        this.statusOpts = R.is(Function, callback) ? callback(res.data) : this.getStatusOpts(res.data)
         this.statusArr = Object.keys(res.data)
       }).catch(err => {
         console.error(err)
         this.statusOpts = []
       })
+    },
+    getStatusOpts (data) {
+      const { ready = {}, running = {} } = data
+
+      // 统计
+      let total = 0
+      let error = 0
+      let other = 0
+
+      for (const k in data) {
+        total += data[k].total_count
+        if (new RegExp('fail').test(k)) {
+          this.errorFilterStatus.push(k)
+          error += data[k].total_count
+        } else {
+          if (!['running', 'ready'].includes(k)) {
+            this.otherFilterStatus.push(k)
+            other += data[k].total_count
+          }
+        }
+      }
+
+      const statusOpts = [
+        { title: this.$t('compute.text_576'), type: 'total', num: total },
+        { title: this.$t('compute.text_574'), type: 'running', num: running?.total_count || 0 },
+        { title: this.$t('compute.text_273'), type: 'ready', num: ready?.total_count || 0 },
+        { title: this.$t('common_623', [this.$t('scope.text_61')]), type: 'error', num: error },
+        { title: this.$t('compute.text_674'), type: 'other', num: other },
+      ]
+      return statusOpts
     },
     statusClickHandle (obj) {
       let statusCheckArr = []
@@ -71,7 +78,6 @@ export default {
           default:
             statusCheckArr = [obj.type]
         }
-        console.log(obj)
         this.filterParams = {
           statusCheckArr,
           statusArr: this.statusArr,
