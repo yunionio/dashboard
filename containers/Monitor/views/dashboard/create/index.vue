702dd3d33f1fb5885d422c4f9c201b8e35d1900e
tb365 <tangbin@yunion.cn>
2021-08-19 17:29:21 +0800

    monitor query data add limit offset

--- containers/Monitor/views/dashboard/create/index.vue
+++ containers/Monitor/views/dashboard/create/index.vue
@@ -33,11 +33,13 @@
           <div v-for="(item, i) in seriesList" :key="i">
             <monitor-line
               class="mb-3"
+              :pager="seriesListPager[i]"
               :loading="loadingList[i]"
               :description="seriesDescription[i]"
               :metricInfo="metricList[i][0]"
               :series="item"
               :timeFormatStr="timeFormatStr"
+              @pageChange="pageChange"
               @chartInstance="setChartInstance" />
           </div>
           <a-card v-if="!seriesList.length && loadingList[0]"
@@ -103,6 +105,7 @@ export default {
       timeOpts,
       metricList: [],
       seriesList: [],
+      seriesListPager: [],
       chartInstanceList: [], // e-chart 实例
       loadingList: [],
       seriesDescription: [],
@@ -208,24 +211,24 @@ export default {
       for (let i = 0; i < this.metricList.length; i++) {
         const metric_query = this.metricList[i]
         this.loadingList.push(true)
-        jobs.push(this.fetchData(metric_query))
+        jobs.push(this.fetchData(metric_query, 10, 0))
       }
       try {
         const res = await Promise.all(jobs)
         this.seriesList = res.map(val => get(val, 'series') || [])
+        this.seriesListPager = res.map((val, index) => ({ seriesIndex: index, total: get(val, 'series_total') || 0, page: 1, limit: 10 }))
         this.loadingList = this.loadingList.map(v => false)
       } catch (error) {
         this.loadingList = this.loadingList.map(v => false)
         throw error
       }
     },
-    async refresh (params, i) { // 将多个查询 分开调用
+    async _refresh (i, limit, offset) {
       try {
-        const metric_query = [{ model: params }]
-        this.$set(this.metricList, i, metric_query)
         this.$set(this.loadingList, i, true)
-        const { series = [] } = await this.fetchData(metric_query)
+        const { series = [], series_total = 0 } = await this.fetchData(this.metricList[i], limit, offset)
         this.$set(this.seriesList, i, series)
+        this.$set(this.seriesListPager, i, { seriesIndex: i, total: series_total, page: 1 + offset / limit, limit: limit })
         this.loadingList[i] = false
       } catch (error) {
         this.$set(this.seriesList, i, [])
@@ -233,14 +236,23 @@ export default {
         throw error
       }
     },
-    async fetchData (metric_query) {
+    async refresh (params, i) { // 将多个查询 分开调用
+      const metric_query = [{ model: params }]
+      this.$set(this.metricList, i, metric_query)
+      await this._refresh(i, 10, 0)
+    },
+    async pageChange (pager) {
+      await this._refresh(pager.seriesIndex, pager.limit, (pager.page - 1) * pager.limit)
+    },
+    async fetchData (metric_query, limit, offset) {
       try {
         const data = {
           metric_query,
           interval: this.timeGroup,
           scope: this.$store.getters.scope,
+          slimit: limit,
+          soffset: offset,
           ...this.timeRangeParams,
-          ...this.extraParams,
         }
         if (!data.metric_query || !data.metric_query.length || !data.from) return
         data.signature = getSignature(data)
