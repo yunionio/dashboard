805fbf4e8b624c9644a7c1413f1b575ea6dab853
mj <gaomingjiu@yunion.cn>
2021-08-31 15:45:59 +0800

    fix: 渠道下放到域预算告警机器人改为下拉列表

--- src/sections/NotifyTypes/index.vue
+++ src/sections/NotifyTypes/index.vue
@@ -1,13 +1,17 @@
 <template>
   <a-form-item :label="label" :extra="extraStr">
-    <a-checkbox-group name="checkboxgroup" :options="channelOpts" v-decorator="decorator" />
+    <base-select
+      v-decorator="decorator"
+      class="w-100"
+      :filterable="true"
+      resource="robots"
+      version="v1"
+      :select-props="{ placeholder: $t('bill.budget_alert_user_placeholder'), allowClear: true, mode: 'multiple' }" />
   </a-form-item>
 </template>
 
 <script>
-import * as R from 'ramda'
 import i18n from '@/locales'
-import { channelMaps } from '@/constants'
 
 export default {
   name: 'NotifyTypes',
@@ -25,59 +29,6 @@ export default {
     label: {
       type: String,
       default: i18n.t('monitor.channel'),
-    },
-    showAllRobot: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data () {
-    return {
-      channelOpts: [],
-      extraStr: '',
-    }
-  },
-  created () {
-    this.fetchData()
-  },
-  methods: {
-    async fetchData () {
-      try {
-        const { data: { types = [] } } = await new this.$Manager('receivers', 'v1').performClassAction({ action: 'get-types', data: this.getParams })
-        const channelOpts = types.map(val => {
-          const channel = channelMaps[val]
-          let label = channel ? channel.label : val
-          if (label === 'webconsole') label = this.$t('dictionary.webconsole')
-          const item = {
-            label,
-            value: val,
-            sort: channel.sort ? channel.sort : 99,
-          }
-          if (val === 'webconsole') item.disabled = true
-          return item
-        })
-        if (this.showAllRobot) {
-          R.forEachObjIndexed((value, key) => {
-            if (!types.includes(key) && key.includes('robot')) {
-              const item = {
-                label: value.label,
-                value: value.value,
-                sort: value.sort,
-                disabled: true,
-              }
-              channelOpts.push(item)
-            }
-          }, channelMaps)
-        }
-        this.channelOpts = channelOpts.sort((a, b) => b.sort - a.sort)
-        const disableRobotArr = channelOpts.filter(val => val.disabled).map(val => val.label)
-        if (disableRobotArr.length > 0) {
-          this.extraStr = this.$t('common_657', [disableRobotArr.join('、')])
-        }
-        this.$emit('channelOptsChange', this.channelOpts)
-      } catch (error) {
-        throw error
-      }
     },
   },
 }
