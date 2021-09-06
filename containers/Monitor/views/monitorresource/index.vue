0fe24823a42743c5be2ce0103fca215ee68fbaaf
tb365 <tangbin@yunion.cn>
2021-09-06 11:08:03 +0800

    add monitor resource page

---
+++ containers/Monitor/views/monitorresource/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <page-header :title="title" />
+    <page-body>
+      <list listId="MonitorresourcesList" :res_type="res_type" :key="res_type" />
+    </page-body>
+  </div>
+</template>
+
+<script>
+import List from './components/List'
+
+export default {
+  name: 'MonitorresourcesIndex',
+  components: {
+    List,
+  },
+  props: {
+    res_type: String,
+  },
+  computed: {
+    title () {
+      return this.res_type === 'guest' ? this.$t('common.server') : this.$t('dictionary.host')
+    },
+  },
+}
+</script>
