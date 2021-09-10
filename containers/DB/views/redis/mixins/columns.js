cdaa00591f08634f14d1eb6e63e1b4f4d4b569c2
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2021-09-10 16:00:25 +0800

    fix: #7755 redis列表，IP复制icon不应该一直显示

--- containers/DB/views/redis/mixins/columns.js
+++ containers/DB/views/redis/mixins/columns.js
@@ -77,14 +77,11 @@ export default {
               if (!value) {
                 return null
               }
-              return (
-                <div class="d-flex align-items-center">
-                  <span class="text-truncate">
-                    {title}：{value}
-                  </span>
-                  <copy message={value} />
-                </div>
-              )
+              return [
+                <list-body-cell-wrap hide-field copy message={value}>
+                  {title} : <span>{ value || '-' }</span>
+                </list-body-cell-wrap>,
+              ]
             }
             return [
               connection(i18n.t('db.text_153'), pri),
