ad842ed050fe55887b3d6cb1058a3b9a60822a10
Qiu Jian <qiujian@yunionyun.com>
2021-10-17 14:16:34 +0800

    feature: support setting vdi and vga of vm

---
+++ containers/Compute/sections/VDI/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <a-form-item>
+    <a-radio-group v-decorator="decorator" @change="handleVdiChange">
+      <a-radio-button value="vnc">VNC</a-radio-button>
+      <a-radio-button value="spice">SPICE</a-radio-button>
+    </a-radio-group>
+  </a-form-item>
+</template>
+
+<script>
+export default {
+  name: 'VDI',
+  props: {
+    decorator: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    handleVdiChange (e) {
+      this.$emit('change', { value: e.target.value })
+    },
+  },
+}
+</script>
---
+++ containers/Compute/sections/VGA/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <a-form-item>
+    <a-radio-group v-decorator="decorator">
+      <a-radio-button :value="opt.value" v-for="opt in vgaOptions" :key="opt.value">{{ opt.text }}</a-radio-button>
+    </a-radio-group>
+  </a-form-item>
+</template>
+
+<script>
+export default {
+  name: 'VGA',
+  props: {
+    decorator: {
+      type: Array,
+      required: true,
+    },
+    vdi: {
+      type: String,
+    },
+  },
+  computed: {
+    vgaOptions () {
+      var options = []
+      if (this.vdi === 'vnc') {
+        options.push({
+          text: 'Standard',
+          value: 'std',
+        })
+      }
+      options.push(
+        {
+          text: 'QXL',
+          value: 'qxl',
+        },
+        /*{
+          text: 'Virtio-VGA',
+          value: 'virtio',
+        },*/
+      )
+      return options
+    },
+  },
+}
+</script>
