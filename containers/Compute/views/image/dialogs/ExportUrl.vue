34079875f8322263f78cd99ccd40fc15d8892b36
tb365 <tangbin@yunion.cn>
2021-10-19 19:04:42 +0800

    export image fix

--- containers/Compute/views/image/dialogs/ExportUrl.vue
+++ containers/Compute/views/image/dialogs/ExportUrl.vue
@@ -3,8 +3,10 @@
     <div slot="header">{{$t('compute.image.export.url.title')}}</div>
     <div slot="body">
       <p>{{ $t('compute.image.export.url.title.tips') }}</p>
-      <code-mirror class="image-url-export" :value="params.url" :options="cmOptions" view-height="100px" :is-scroll="true" />
-      <copy :message="params.url" />
+      <div class="d-flex align-items-center list-body-cell-wrap">
+        <pre class="image-url-export">{{ params.url }}</pre>
+        <copy :message="params.url" />
+      </div>
     </div>
     <div slot="footer">
       <a-button type="primary" @click="handleConfirm" :loading="loading">{{ $t('compute.image.export.url.confirm') }}</a-button>
@@ -38,6 +40,7 @@ export default {
       try {
         this.$copyText(this.params.url)
         this.cancelDialog()
+        this.$message.success(this.$t('common.copy'))
       } catch (error) {
         this.$message.warn(this.$t('common.copyError'))
       }
@@ -48,10 +51,9 @@ export default {
 
 <style lang="less" scoped>
 .image-url-export {
-  ::v-deep {
-    .CodeMirror {
-      height: 100px !important;
-    }
-  }
+  white-space: pre-wrap;       /* Since CSS 2.1 */
+  white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+  white-space: -o-pre-wrap;    /* Opera 7 */
+  word-wrap: break-word;       /* Internet Explorer 5.5+ */
 }
 </style>
