c42056ce07a489c43c2242dbe9f57a749833c3e8
GuoLiBin6 <782518577@qq.com>
2021-10-22 11:15:03 +0800

    feat[3.9]: change pdf size
    调整导出pdf的尺寸

--- src/utils/echart.js
+++ src/utils/echart.js
@@ -4,25 +4,26 @@ import JsPDF from 'jspdf'
 /*
  * @params {Object} options { target: <domID>, title: <pdf name>}
  */
-function getPdf (options) {
-  const dom = document.querySelector(options.target)
-  html2canvas(dom, {
-    allowTaint: true,
-  }).then(function (canvas) {
+async function getPdf (options) {
+  // eslint-disable-next-line
+  return new Promise(async (resolve, reject) => {
+    const dom = document.querySelector(options.target)
+    const canvas = await html2canvas(dom, { allowTaint: true })
     const contentWidth = canvas.width
     const contentHeight = canvas.height
-    let imgWidth = 595.28
+    let imgWidth = 841.89
     let imgHeight = imgWidth / contentWidth * contentHeight
     const pageData = canvas.toDataURL('image/jpeg', 1)
-    const PDF = new JsPDF('', 'pt', 'a4')
-    if (imgHeight < 841.89) {
+    const PDF = new JsPDF('l', 'pt', 'a4')
+    if (imgHeight < 595.28) {
       PDF.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
     } else {
-      imgHeight = 841.89
+      imgHeight = 595.28
       imgWidth = imgHeight / contentHeight * contentWidth
       PDF.addImage(pageData, 'JPEG', (contentWidth - imgWidth) / 2, 0, imgWidth, imgHeight)
     }
     PDF.save((options.title || 'echart') + '.pdf')
+    resolve(true)
   })
 }
 
