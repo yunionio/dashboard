b346024fdb015619d804f3d02bacfadc38537da7
GuoLiBin6 <782518577@qq.com>
2021-10-25 16:03:51 +0800

    feat[3.9]: add padding for export pdf
    导出pdf增加页边距

--- src/utils/echart.js
+++ src/utils/echart.js
@@ -11,16 +11,19 @@ async function getPdf (options) {
     const canvas = await html2canvas(dom, { allowTaint: true })
     const contentWidth = canvas.width
     const contentHeight = canvas.height
-    let imgWidth = 841.89
+    const pageWidth = 841.89
+    const pageHeight = 595.28
+    const pagePadding = 20
+    let imgWidth = pageWidth - pagePadding * 2
     let imgHeight = imgWidth / contentWidth * contentHeight
     const pageData = canvas.toDataURL('image/jpeg', 1)
     const PDF = new JsPDF('l', 'pt', 'a4')
-    if (imgHeight < 595.28) {
-      PDF.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
+    if (imgHeight < pageHeight - pagePadding * 2) {
+      PDF.addImage(pageData, 'JPEG', pagePadding, pagePadding, imgWidth, imgHeight)
     } else {
-      imgHeight = 595.28
+      imgHeight = pageHeight - pagePadding * 2
       imgWidth = imgHeight / contentHeight * contentWidth
-      PDF.addImage(pageData, 'JPEG', (contentWidth - imgWidth) / 2, 0, imgWidth, imgHeight)
+      PDF.addImage(pageData, 'JPEG', (contentWidth - imgWidth) / 2, pagePadding, imgWidth, imgHeight)
     }
     PDF.save((options.title || 'echart') + '.pdf')
     resolve(true)
