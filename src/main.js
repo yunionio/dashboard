66efe33f03ebab8f918c2f13a26fca8a008031f2
mj <gaomingjiu@yunion.cn>
2021-10-28 10:41:21 +0800

    fix: login page global service error

--- src/main.js
+++ src/main.js
@@ -55,7 +55,6 @@ async function start () {
   try {
     await store.dispatch('app/fetchCompayInfo')
     await store.dispatch('app/fetchWorkflowEnabledKeys', { $t: uuid() })
-    await store.dispatch('common/fetchGlobalServices', { $t: uuid() })
   } finally {
     app.$mount('#app')
   }
