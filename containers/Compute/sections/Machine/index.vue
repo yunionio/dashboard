830678fcb2d2f3e963fd94d65046758e9d0f810d
Qiu Jian <qiujian@yunionyun.com>
2021-11-04 23:56:28 +0800

    feature: support change vm machine type(pc or q35)

---
+++ containers/Compute/sections/Machine/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <a-form-item>
+    <a-radio-group v-decorator="decorator" @change="handleMachineChange">
+      <a-radio-button value="pc">PC</a-radio-button>
+      <a-radio-button value="q35">Q35</a-radio-button>
+    </a-radio-group>
+  </a-form-item>
+</template>
+
+<script>
+export default {
+  name: 'Machine',
+  props: {
+    decorator: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    handleMachineChange (e) {
+      this.$emit('change', { value: e.target.value })
+    },
+  },
+}
+</script>
