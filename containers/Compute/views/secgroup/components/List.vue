21e45672bd2b3efc292f77221cd84224a5f3227f
mj <gaomingjiu@yunion.cn>
2021-12-06 15:28:36 +0800

    fix: #8019 导入安全组规则时下载模板报错

--- containers/Compute/views/secgroup/components/List.vue
+++ containers/Compute/views/secgroup/components/List.vue
@@ -25,6 +25,7 @@ import globalSearchMixins from '@/mixins/globalSearch'
 import { getSetPublicAction } from '@/utils/common/tableActions'
 import expectStatus from '@/constants/expectStatus'
 import regexp from '@/utils/regexp'
+import { exportDataOptions } from '../utils'
 
 export default {
   name: 'SecgroupList',
@@ -81,41 +82,7 @@ export default {
         },
         responseData: this.responseData,
       }),
-      exportDataOptions: {
-        title: this.$t('compute.text_986'),
-        resource: 'secgrouprules',
-        items: [
-          { label: this.$t('compute.text_987'), key: 'id' },
-          { label: this.$t('compute.text_988'), key: 'secgroup' },
-          { label: this.$t('compute.text_989'), key: 'secgroup_id' },
-          { label: this.$t('compute.text_990'), key: 'direction' },
-          { label: this.$t('compute.text_694'), key: 'action' },
-          { label: this.$t('compute.text_980'), key: 'protocol' },
-          { label: this.$t('compute.text_349'), key: 'ports' },
-          { label: this.$t('compute.text_981'), key: 'priority' },
-          { label: 'CIDR', key: 'cidr' },
-          { label: this.$t('dictionary.project'), key: 'tenant' },
-          { label: this.$t('compute.text_271'), key: 'user_tags' },
-        ],
-        transformParams (params) {
-          if (params.filter) {
-            params.filter = params.filter.map((item) => {
-              if (item.includes('name.contains(')) {
-                const val = /\((.+?)\)/.exec(item)[1]
-                params.secgroup_name = val
-                return ''
-              }
-              if (item.startsWith('id.in(')) {
-                return item.replace('id', 'secgroup_id')
-              }
-              return item
-            }).filter((item) => {
-              return item
-            })
-          }
-          return params
-        },
-      },
+      exportDataOptions,
     }
   },
   computed: {
