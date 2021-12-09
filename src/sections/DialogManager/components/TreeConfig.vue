f9d58b0a20cae491af854e6b5a75d45d095c49f5
GuoLiBin6 <782518577@qq.com>
2021-12-09 17:36:00 +0800

    feat: pairs tags config
    修改 层级项目配置

--- src/sections/DialogManager/components/TreeConfig.vue
+++ src/sections/DialogManager/components/TreeConfig.vue
@@ -1,16 +1,36 @@
 <template>
   <base-dialog @cancel="cancelDialog">
     <div slot="header">{{ params.title }}</div>
-    <div slot="body" style="height:400px">
-      <draggable-transfer
-        idKey="key"
-        :loading="transferLoading"
-        :dataList="resourceDataList"
-        :defaultList="defaultList"
-        :draggable="{right:true}"
-        @change="handleSelectChange" />
+    <div slot="body" style="height:400px;overflow-y:auto">
+      <template v-if="!transferLoading">
+        <a-form v-bind="formItemLayout">
+          <template v-for="(item,index) in keysList">
+            <a-form-item :key="index" :label="`${$t('common_743')}${index+1}`">
+              <div class="d-flex">
+                <div class="flex-fill">
+                  <base-select
+                  v-model="keysList[index]"
+                  id-key="key"
+                  name-key="label"
+                  @change="v => change(v, index)"
+                  :options="resourceDataList"
+                  :select-props="{ allowClear: true, placeholder: $t('common_742') }"
+                  :disabledItems="disabledList[index]" />
+                </div>
+                <a-button v-if="index>0" style="flex: 0 0 24px;margin-left: 20px;transform:translateY(8px)" shape="circle" icon="minus" size="small" @click="deleteKey(index)" />
+                <div v-else style="flex: 0 0 24px;margin-left: 20px;" />
+              </div>
+            </a-form-item>
+          </template>
+        </a-form>
+        <!-- 添加 -->
+        <div class="d-flex align-items-center">
+          <a-button type="primary" shape="circle" icon="plus" size="small" @click="addKey" />
+          <a-button type="link" @click="addKey">{{$t('common_741')}}</a-button>
+        </div>
+      </template>
+      <div v-else class="loading"><a-spin :spinning="transferLoading" /></div>
     </div>
-
     <div slot="footer">
       <a-button v-bind="okButtonProps" @click="handleConfirm">{{ $t("dialog.ok") }}</a-button>
       <a-button @click="cancelDialog">{{ $t('dialog.cancel') }}</a-button>
@@ -34,7 +54,16 @@ export default {
       loading: false,
       transferLoading: false,
       resourceDataList: [],
-      selectedTreeList: [],
+      keysList: [],
+      formItemLayout: {
+        wrapperCol: {
+          span: 21,
+        },
+        labelCol: {
+          span: 3,
+        },
+      },
+      disabledList: [],
     }
   },
   computed: {
@@ -69,9 +98,12 @@ export default {
     async handleConfirm () {
       this.loading = true
       try {
-        const projectTreeTags = this.selectedTreeList.map(item => {
-          return {
-            key: item.key,
+        const projectTreeTags = []
+        this.keysList.map(item => {
+          if (item) {
+            projectTreeTags.push({
+              key: item,
+            })
           }
         })
         await this.updateProjectTreeTags(projectTreeTags)
@@ -102,15 +134,44 @@ export default {
         }).sort((a, b) => {
           return b.count - a.count
         })
+        const defaultKeysList = this.projectTreeTags.map(item => {
+          return item.key
+        })
+        this.keysList = defaultKeysList.length ? defaultKeysList : [undefined]
+        this.updateDisabledItems()
         this.transferLoading = false
       } catch (err) {
         console.error(err)
         this.transferLoading = false
       }
     },
-    handleSelectChange (val) {
-      this.selectedTreeList = val
+    change (val, index) {
+      this.keysList[index] = val
+      this.updateDisabledItems()
+    },
+    updateDisabledItems () {
+      this.$nextTick(() => {
+        this.disabledList = this.keysList.map(key => {
+          const disabledItems = this.keysList.filter(item => {
+            return item !== key
+          })
+          return disabledItems
+        })
+      })
+    },
+    addKey () {
+      this.keysList = this.keysList.concat([undefined])
+    },
+    deleteKey (index) {
+      this.keysList = this.keysList.filter((item, idx) => index !== idx)
     },
   },
 }
 </script>
+<style lang="less" scoped>
+.loading {
+  display: flex;
+  justify-content: center;
+  padding-top: 30px;
+}
+</style>
