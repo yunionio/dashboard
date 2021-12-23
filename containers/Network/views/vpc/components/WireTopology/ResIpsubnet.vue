88b0450b16c56f2997076d0db229fe4a7591413b
mj <gaomingjiu@yunion.cn>
2021-12-23 15:23:50 +0800

    feat: add wire topology

---
+++ containers/Network/views/vpc/components/WireTopology/ResIpsubnet.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="res-container d-flex justify-content-center">
+    <div class="res res-ipsubnet">
+      <span class="name">{{ name }}</span>
+      <a-tooltip placement="right">
+        <template slot="title">
+          <p>名称：{{ name }}</p>
+          <p>状态：运行中</p>
+        </template>
+        <icon type="res-network" />
+      </a-tooltip>
+      <p class="desc">{{ desc }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResIpsubnet',
+  props: {
+    name: String,
+    desc: String,
+  },
+  data () {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "index.scss";
+</style>
---
+++ containers/Network/views/vpc/components/WireTopology/ResWire.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wire d-flex">
+    <span class="line" />
+    <div class="text-center">
+      <a-tooltip placement="right">
+        <template slot="title">
+          <p>名称：{{ name }}</p>
+          <p>状态：运行中</p>
+        </template>
+        <icon type="res-wire" />
+      </a-tooltip>
+      <span class="name">{{ name }}</span>
+    </div>
+    <span class="line" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResWire',
+  props: {
+    name: String,
+  },
+  data () {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "index.scss";
+</style>
