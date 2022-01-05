8d095974ecd5a17ec7896f305394c6f46b512cd7
mj <gaomingjiu@yunion.cn>
2022-01-05 15:12:01 +0800

    fix: i18n for load no more

--- src/components/PageList/components/Table.vue
+++ src/components/PageList/components/Table.vue
@@ -37,7 +37,7 @@
         <template v-if="tableData.length > 0">
           <div class="text-center mt-4">
             <a-button v-if="nextMarker" :loading="loading" type="link" @click="handleNextMarkerChange">{{ loading ? $t('common.loding') : $t('common.LoadMore') }}</a-button>
-            <span v-else>没有更多了</span>
+            <span v-else>{{ $t('common.load_no_more') }}</span>
           </div>
         </template>
       </div>
