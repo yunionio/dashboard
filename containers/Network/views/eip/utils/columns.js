cb08737bad11cd1006e78edddb3f7dad34c99c08
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2022-01-06 15:04:39 +0800

    fix: #8081 已经绑定反亲和组的eip，在绑定资源列表 应该加上 反亲和组

--- containers/Network/views/eip/utils/columns.js
+++ containers/Network/views/eip/utils/columns.js
@@ -1,4 +1,6 @@
 import i18n from '@/locales'
+import { ASSOCIATE_MAP } from '../constants'
+
 export const getAssociateNameTableColumn = ({ vm = {} } = {}) => {
   return {
     field: 'associate_name',
@@ -7,24 +9,12 @@ export const getAssociateNameTableColumn = ({ vm = {} } = {}) => {
     slots: {
       default: ({ row }, h) => {
         if (vm.isPreLoad && !row.associate_name) return [<data-loading />]
-        const type = {
-          server: i18n.t('network.text_226'),
-          natgateway: i18n.t('network.text_227'),
-          loadbalancer: i18n.t('network.text_137'),
-        }
         if (!row.associate_name) return '-'
-        const text = `${row.associate_name}(${type[row.associate_type] || '-'})`
-        let permission = 'server_get'
-        let name = 'VmInstanceSidePage'
-        let tab = 'vm-instance-detail'
-        if (row.associate_type === 'loadbalancer') {
-          name = 'LbSidePage'
-          permission = 'lb_loadbalancers_get'
-          tab = 'lb-detail'
-        }
-        if (vm) {
+        const associate = ASSOCIATE_MAP[row.associate_type]
+        const text = `${row.associate_name}(${associate.name || '-'})`
+        if (vm && associate) {
           return [
-            <side-page-trigger permission={permission} tab={tab} name={name} id={row.associate_id} vm={vm}>{text}</side-page-trigger>,
+            <side-page-trigger permission={associate.permission} tab={associate.tab} name={associate.sidePage} id={row.associate_id} vm={vm}>{text}</side-page-trigger>,
           ]
         }
         return text
