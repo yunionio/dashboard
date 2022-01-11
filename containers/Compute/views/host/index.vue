85e1bc32bd2fd91d4e6a8c5a147404c941f74c92
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2022-01-11 16:10:36 +0800

    feat: host resource update statistics

--- containers/Compute/views/host/index.vue
+++ containers/Compute/views/host/index.vue
@@ -41,7 +41,37 @@ export default {
     this.fetchResStatistics({
       scope: this.$store.getters.scope,
       baremetal: false,
+    }, (resData) => {
+      return this.getStatusOpts(resData)
     })
+  },
+  methods: {
+    getStatusOpts (data) {
+      const { running = {} } = data
+      // 统计
+      let total = 0
+      let error = 0
+      let other = 0
+      for (const k in data) {
+        total += data[k].total_count
+        if (new RegExp('fail').test(k)) {
+          this.errorFilterStatus.push(k)
+          error += data[k].total_count
+        } else {
+          if (!['running'].includes(k)) {
+            this.otherFilterStatus.push(k)
+            other += data[k].total_count
+          }
+        }
+      }
+      const statusOpts = [
+        { title: this.$t('compute.text_576'), type: 'total', num: total },
+        { title: this.$t('status.host.running'), type: 'running', num: running?.total_count || 0 },
+        { title: this.$t('common_623', [this.$t('scope.text_61')]), type: 'error', num: error },
+        { title: this.$t('compute.text_674'), type: 'other', num: other },
+      ]
+      return statusOpts
+    },
   },
 }
 </script>
