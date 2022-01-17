f36370e1d5f8ffb8c10c3ca6f11089e42eb4a66d
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2022-01-17 17:40:35 +0800

    feat: create instance backup

--- containers/Compute/views/disk/dialogs/CreateBackupDialog.vue
+++ containers/Compute/views/disk/dialogs/CreateBackupDialog.vue
@@ -56,6 +56,11 @@ export default {
         ],
         storage: [
           'storage',
+          {
+            rules: [
+              { required: true, message: this.$t('compute.text_1022', [this.$t('compute.backup_storage')]), trigger: 'change' },
+            ],
+          },
         ],
       },
       formItemLayout: {
