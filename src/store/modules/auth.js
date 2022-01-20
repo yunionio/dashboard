9b56e191771a347dc23290b422f41e6ba9f7d157
GuoLiBin6 <782518577@qq.com>
2022-01-20 16:06:29 +0800

    fix: error commit

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -53,7 +53,6 @@ const initialState = {
   // 提交的登录表单数据
   loginFormData: {},
   canRenderDefaultLayout: false,
-  noActionLogoutSeconds: 0,
 }
 
 export default {
@@ -190,9 +189,6 @@ export default {
       state.capability = {}
       state.stats = {}
       state.canRenderDefaultLayout = false
-    },
-    SET_NO_ACTION_LOGOUT_SECONDS (state, payload) {
-      state.noActionLogoutSeconds = payload
     },
     CLEAR_LOGGED_USERS (state) {
       setLoggedUsersInStorage({})
