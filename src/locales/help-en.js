729179ccd7ff2acee49b0f10c2f515a26b56d98a
GuoLiBin6 <782518577@qq.com>
2022-01-26 10:46:50 +0800

    feat: #3609 add tooltip i18n

--- src/locales/help-en.js
+++ src/locales/help-en.js
@@ -64,5 +64,6 @@ export default {
     billNegativeNumberReson: 'Negative numbers in the bill are due to refunds arising, such as early release refunds for annual and monthly machines, refunds following the use of coupons, and refunds for other benefits enjoyed by the account.',
     billCurrentYearFee: 'This year\'s consumption is displayed as the current year\'s expenses on the currently selected date and end date',
     restartNetworkToEffectIp: 'After replacing the IP, you need to restart the network card at the same time to take effect',
+    billResourceSharedTip: 'After enabling, the resource cost bill of prepaid will be allocated to the specific use date. The cost after allocation = postpaid + prepaid',
   },
 }
