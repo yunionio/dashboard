8e801c4044105bfa51bd629bb46bdb3ffc48301e
GuoLiBin6 <782518577@qq.com>
2022-02-07 14:55:11 +0800

    fix: #8172 add permission for instancebackup and diskbackup

--- containers/Compute/router/index.js
+++ containers/Compute/router/index.js
@@ -383,6 +383,7 @@ export default {
           path: '/disk-backup',
           meta: {
             label: i18n.t('compute.disk_backup'),
+            permission: 'diskbackups_list',
             hidden: () => {
               return !hasSetupKey(['onestack'])
             },
@@ -400,6 +401,7 @@ export default {
           path: '/instance-backup',
           meta: {
             label: i18n.t('compute.instance_backup'),
+            permission: 'instancebackups_list',
             hidden: () => {
               return !hasSetupKey(['onestack'])
             },
