76c14f4f4e43285be9caedf4fe686d31c793949b
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2022-02-14 14:29:38 +0800

    fix: #8192 arm架构的虚拟机传参应该是os_arch=arm

--- containers/Compute/sections/DataDisk/index.vue
+++ containers/Compute/sections/DataDisk/index.vue
@@ -262,6 +262,7 @@ export default {
         is_instance_snapshot: false,
         $t: uuid(),
         status: 'ready',
+        os_arch: this.form.fd.os_arch,
       }
       const scopeParams = {}
       if (this.$store.getters.isAdminMode) {
