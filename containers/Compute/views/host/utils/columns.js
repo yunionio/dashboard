cef350133b6e444a79e6f6a36b3d49586fbed47a
Qiu Jian <qiujian@yunionyun.com>
2022-02-14 09:40:12 +0800

    fix: misc fixes, vip, evip, passthrough, etc.

--- containers/Compute/views/host/utils/columns.js
+++ containers/Compute/views/host/utils/columns.js
@@ -2,7 +2,7 @@ import i18n from '@/locales'
 export const getIsolatedDeviceCountColumns = () => {
   return {
     field: 'isolated_device_count',
-    title: i18n.t('compute.text_609'),
+    title: i18n.t('compute.passthrough_device_count'),
     minWidth: 80,
     showOverflow: 'title',
     formatter: ({ row }) => {
