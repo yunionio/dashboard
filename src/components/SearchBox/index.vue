ea61c2d078617d73df2e67ac8152e8b6d83651c2
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2022-03-07 17:32:15 +0800

    feat: #3688 迁移选择宿主机框优化

--- src/components/SearchBox/index.vue
+++ src/components/SearchBox/index.vue
@@ -161,7 +161,7 @@ export default {
         } else {
           let newKey = key
           if (this.autocompleterSearch && !this.autocompleterSearch.includes(this.keySeparator)) {
-            const ipKey = Object.keys(this.options).find(v => v.startsWith('ip'))
+            const ipKey = Object.keys(this.options).find(v => v.startsWith('ip') || v.endsWith('ip'))
             if (/^\d+.*$/.test(value) && value <= 255 && ipKey) {
               if (Array.isArray(value)) {
                 const isErrIp = value[0].split('.').some(v => v > 255)
