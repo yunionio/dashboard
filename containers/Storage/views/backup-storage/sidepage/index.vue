a92e5d8b5208edb2778399ee84c3c87adeb7a483
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2022-03-10 16:41:25 +0800

    feat: #3691 add backup storage list

---
+++ containers/Storage/views/backup-storage/sidepage/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <base-side-page
+    @cancel="cancelSidePage"
+    :title="$t('compute.backup_storage')"
+    icon="res-backup-storage"
+    :res-name="detailData.name"
+    :current-tab="params.windowData.currentTab"
+    :tabs="detailTabs"
+    :loaded="loaded"
+    @tab-change="handleTabChange">
+    <template v-slot:actions>
+      <actions :options="singleActions" :row="detailData" button-type="link" button-size="small" />
+    </template>
+    <component
+      :is="params.windowData.currentTab"
+      :data="detailData"
+      :res-id="data.id"
+      :id="listId"
+      :on-manager="onManager"
+      :columns="columns" />
+  </base-side-page>
+</template>
+
+<script>
+import SingleActionsMixin from '../mixins/singleActions'
+import ColumnsMixin from '../mixins/columns'
+import BackupStorageDetail from './Detail'
+import SidePageMixin from '@/mixins/sidePage'
+import WindowsMixin from '@/mixins/windows'
+import Actions from '@/components/PageList/Actions'
+
+export default {
+  name: 'BackupStorageSidePage',
+  components: {
+    BackupStorageDetail,
+    Actions,
+  },
+  mixins: [SidePageMixin, WindowsMixin, ColumnsMixin, SingleActionsMixin],
+  data () {
+    return {
+      detailTabs: [
+        { label: this.$t('compute.text_238'), key: 'backup-storage-detail' },
+        { label: this.$t('compute.text_240'), key: 'event-drawer' },
+      ],
+    }
+  },
+  computed: {
+    listId () {
+      switch (this.params.windowData.currentTab) {
+        case 'event-drawer':
+          return 'EventListForBackupStorageSidePage'
+        default:
+          return ''
+      }
+    },
+  },
+}
+</script>
---
+++ containers/Storage/views/backup-storage/utils/columns.js
@@ -0,0 +1,47 @@
+import i18n from '@/locales'
+import { sizestr } from '@/utils/utils'
+
+export const getStorageTypeColumns = () => {
+  return {
+    field: 'storage_type',
+    title: i18n.t('storage.text_38'),
+    slot: {
+      default: (row) => {
+        return row.storage_type
+      },
+    },
+  }
+}
+
+export const getCapacityMbColumns = () => {
+  return {
+    field: 'capacity_mb',
+    title: i18n.t('storage.capacity'),
+    slot: {
+      default: (row) => {
+        return sizestr(row.capacity_mb, 'M', 1024) || '-'
+      },
+    },
+  }
+}
+
+export const getProjectDomainTableColumns = () => {
+  return {
+    field: 'project_domain',
+    title: i18n.t('common.attribution_scope'),
+    slots: {
+      default: ({ row }, h) => {
+        const ret = []
+        const domain = row.project_domain
+        if (domain) {
+          ret.push(
+            <list-body-cell-wrap hide-field copy field="domain" row={{ domain }}>
+              <span>{ domain }</span>
+            </list-body-cell-wrap>,
+          )
+        }
+        return ret
+      },
+    },
+  }
+}
