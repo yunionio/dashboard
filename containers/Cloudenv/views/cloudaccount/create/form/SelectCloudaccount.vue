ab08eeac7b00777fcd89501f24c5d9196020cc29
GuoLiBin6 <782518577@qq.com>
2022-03-17 14:41:30 +0800

    fix: 云账号新建限制

--- containers/Cloudenv/views/cloudaccount/create/form/SelectCloudaccount.vue
+++ containers/Cloudenv/views/cloudaccount/create/form/SelectCloudaccount.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-// import { hasSetupKey } from '@/utils/auth'
+import { hasSetupKey } from '@/utils/auth'
 import { CLOUDACCOUNT_TYPES, ENV_TITLE } from '@Cloudenv/views/cloudaccount/constants'
 
 export default {
@@ -45,22 +45,21 @@ export default {
       return undefined
     },
     types () {
-      // const typesMap = {}
-      // for (const box in CLOUDACCOUNT_TYPES) {
-      //   for (const brand in CLOUDACCOUNT_TYPES[box]) {
-      //     if (hasSetupKey([brand])) {
-      //       if (!typesMap[box]) {
-      //         typesMap[box] = {
-      //           [brand]: CLOUDACCOUNT_TYPES[box][brand],
-      //         }
-      //       } else {
-      //         typesMap[box][brand] = CLOUDACCOUNT_TYPES[box][brand]
-      //       }
-      //     }
-      //   }
-      // }
-      // return typesMap
-      return CLOUDACCOUNT_TYPES
+      const typesMap = {}
+      for (const box in CLOUDACCOUNT_TYPES) {
+        for (const brand in CLOUDACCOUNT_TYPES[box]) {
+          if (hasSetupKey([brand])) {
+            if (!typesMap[box]) {
+              typesMap[box] = {
+                [brand]: CLOUDACCOUNT_TYPES[box][brand],
+              }
+            } else {
+              typesMap[box][brand] = CLOUDACCOUNT_TYPES[box][brand]
+            }
+          }
+        }
+      }
+      return typesMap
     },
   },
   watch: {
@@ -84,16 +83,15 @@ export default {
       }
     },
     isShowItem (item) {
-      return true
-      // if (this.globalSettingSetupKeys === undefined) {
-      //   return true
-      // }
-      // if (typeof item === 'string') {
-      //   if (item === 'private' && this.globalSettingSetupKeys.indexOf('vmware') > -1) return true
-      //   return this.globalSettingSetupKeys.indexOf(item) > -1 || (this.globalSettingSetupKeys.indexOf('bill') > -1 && this.isBillEnv(item))
-      // }
-      // // console.log(this.globalSettingSetupKeys, item.provider.toLowerCase(), this.globalSettingSetupKeys.indexOf(item.provider.toLowerCase()) > -1)
-      // return this.globalSettingSetupKeys.indexOf(item.provider.toLowerCase()) > -1 || (this.globalSettingSetupKeys.indexOf('bill') > -1 && this.isBillItem(item))
+      if (this.globalSettingSetupKeys === undefined) {
+        return true
+      }
+      if (typeof item === 'string') {
+        if (item === 'private' && this.globalSettingSetupKeys.indexOf('vmware') > -1) return true
+        return this.globalSettingSetupKeys.indexOf(item) > -1 || (this.globalSettingSetupKeys.indexOf('bill') > -1 && this.isBillEnv(item))
+      }
+      // console.log(this.globalSettingSetupKeys, item.provider.toLowerCase(), this.globalSettingSetupKeys.indexOf(item.provider.toLowerCase()) > -1)
+      return this.globalSettingSetupKeys.indexOf(item.provider.toLowerCase()) > -1 || (this.globalSettingSetupKeys.indexOf('bill') > -1 && this.isBillItem(item))
     },
     isBillEnv (env) {
       if (env === 'public') {
