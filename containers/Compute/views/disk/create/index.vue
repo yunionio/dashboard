ee0f45d82330711067d1f1d18490d328a8e621f4
Qiu Jian <qiujian@yunionyun.com>
2022-04-11 13:26:52 +0800

    fix: allow create key with resource

--- containers/Compute/views/disk/create/index.vue
+++ containers/Compute/views/disk/create/index.vue
@@ -223,6 +223,12 @@ export default {
               initialValue: '',
             },
           ],
+          encrypt_key_alg: [
+            'encrypt_key_alg',
+            {
+              initialValue: '',
+            },
+          ],
           encrypt_key_id: [
             'encrypt_key_id',
           ],
