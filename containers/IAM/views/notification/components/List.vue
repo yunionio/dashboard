9a27ce1ed86cf3b57c3208570f2aee816077fa36
mj <gaomingjiu@yunion.cn>
2022-04-12 18:44:55 +0800

    feat: #3719 安全告警优化

--- containers/IAM/views/notification/components/List.vue
+++ containers/IAM/views/notification/components/List.vue
@@ -6,6 +6,7 @@
 import NotificationBaseList from '@IAM/sections/NotificationBaseList'
 import { getTimeRangeFilter } from '@/utils/common/tableFilter'
 import { getTimeTableColumn } from '@/utils/common/tableColumn'
+import { NOTIFY_TOPIC_TYPES_MAP } from '@IAM/constants'
 
 export default {
   name: 'NotificationList',
@@ -19,6 +20,16 @@ export default {
     return {
       ExtraListParams: { contact_type: 'webconsole' },
       ExtraColumns: [
+        {
+          title: this.$t('system.notify.topic.type'),
+          field: 'type',
+          sortable: true,
+          minWidth: 100,
+          showOverflow: 'title',
+          formatter: ({ row }) => {
+            return NOTIFY_TOPIC_TYPES_MAP[row.type] ? NOTIFY_TOPIC_TYPES_MAP[row.type].label : '-'
+          },
+        },
         getTimeTableColumn({
           field: 'received_at',
           title: this.$t('system.text_316'),
@@ -29,6 +40,27 @@ export default {
         },
       ],
       ExtraFilterOptions: {
+        type: {
+          label: this.$t('system.text_48'),
+          filter: true,
+          dropdown: true,
+          distinctField: {
+            type: 'field',
+            key: 'topic_type',
+            getParams: this.getParam,
+          },
+          formatter: val => {
+            return `topic_type.contains("${val}")`
+          },
+          mapper: list => {
+            return list.map(({ key, label }) => {
+              return {
+                key,
+                label: NOTIFY_TOPIC_TYPES_MAP[key] ? NOTIFY_TOPIC_TYPES_MAP[key].label : label,
+              }
+            })
+          },
+        },
         received_at: getTimeRangeFilter({ label: this.$t('system.text_316'), field: 'received_at' }),
       },
     }
