6bfaa5d173e435fd0a51e09f9a8827c2d12c385a
mj <gaomingjiu@yunion.cn>
2022-04-13 18:07:28 +0800

    feat: 增加站内信消息类型

--- containers/IAM/views/notification/components/List.vue
+++ containers/IAM/views/notification/components/List.vue
@@ -22,12 +22,12 @@ export default {
       ExtraColumns: [
         {
           title: this.$t('system.notify.topic.type'),
-          field: 'type',
+          field: 'topic_type',
           sortable: true,
           minWidth: 100,
           showOverflow: 'title',
           formatter: ({ row }) => {
-            return NOTIFY_TOPIC_TYPES_MAP[row.type] ? NOTIFY_TOPIC_TYPES_MAP[row.type].label : '-'
+            return NOTIFY_TOPIC_TYPES_MAP[row.topic_type] ? NOTIFY_TOPIC_TYPES_MAP[row.topic_type].label : '-'
           },
         },
         getTimeTableColumn({
@@ -40,27 +40,27 @@ export default {
         },
       ],
       ExtraFilterOptions: {
-        type: {
-          label: this.$t('system.text_48'),
-          filter: true,
-          dropdown: true,
-          distinctField: {
-            type: 'field',
-            key: 'topic_type',
-            getParams: this.getParam,
-          },
-          formatter: val => {
-            return `topic_type.contains("${val}")`
-          },
-          mapper: list => {
-            return list.map(({ key, label }) => {
-              return {
-                key,
-                label: NOTIFY_TOPIC_TYPES_MAP[key] ? NOTIFY_TOPIC_TYPES_MAP[key].label : label,
-              }
-            })
-          },
-        },
+        // topic_type: {
+        //   label: this.$t('system.text_48'),
+        //   filter: true,
+        //   dropdown: true,
+        //   distinctField: {
+        //     type: 'field',
+        //     key: 'topic_type',
+        //     getParams: this.getParam,
+        //   },
+        //   formatter: val => {
+        //     return `topic_type.contains("${val}")`
+        //   },
+        //   mapper: list => {
+        //     return list.map(({ key, label }) => {
+        //       return {
+        //         key,
+        //         label: NOTIFY_TOPIC_TYPES_MAP[key] ? NOTIFY_TOPIC_TYPES_MAP[key].label : label,
+        //       }
+        //     })
+        //   },
+        // },
         received_at: getTimeRangeFilter({ label: this.$t('system.text_316'), field: 'received_at' }),
       },
     }
