89e64ba0f00687e8eabf49287ed24b4378b16cb4
GuoLiBin6 <kongxin@kongxin.local>
2022-04-13 16:47:29 +0800

    feat: #3579 页面允许缓存

--- src/layouts/RouterView/index.vue
+++ src/layouts/RouterView/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <router-view />
+    <keep-alive v-if="keepAlive">
+      <router-view :key="key" />
+    </keep-alive>
+    <router-view v-else :key="key" />
 </template>
+
+<script>
+export default {
+  computed: {
+    key () {
+      return this.$route.fullPath
+    },
+    keepAlive () {
+      const { meta = {} } = this.$route
+      const { keepAlive = false } = meta
+      return keepAlive
+    },
+  },
+}
+</script>
