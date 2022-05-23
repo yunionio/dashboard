1dfe1ab5de519a080329203cfd825622384743c0
GuoLiBin6 <782518577@qq.com>
2022-05-23 16:33:43 +0800

    fix: #8417 更正 策略定义-通知渠道设置权限控制

--- containers/IAM/router/index.js
+++ containers/IAM/router/index.js
@@ -320,7 +320,7 @@ export default {
           meta: {
             label: i18n.t('system.text_19'),
             hidden: () => {
-              if (isScopedPolicyMenuHidden('sub_hidden_menus.mailconfig')) {
+              if (isScopedPolicyMenuHidden('sub_hidden_menus.notifyconfig')) {
                 return true
               }
               if (!(store.getters.isAdminMode || store.getters.isDomainMode)) {
