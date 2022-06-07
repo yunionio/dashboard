ab4639f3afecdd5f3cbdb46a38ccd2a449879352
mj <gaomingjiu@yunion.cn>
2022-06-07 10:58:50 +0800

    feat: 调整菜单样式

--- src/sections/ProductsList/Sub.vue
+++ src/sections/ProductsList/Sub.vue
@@ -189,7 +189,8 @@ export default {
     }
     .l2-menu-list {
       .l2-menu-group-title {
-        color: rgba(0, 0, 0, .7);
+        color: #000000;
+        font-weight: 500;
       }
       .l2-menu-item {
         color: @sidebar-light-text-color;
@@ -209,6 +210,14 @@ export default {
   padding: 8px 0;
 }
 .l2-menu-list {
+  padding: 10px 20px 0 20px;
+  .l2-menu-group {
+    padding-bottom: 10px;
+    border-bottom: 1px solid #E2E2E2;
+  }
+  &:last-child .l2-menu-group {
+    border-bottom:none
+  }
   .l2-menu-group-title {
     font-size: 12px;
     color: rgba(255, 255, 255, .7);
