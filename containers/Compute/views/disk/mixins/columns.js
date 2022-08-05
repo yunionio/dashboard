327a0e3c33f293fc66e52d532b384a0023fc142a
GuoLiBin6 <782518577@qq.com>
2022-08-05 14:59:14 +0800

    feat: #3872 硬盘增加字段排序

--- containers/Compute/views/disk/mixins/columns.js
+++ containers/Compute/views/disk/mixins/columns.js
@@ -39,6 +39,7 @@ export default {
       {
         field: 'disk_size',
         title: i18n.t('table.title.disk_size'),
+        sortable: true,
         minWidth: 50,
         formatter: ({ cellValue }) => {
           return sizestr(cellValue, 'M', 1024)
@@ -64,6 +65,7 @@ export default {
         title: this.$t('res.server'),
         minWidth: 100,
         showOverflow: 'ellipsis',
+        sortable: true,
         slots: {
           default: ({ row }, h) => {
             if (this.isPreLoad && !row.guest) return [<data-loading />]
