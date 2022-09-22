8b4dcc90da30952ac058d9287086aa54a4a7227c
GuoLiBin6 <glbin533@163.com>
2022-09-22 20:04:10 +0800

    feat: #3943 云账号新建-华为去除全球区类型

--- containers/Cloudenv/views/cloudaccount/constants.js
+++ containers/Cloudenv/views/cloudaccount/constants.js
@@ -222,7 +222,7 @@ export function getCloudaccountDocs (scope) {
 
 export const ACCESS_URL = {
   huawei: {
-    InternationalCloud: i18n.t('cloudenv.text_139'),
+    // InternationalCloud: i18n.t('cloudenv.text_139'),
     ChinaCloud: i18n.t('cloudenv.text_140'),
   },
   aws: {
