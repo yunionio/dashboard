4c62e134370a39434342b102d88263b3c32485a3
mj <gaomingjiu@yunion.cn>
2022-09-29 19:33:10 +0800

    fix: conflict

--- src/constants/error.js
+++ src/constants/error.js
@@ -2,8 +2,6 @@ import i18n from '@/locales'
 
 export default {
   ForbiddenError: i18n.t('error_title.ForbiddenError'),
-<<<<<<< HEAD
-=======
   NotFoundError: i18n.t('notify.NotFoundError'),
   IncrrectUsernameOrPassword: i18n.t('notify.IncrrectUsernameOrPassword'),
   InvalidCredentialError: i18n.t('notify.InvalidCredentialError'),
@@ -11,5 +9,4 @@ export default {
 
 export const DETAIL_ERRMSG = {
   findUserByMobile: i18n.t('notify.FindUserByMobile'),
->>>>>>> feat: auth login error i18n
 }
