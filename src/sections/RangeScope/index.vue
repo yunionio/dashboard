a03fa69316614500a307b5273b0a82f396419239
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2023-01-05 19:10:32 +0800

    feat: #4086 账单汇总订阅新增账单数据范围和新通知类型角色

---
+++ src/sections/RangeScope/index.vue
@@ -0,0 +1,206 @@
+<template>
+  <div>
+    <a-radio-group v-model="range_scope" @change="billingScopeChangeHandler">
+      <a-radio-button
+        v-for="item in rangeScopeOptions"
+        :value="item.value"
+        :key="item.value">{{ item.label }}</a-radio-button>
+    </a-radio-group>
+    <!-- 域选择 -->
+    <a-select
+      v-if="isShowDomainSelect"
+      v-model="domain_id"
+      :loading="domainLoading"
+      :disabled="domainLoading"
+      style="width: 100%"
+      show-search
+      option-filter-prop="children"
+      :filter-option="filterDomainOption"
+      @search="handleDomainSearch"
+      @change="domainChangeHandler">
+      <a-select-option v-for="item in domainOptions" :value="item.value" :key="item.value">{{ item.label }}</a-select-option>
+    </a-select>
+    <!-- 项目选择 -->
+    <a-select
+      v-if="isShowProjectSelect"
+      v-model="project_id"
+      :loading="projectLoading"
+      :disabled="projectLoading"
+      style="width: 100%"
+      show-search
+      option-filter-prop="children"
+      :filter-option="filterProjectOption"
+      @search="handleDomainSearch"
+      @change="projectChangeHandler">
+      <a-select-option v-for="item in projectOptions" :value="item.value" :key="item.value">
+        <div class="d-flex">
+          <span class="text-truncate flex-fill mr-2" :title="item.label">{{ item.label }}</span>
+          <span style="color: #8492a6; font-size: 13px" v-if="isAdminMode && l3PermissionEnable">
+            {{ $t('common_257') }}{{ $t('dictionary.domain') }}: {{ item.project_domain }}
+          </span>
+        </div>
+      </a-select-option>
+    </a-select>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'RangeScope',
+  props: {
+    value: {
+      type: Object,
+    },
+    subscriptionScope: {
+      type: String,
+      default: 'system',
+    },
+  },
+  data () {
+    const value = this.value || {}
+    return {
+      range_scope: value.range_scope,
+      domain_id: value.domain_id,
+      project_id: value.project_id,
+      domainOptions: [],
+      domainLoading: false,
+      domainLoaded: true,
+      projectOptions: [],
+      projectLoading: false,
+      projectLoaded: true,
+    }
+  },
+  computed: {
+    ...mapGetters(['l3PermissionEnable', 'scope', 'isAdminMode', 'userInfo']),
+    rangeScopeOptions () {
+      const domainLabel = this.subscriptionScope === 'domain' ? this.$t('common.rangescope.domain.all') : this.$t('common.rangescope.domain')
+      const projectLabel = this.subscriptionScope === 'project' ? this.$t('common.rangescope.project.all') : this.$t('common.rangescope.project')
+      const options = [
+        { label: this.$t('common.rangescope.system'), value: 'system', scope: ['system'] },
+        { label: this.$t('common.rangescope.any_domain'), value: 'any_domain', scope: ['system'] },
+        { label: domainLabel, value: 'domain', scope: ['system', 'domain'] },
+        { label: this.$t('common.rangescope.any_project'), value: 'any_project', scope: ['system', 'domain'] },
+        { label: this.$t('common.rangescope.any_project_in_domain'), value: 'any_project_in_domain', scope: ['system'] },
+        { label: projectLabel, value: 'project', scope: ['system', 'domain', 'project'] },
+      ]
+      return options.filter(item => item.scope.includes(this.subscriptionScope))
+    },
+    isShowDomainSelect () {
+      return this.l3PermissionEnable &&
+      ['domain', 'any_project_in_domain'].includes(this.range_scope) &&
+      this.subscriptionScope === 'system'
+    },
+    isShowProjectSelect () {
+      return this.range_scope === 'project' && this.subscriptionScope !== 'project'
+    },
+  },
+  watch: {
+    value (val = {}) {
+      this.range_scope = val.range_scope
+      this.domain_id = val.domain_id
+      this.project_id = val.project_id
+    },
+    subscriptionScope (val) {
+      this.range_scope = val
+      this.triggerChange({ range_scope: val })
+    },
+  },
+  created () {
+    this.dm = new this.$Manager('domains', 'v1')
+    this.pm = new this.$Manager('projects', 'v1')
+    this.initData()
+  },
+  methods: {
+    initData () {
+      this.initDomainData((data) => {
+        if (data && data.length > 0) {
+          this.domainOptions = data
+          const domain_id = this.userInfo.projectDomainId
+          this.domain_id = domain_id
+          this.triggerChange({ domain_id })
+        }
+      })
+      this.initProjectData((data) => {
+        if (data && data.length > 0) {
+          this.projectOptions = data
+          const project_id = this.userInfo.projectId
+          this.project_id = project_id
+          this.triggerChange({ project_id })
+        }
+      })
+    },
+    async initDomainData (callback) {
+      this.domainLoading = true
+      this.domainLoaded = false
+      this.domainOptions = []
+
+      try {
+        const params = { limit: 0, scope: this.scope }
+        const response = await this.dm.list({ params })
+        const domainOptions = response.data.data.map(item => {
+          return {
+            label: item.name,
+            value: item.id,
+          }
+        })
+        this.domainLoaded = true
+        callback && callback(domainOptions)
+      } catch (error) {
+        throw error
+      } finally {
+        this.domainLoading = false
+      }
+    },
+    async initProjectData (callback) {
+      this.projectLoading = true
+      this.projectLoaded = false
+      this.projectOptions = []
+
+      try {
+        const params = { limit: 0, scope: this.scope }
+        const response = await this.pm.list({ params })
+        const projectOptions = response.data.data.map(item => {
+          return {
+            label: item.name,
+            value: item.id,
+            project_domain: item.project_domain,
+          }
+        })
+        this.projectLoaded = true
+        callback && callback(projectOptions)
+      } catch (error) {
+        throw error
+      } finally {
+        this.projectLoading = false
+      }
+    },
+    billingScopeChangeHandler (e) {
+      this.triggerChange({ range_scope: e.target.value })
+    },
+    domainChangeHandler (domain_id) {
+      this.triggerChange({ domain_id })
+    },
+    projectChangeHandler (project_id) {
+      this.triggerChange({ project_id })
+    },
+    triggerChange (changedValue) {
+      this.$emit('change', Object.assign({}, _.pick(this.$data, ['range_scope', 'domain_id', 'project_id']), changedValue))
+    },
+    filterDomainOption (input, option) {
+      return (
+        option.componentOptions.children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      )
+    },
+    filterProjectOption (input, option) {
+      return (
+        option.componentOptions.children[0].children[0].children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      )
+    },
+  },
+}
+</script>
+
+<style></style>
