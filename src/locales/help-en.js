27d4a36f590e468d0ea4d14ee986b222cdb1c80b
GuoLiBin6 <glbin533@163.com>
2023-02-20 11:47:05 +0800

    feat(4167): project detail add admin user

--- src/locales/help-en.js
+++ src/locales/help-en.js
@@ -69,6 +69,7 @@ export default {
     secgroupSystemGuestCnt: 'Number of virtual machines bound by security group as system resources',
     v2vTransferType: 'After the migration is successful, a new KVM virtual machine with the same configuration will be created automatically, the IP will be migrated to the new instance, and the original instance will be retained',
     billCostConversionRateTip: 'The resource price coefficient is the official website price of the product',
+    projectAdminTip: 'By default, the administrator owns the project under the current project_owner of the owner role, if you need to change, please go to IAM & Security - Project to modify the user and corresponding role of the project.',
   },
   notify: {
     TooManyRequests: 'TooManyRequests',
