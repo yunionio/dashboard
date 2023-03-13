af068d361361830714237a7e6b36692f481e7233
GuoLiBin6 <glbin533@163.com>
2023-03-13 15:23:29 +0800

    fix(8841): image recovery restore guest images error

--- containers/Compute/views/image-recovery/components/List.vue
+++ containers/Compute/views/image-recovery/components/List.vue
@@ -82,6 +82,7 @@ export default {
               data: this.list.selectedItems,
               columns: this.columns,
               refresh: this.refresh,
+              cloudEnv: this.cloudEnv,
             })
           },
           meta: () => {
