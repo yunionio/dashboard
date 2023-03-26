720f220a694b5cff12bb17845d12db9651460ece
ioito <qu_xuan@icloud.com>
2023-03-26 18:36:35 +0800

    feat: support h3c

---
+++ containers/Cloudenv/views/cloudaccount/create/form/components/H3C.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <a-form :form="form.fc" v-bind="formLayout">
+      <a-form-item :label="$t('cloudenv.text_95')">
+        <a-input v-decorator="decorators.name" :placeholder="$t('cloudenv.text_190')" />
+      </a-form-item>
+      <a-form-item :label="$t('common.description')">
+        <a-textarea :auto-size="{ minRows: 1, maxRows: 3 }" v-decorator="decorators.description" :placeholder="$t('common_367')" />
+      </a-form-item>
+      <a-form-item :label="$t('cloudenv.text_261')" :extra="this.$t('common_572')">
+        <a-input v-decorator="decorators.host" />
+      </a-form-item>
+      <a-form-item :label="$t('cloudenv.text_266')">
+        <a-input v-decorator="decorators.port" />
+      </a-form-item>
+      <a-form-item :label="keySecretField.label.k">
+        <a-input v-decorator="decorators.username" :placeholder="keySecretField.placeholder.k" />
+        <div slot="extra">
+           {{$t('cloudenv.text_236', [keySecretField.text, keySecretField.label.k])}}
+           <help-link :href="docs[provider.toLowerCase()]">{{$t('cloudenv.text_237')}}</help-link>
+        </div>
+      </a-form-item>
+      <a-form-item :label="keySecretField.label.s">
+        <a-input-password v-decorator="decorators.password" :placeholder="keySecretField.placeholder.s" />
+      </a-form-item>
+      <domain-project :fc="form.fc" :form-layout="formLayout" :decorators="{ project: decorators.project, domain: decorators.domain, auto_create_project: decorators.auto_create_project }" />
+      <proxy-setting :fc="form.fc" :fd="form.fd" ref="proxySetting" />
+      <auto-sync :fc="form.fc" :form-layout="formLayout" />
+      <share-mode :fd="form.fd" />
+    </a-form>
+  </div>
+</template>
+
+<script>
+import AutoSync from '@Cloudenv/views/cloudaccount/components/AutoSync'
+import ProxySetting from '@Cloudenv/views/cloudaccount/components/ProxySetting'
+import ShareMode from '@Cloudenv/views/cloudaccount/components/ShareMode'
+import { getCloudaccountDocs, keySecretFields } from '@Cloudenv/views/cloudaccount/constants'
+import { isRequired } from '@/utils/validate'
+import createMixin from './createMixin'
+import DomainProject from '../../../components/DomainProject'
+
+export default {
+  name: 'H3CCreate',
+  components: {
+    AutoSync,
+    DomainProject,
+    ProxySetting,
+    ShareMode,
+  },
+  mixins: [createMixin],
+  data () {
+    const keySecretField = keySecretFields[this.provider.toLowerCase()]
+    return {
+      docs: getCloudaccountDocs(this.$store.getters.scope),
+      decorators: {
+        name: [
+          'name',
+          {
+            validateFirst: true,
+            rules: [
+              { required: true, message: this.$t('cloudenv.text_190') },
+            ],
+          },
+        ],
+        description: ['description'],
+        host: [
+          'host',
+          {
+            validateFirst: true,
+            rules: [
+              { required: true, message: this.$t('cloudenv.text_268') },
+              { validator: this.$validate(['domain', 'IPv4'], true, 'some'), trigger: ['blur', 'change'], message: '请输入域名或者ip' },
+            ],
+          },
+        ],
+        port: [
+          'port',
+          {
+            initialValue: 11000,
+            rules: [
+              { type: 'number', min: 0, max: 65535, message: this.$t('cloudenv.text_270'), trigger: 'blur', transform: (v) => parseFloat(v) },
+            ],
+          },
+        ],
+        username: [
+          keySecretField.k,
+          {
+            rules: [
+              { required: true, message: keySecretField.placeholder.k },
+            ],
+          },
+        ],
+        password: [
+          keySecretField.s,
+          {
+            rules: [
+              { required: true, message: keySecretField.placeholder.s },
+            ],
+          },
+        ],
+        domain: [
+          'domain',
+          {
+            initialValue: this.$store.getters.userInfo.projectDomainId,
+            rules: [
+              { validator: isRequired(), message: this.$t('rules.domain'), trigger: 'change' },
+            ],
+          },
+        ],
+        auto_create_project: [
+          'auto_create_project',
+          {
+            initialValue: false,
+            valuePropName: 'checked',
+          },
+        ],
+      },
+      keepAliveFields: true,
+    }
+  },
+}
+</script>
