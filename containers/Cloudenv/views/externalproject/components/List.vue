ec80ab1fa5e9a5e66ac9d4a415f3976f00fc57a2
GuoLiBin6 <glbin533@163.com>
2023-04-21 10:54:55 +0800

    fix(8954): external project name edit limit brand

--- containers/Cloudenv/views/externalproject/components/List.vue
+++ containers/Cloudenv/views/externalproject/components/List.vue
@@ -210,14 +210,13 @@ export default {
           onManager: this.onManager,
           hideField: true,
           showDesc: false,
+          edit: this.isAllowCreate,
           formRules: [
             { required: true, message: this.$t('cloudenv.text_190') },
             { validator: this.$validate('externalProjectName') },
           ],
           slotCallback: row => {
-            return (
-              <side-page-trigger onTrigger={() => this.handleOpenSidepage(row)}>{ row.name }</side-page-trigger>
-            )
+            return row.name || '-'
           },
         }),
         {
