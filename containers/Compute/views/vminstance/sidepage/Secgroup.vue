05b55eb94083b58e6ca266c00b0204caaf8c331d
mj <boss_think@163.com>
2023-04-26 16:56:31 +0800

    fix(8957): 主机详情安全组对安全组的修改逻辑

--- containers/Compute/views/vminstance/sidepage/Secgroup.vue
+++ containers/Compute/views/vminstance/sidepage/Secgroup.vue
@@ -6,11 +6,15 @@
     :hiddenSidepageTabs="['vminstance-list']"
     :id="id"
     :getParams="getParams"
-    :show-create-action="false" />
+    :show-create-action="false"
+    :customSingleActions="customSingleActions"
+    frontGroupMethods="coverage"
+    :frontGroupActions="frontGroupActions" />
 </template>
 
 <script>
 import SecgroupList from '@Compute/views/secgroup/components/List'
+import WindowsMixin from '@/mixins/windows'
 import { SECGROUP_LIST_FOR_VMINSTANCE_SIDEPAGE_REFRESH } from '@/constants/event-bus'
 
 export default {
@@ -18,9 +22,69 @@ export default {
   components: {
     SecgroupList,
   },
+  mixins: [WindowsMixin],
   props: {
     id: String,
+    data: Object,
     getParams: [Function, Object],
+  },
+  data () {
+    const that = this
+    return {
+      frontGroupActions: function () {
+        return [
+          {
+            label: this.$t('compute.text_1116'),
+            action: () => {
+              this.createDialog('VmSetSecgroupDialog', {
+                vm: this,
+                data: [that.data],
+                columns: this.columns,
+                manager: new this.$Manager('servers'),
+              })
+            },
+          },
+          {
+            label: this.$t('common.remove'),
+            action: () => {
+              this.createDialog('VmSidepageRevokeSecgroupDialog', {
+                detailData: that.data,
+                data: this.list.selectedItems,
+                onManager: this.onManager,
+                refresh: () => {
+                  this.$bus.$emit(SECGROUP_LIST_FOR_VMINSTANCE_SIDEPAGE_REFRESH)
+                },
+              })
+            },
+            meta: () => {
+              return {
+                validate: this.list.selectedItems.length && this.list.selectedItems.length < this.list.total,
+              }
+            },
+          },
+        ]
+      },
+      customSingleActions: [
+        {
+          label: this.$t('common.remove'),
+          action: (obj) => {
+            this.createDialog('VmSidepageRevokeSecgroupDialog', {
+              detailData: this.data,
+              data: [obj],
+              onManager: this.onManager,
+              refresh: () => {
+                this.$bus.$emit(SECGROUP_LIST_FOR_VMINSTANCE_SIDEPAGE_REFRESH)
+              },
+            })
+          },
+          meta: () => {
+            return {
+              validate: this.$refs.secgroupList?.list?.total > 1,
+            }
+          },
+        },
+      ],
+    }
   },
   created () {
     this.$bus.$on(SECGROUP_LIST_FOR_VMINSTANCE_SIDEPAGE_REFRESH, () => {
