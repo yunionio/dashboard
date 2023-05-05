5801b1267a92e08dec2c28c2388ae15b918fc998
GuoLiBin6 <glbin533@163.com>
2023-05-05 11:28:03 +0800

    feat(4316): policy support add domain tags

--- containers/IAM/views/policy/sidepage/Detail.vue
+++ containers/IAM/views/policy/sidepage/Detail.vue
@@ -41,6 +41,25 @@ export default {
           },
         },
         {
+          field: 'domain_tags',
+          title: this.$t('iam.domain_tag'),
+          slots: {
+            default: ({ row }, h) => {
+              const ret = []
+              if (row.domain_tags) {
+                row.domain_tags.map(item => {
+                  const rgb = getTagColor(item.key, item.value, 'rgb')
+                  const strRgb = rgb.join(',')
+                  const style = `background:rgba(${strRgb},.1);border: solid 1px rgb(${strRgb});padding: 0px 6px 0 4px;line-height: 20px;margin-right:10px;overflow:hidden;max-width:100%;text-overflow: ellipsis;white-space: nowrap;`
+                  ret.push(<span class="tag text-truncate d-inline-block" style={style}>{item.key.replace('user:', '')}:{item.value}</span>)
+                })
+                return <div>{...ret}</div>
+              }
+              return '-'
+            },
+          },
+        },
+        {
           field: 'project_tags',
           title: this.$t('iam.project_tag'),
           slots: {
