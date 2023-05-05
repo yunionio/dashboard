a6ac99ac2744acd2cb6eb638618227ae736ea55d
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2023-05-05 19:39:33 +0800

    fix: 修复K8S集群新建磁盘报错的问题

--- containers/Compute/views/vminstance/create/components/SystemDisk.vue
+++ containers/Compute/views/vminstance/create/components/SystemDisk.vue
@@ -240,7 +240,7 @@ export default {
     },
     currentDiskCapability () {
       if (this.hypervisor !== HYPERVISORS_MAP.kvm.key) return {}
-      const instance_capabilities = this.capabilityData.instance_capabilities || []
+      const instance_capabilities = this.capabilityData?.instance_capabilities || []
       const storages = instance_capabilities.find(item => item.hypervisor === this.hypervisor)?.storages
       const sys_disk = storages.sys_disk || []
       const currentDisk = sys_disk.find(item => this.currentTypeObj.key?.startsWith(item.storage_type))
