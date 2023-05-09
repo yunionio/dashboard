1826eee616f447248eec3ceb2b927143a5932457
mj <gaomingjiu@yunion.cn>
2023-05-09 17:41:31 +0800

    feat(4318): 自定义的PCI设备类型

---
+++ containers/Compute/views/pci/components/PciList.vue
@@ -0,0 +1,118 @@
+<template>
+  <page-list
+    :list="list"
+    :columns="columns"
+    :group-actions="groupActions"
+    :single-actions="singleActions"
+    :export-data-options="exportDataOptions" />
+</template>
+
+<script>
+import WindowsMixin from '@/mixins/windows'
+import ListMixin from '@/mixins/list'
+import ColumnsMixin from '../mixins/columns'
+import SingleActionsMixin from '../mixins/singleActions'
+
+export default {
+  name: 'PciList',
+  mixins: [WindowsMixin, ListMixin, ColumnsMixin, SingleActionsMixin],
+  data () {
+    return {
+      list: this.$list.createList(this, {
+        id: 'pciList',
+        resource: 'isolated_device_models',
+        getParams: this.getParam,
+        filterOptions: {
+          dev_type: {
+            label: this.$t('compute.pci.dev_type'),
+            filter: true,
+            formatter: val => {
+              return `dev_type.contains("${val}")`
+            },
+          },
+          model: {
+            label: this.$t('compute.pci.model'),
+            filter: true,
+            formatter: val => {
+              return `model.contains("${val}")`
+            },
+          },
+          vendor_id: {
+            label: this.$t('compute.pci.vendor_id'),
+            filter: true,
+            formatter: val => {
+              return `vendor_id.contains("${val}")`
+            },
+          },
+          device_id: {
+            label: this.$t('compute.pci.device_id'),
+            filter: true,
+            formatter: val => {
+              return `device_id.contains("${val}")`
+            },
+          },
+        },
+      }),
+      exportDataOptions: {
+        items: [
+          { label: this.$t('compute.pci.dev_type'), key: 'dev_type' },
+          { label: this.$t('compute.pci.model'), key: 'model' },
+          { label: this.$t('compute.pci.vendor_id'), key: 'vendor_id' },
+          { label: this.$t('compute.pci.device_id'), key: 'device_id' },
+        ],
+      },
+      columns: [],
+      groupActions: [
+        {
+          label: this.$t('common.create'),
+          action: () => {
+            this.createDialog('PciCreateDialog', {
+              refresh: this.refresh,
+            })
+          },
+          meta: () => ({
+            buttonType: 'primary',
+          }),
+        },
+        {
+          label: this.$t('table.action.delete'),
+          action: () => {
+            this.createDialog('DeleteResDialog', {
+              vm: this,
+              data: this.list.selectedItems,
+              columns: this.columns,
+              title: this.$t('table.action.delete'),
+              name: this.$t('compute.pci.pass_through_device_type'),
+              onManager: this.onManager,
+            })
+          },
+          meta: () => {
+            const ret = { validate: this.list.selected.length }
+            if (this.list.selectedItems.some(item => !item.can_delete)) {
+              ret.validate = false
+              return ret
+            }
+            return ret
+          },
+        },
+      ],
+    }
+  },
+  created () {
+    this.list.fetchData()
+  },
+  methods: {
+    getParam () {
+      const ret = {
+        with_meta: true,
+        details: true,
+      }
+      return ret
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
