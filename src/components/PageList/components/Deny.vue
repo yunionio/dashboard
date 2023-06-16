912a9a333f155d5a4659aae394ad29d261554942
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2023-06-16 17:26:48 +0800

    feat: 列表支持资源权限控制

---
+++ src/components/PageList/components/Deny.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrap d-flex justify-content-center align-items-center">
+    <div>
+      <div class="empty-image">
+        <img src="../../../assets/images/empty.svg" />
+      </div>
+      <div class="empty-description">{{ $t('common.permission.403') }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Deny',
+  props: {
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.wrap {
+  margin: 32px 0;
+  color: rgba(0, 0, 0, .25);
+}
+.image {
+  height: 40px;
+}
+</style>
