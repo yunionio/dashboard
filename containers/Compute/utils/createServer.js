efa8fb729e9a98c5282c34df046e6a71e0241113
ioito <qu_xuan@icloud.com>
2023-06-29 15:51:03 +0800

    fix: support pve create

--- containers/Compute/utils/createServer.js
+++ containers/Compute/utils/createServer.js
@@ -799,7 +799,7 @@ export class GenCreateData {
       backend: item.type === STORAGE_AUTO.key ? '' : item.type,
       size: item.size * 1024,
     }
-    if (type === 'sys' && this.fd.imageType !== IMAGES_TYPE_MAP.iso.key) {
+    if (type === 'sys' && this.fd.imageType !== IMAGES_TYPE_MAP.iso.key && this.fd.hypervisor !== HYPERVISORS_MAP.proxmox.key) {
       ret.image_id = this.fd.image.key
     }
     if (type === 'sys' && this.fd.imageType === IMAGES_TYPE_MAP.iso.key && this.isWindows()) {
@@ -1297,7 +1297,7 @@ export class GenCreateData {
       data.billing_type = 'postpaid'
     }
     // 镜像类型为 iso 需要加参数 cdrom
-    if (this.fd.imageType === IMAGES_TYPE_MAP.iso.key) {
+    if ((this.fd.imageType === IMAGES_TYPE_MAP.iso.key) || (this.fd.hypervisor === HYPERVISORS_MAP.proxmox.key)) {
       data.cdrom = this.fd.image.key
     }
     // 主机镜像需要guest image id参数，并且把磁盘中的镜像ID回填回去
