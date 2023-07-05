dc089c296acdfe520210f160a4008ad1d23348ab
mj <gaomingjiu@yunion.cn>
2023-07-05 14:31:27 +0800

    feat(4358): pve 支持新建

--- containers/Compute/utils/createServer.js
+++ containers/Compute/utils/createServer.js
@@ -1297,7 +1297,7 @@ export class GenCreateData {
       data.billing_type = 'postpaid'
     }
     // 镜像类型为 iso 需要加参数 cdrom
-    if ((this.fd.imageType === IMAGES_TYPE_MAP.iso.key) || (this.fd.hypervisor === HYPERVISORS_MAP.proxmox.key)) {
+    if ((this.fd.imageType === IMAGES_TYPE_MAP.iso.key)) {
       data.cdrom = this.fd.image.key
     }
     // 主机镜像需要guest image id参数，并且把磁盘中的镜像ID回填回去
