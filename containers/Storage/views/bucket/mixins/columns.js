6859b48bd4abe8735ee599fa1e954899ae0dd203
gaomingjiu@yunion.cn <gaomingjiu@yunion.cn>
2023-07-07 15:56:14 +0800

    feat(4391): Bucket列表支持隐藏列表项

--- containers/Storage/views/bucket/mixins/columns.js
+++ containers/Storage/views/bucket/mixins/columns.js
@@ -18,15 +18,35 @@ export default {
             <side-page-trigger onTrigger={ () => this.handleOpenSidepage(row) }>{ row.name_cn ? `${row.name}(${row.name_cn})` : row.name }</side-page-trigger>
           )
         },
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.name')
+        },
       }),
-      getStatusTableColumn({ statusModule: 'bucket', vm: this }),
-      getTagTableColumn({ onManager: this.onManager, resource: 'buckets', columns: () => this.columns, tipName: this.$t('dictionary.bucket') }),
+      getStatusTableColumn({
+        statusModule: 'bucket',
+        vm: this,
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.status')
+        },
+      }),
+      getTagTableColumn({
+        onManager: this.onManager,
+        resource: 'buckets',
+        columns: () => this.columns,
+        tipName: this.$t('dictionary.bucket'),
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.metadata')
+        },
+      }),
       {
         field: 'storage_class',
         title: i18n.t('storage.text_38'),
         width: 120,
         formatter: ({ row }) => {
           return row.storage_class || '-'
+        },
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.storage_class')
         },
       },
       {
@@ -35,6 +55,9 @@ export default {
         width: 120,
         formatter: ({ row }) => {
           return ACL_TYPE[row.acl] || row.acl || '-'
+        },
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.acl')
         },
       },
       // {
@@ -49,12 +72,38 @@ export default {
       //     },
       //   },
       // },
-      getBrandTableColumn(),
-      getAccountTableColumn(),
-      getPublicScopeTableColumn({ vm: this, resource: 'buckets' }),
-      getProjectTableColumn(),
-      getRegionTableColumn(),
-      getTimeTableColumn(),
+      getBrandTableColumn({
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.brand')
+        },
+      }),
+      getAccountTableColumn({
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.account')
+        },
+      }),
+      getPublicScopeTableColumn({
+        vm: this,
+        resource: 'buckets',
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.public_scope')
+        },
+      }),
+      getProjectTableColumn({
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.tenant')
+        },
+      }),
+      getRegionTableColumn({
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.region')
+        },
+      }),
+      getTimeTableColumn({
+        hidden: () => {
+          return this.$isScopedPolicyMenuHidden('oss_hidden_columns.created_at')
+        },
+      }),
     ]
   },
 }
