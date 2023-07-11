f47b1658717b04be2b8855478e2cbf888683324a
mj <gaomingjiu@yunion.cn>
2023-07-11 16:29:49 +0800

    feat(4352): 项目和域选择控件应该记忆用户的选择

---
+++ src/store/modules/storage.js
@@ -0,0 +1,43 @@
+import storage from '@/utils/storage'
+import http from '@/utils/http'
+
+const STORAGE_DOMAIN_KEY = '__oc_selected_domain__'
+const STORAGE_PROJECT_KEY = '__oc_selected_project__'
+
+export default {
+  state: {
+    domain: storage.get(STORAGE_DOMAIN_KEY) || {},
+    project: storage.get(STORAGE_PROJECT_KEY) || {},
+  },
+  mutations: {
+    SET_DOMAIN (state, domain) {
+      storage.set(STORAGE_DOMAIN_KEY, domain)
+      state.domain = domain
+    },
+    SET_PROJECT (state, project) {
+      storage.set(STORAGE_PROJECT_KEY, project)
+      state.project = project
+    },
+  },
+  actions: {
+    async getDomainById ({ commit }, payload = {}) {
+      try {
+        const { data } = await http.get('/v1/domains/' + payload.key)
+        return Promise.resolve(data)
+      } catch (err) {
+        commit('SET_DOMAIN', {})
+      }
+    },
+    async getProjectById ({ commit, state }, payload = {}) {
+      try {
+        const { data } = await http.get('/v1/projects/' + payload.key, { params: { project_domain: state.domain?.key } })
+        if (data) {
+          commit('SET_PROJECT', payload)
+        }
+        return Promise.resolve(data)
+      } catch (err) {
+        commit('SET_PROJECT', {})
+      }
+    },
+  },
+}
