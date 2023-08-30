f2001c132851506bc155466418e0c95a5bc0957a
GuoLiBin6 <glbin533@163.com>
2023-08-30 16:27:24 +0800

    feat(4442): notify add notification time

--- containers/IAM/views/notify-topic/sidepage/Detail.vue
+++ containers/IAM/views/notify-topic/sidepage/Detail.vue
@@ -12,11 +12,14 @@
 </template>
 
 <script>
+import WindowsMixin from '@/mixins/windows'
+import ColumnsMixin from '../mixins/columns'
 import { NOTIFY_TOPIC_NAMES_MAP, NOTIFY_TOPIC_TYPES_MAP } from '@IAM/constants'
 import { getEnabledTableColumn } from '@/utils/common/tableColumn'
 
 export default {
   name: 'TopicDetail',
+  mixins: [WindowsMixin, ColumnsMixin],
   props: {
     data: {
       type: Object,
@@ -66,8 +69,45 @@ export default {
             },
           ],
         },
+        {
+          title: this.$t('iam.notify_time'),
+          items: [
+            {
+              field: 'advance_days',
+              title: this.$t('iam.notify_time'),
+              slots: {
+                default: ({ row }) => {
+                  const { advance_days = [] } = row
+                  if (!advance_days.length) return '-'
+                  const ret = []
+                  if (advance_days.length === 1) {
+                    ret.push(<span>{this.$t('iam.notify_one_day', [this.$t('iam.notify_time.some_day', [advance_days[0]])])}</span>)
+                  } else {
+                    const dayList = []
+                    advance_days.map(day => {
+                      dayList.push(this.$t('iam.notify_time.some_day', [day]))
+                    })
+                    ret.push(<span>{this.$t('iam.notify_some_day', [dayList.join('、')])}</span>)
+                  }
+                  ret.push(<a-icon type='edit' class='edit-icon primary-color ml-2' onclick={this.updateNotifyTime} />)
+                  return ret
+                },
+              },
+            },
+          ],
+          hidden: () => !['resource release', 'password expire'].includes(this.data.name),
+        },
       ],
     }
+  },
+  methods: {
+    updateNotifyTime () {
+      this.createDialog('NotifyTopicUpdateTimeDialog', {
+        onManager: this.onManager,
+        data: [this.data],
+        columns: this.columns,
+      })
+    },
   },
 }
 </script>
