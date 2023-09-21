53416f8f1ee4a918a126a6cc4f5a667232b6be69
GuoLiBin6 <glbin533@163.com>
2023-09-21 16:37:34 +0800

    feat(4533): 部署虚拟机应用实例支持选择镜像

---
+++ src/components/JsonSchemaForm/JComponents/JImageList/index.js
@@ -0,0 +1,8 @@
+import JImageList from './JImageList'
+
+/* istanbul ignore next */
+JImageList.install = function (Vue) {
+  Vue.component(JImageList.name, JImageList)
+}
+
+export default JImageList
