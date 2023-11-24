341f0a1770cb0e9b221cd7aab86c379715a42de7
easy-mj <boss_think@163.com>
2023-11-24 16:01:16 +0800

    fix(9452): 新建主机操作系统列，镜像类型增加一个全部的选项，镜像列表不过滤

--- containers/Compute/views/vminstance/dialogs/RebuildRoot.vue
+++ containers/Compute/views/vminstance/dialogs/RebuildRoot.vue
@@ -1,5 +1,5 @@
 <template>
-  <base-dialog @cancel="cancelDialog">
+  <base-dialog @cancel="cancelDialog" :width="1000">
     <div slot="header">{{action}}</div>
     <div slot="body">
       <a-alert class="mb-2" type="warning" v-if="tips">
