48de5c24a29cceca39012ac0baf00b9fad3246ff
GuoLiBin6 <glbin533@163.com>
2023-11-30 18:13:16 +0800

    fix: 主机创建备份操作隐藏

--- containers/Compute/views/vminstance/constants/actions.js
+++ containers/Compute/views/vminstance/constants/actions.js
@@ -1327,7 +1327,7 @@ const getSingleActions = function () {
                   ret.tooltip = cloudUnabledTip('createBackup', obj)
                   return ret
                 },
-                hidden: () => !hasSetupKey(['onecloud']),
+                hidden: () => !hasSetupKey(['onecloud']) || this.$isScopedPolicyMenuHidden('vminstance_hidden_menus.server_perform_create_backup'),
               },
             ],
           },
