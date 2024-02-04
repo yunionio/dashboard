974863f70978803bc6aa593b667886010a10f676
easy-mj <boss_think@163.com>
2024-02-04 19:28:22 +0800

    fix: 云账号屏蔽资源类型缺失导致页面报错

--- containers/Cloudenv/constants/index.js
+++ containers/Cloudenv/constants/index.js
@@ -11,6 +11,7 @@ export const BLOCKED_RESOURCES = [
   { label: i18n.t('dictionary.nat'), value: 'natgateway' },
   { label: i18n.t('dictionary.file_system'), value: 'file_system' },
   { label: i18n.t('scope.text_79'), value: 'kubecluster' },
+  { label: i18n.t('dictionary.dbinstancebackups'), value: 'dbinstancebackups' },
 ]
 
 export const BLOCKED_RESOURCES_MAP = arrayToObj(BLOCKED_RESOURCES, 'value')
