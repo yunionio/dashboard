a31173e633592d2b23d152e1ae23b280f0bf1a0c
easy-mj <boss_think@163.com>
2024-02-23 19:36:46 +0800

    fix: update rescue status

--- src/constants/expectStatus.js
+++ src/constants/expectStatus.js
@@ -7,7 +7,7 @@ export default {
     danger: ['create_failed', 'delete_failed', 'update_tags_fail'],
   },
   server: {
-    success: ['running', 'on', 'rescving'],
+    success: ['running', 'on', 'rescue'],
     info: ['ready', 'deallocated', 'unknown', 'suspend', 'converted', 'stopped', 'off'],
     // danger: [new RegExp('fail')] // 这条会在 base组件 中默认存在
   },
