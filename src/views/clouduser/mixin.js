537c0a04cd84da9b15c4861ae4a6a6299a02f728
Qu Xuan <qu_xuan@icloud.com>
2024-03-27 13:27:03 +0800

    fix(cloudid): refactor cloud iam

--- src/views/clouduser/mixin.js
+++ src/views/clouduser/mixin.js
@@ -1,11 +1,11 @@
 export default {
   methods: {
     async samlLogin (row) {
-      let manager = new this.$Manager('cloudaccounts')
+      let manager = new this.$Manager('cloudgroups')
       const hiddenLoadingMessage = this.$message.loading(this.$t('smal_login_message.loading'), 0)
       try {
         const response = await manager.getSpecific({
-          id: row.cloudaccount_id,
+          id: row.cloudgroup_id,
           spec: 'saml',
         })
         if (response.data.init_login_url) {
