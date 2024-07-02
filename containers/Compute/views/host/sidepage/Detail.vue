81b3da1c6ea0ab35a39deb0b24fc1ec6d0644aa6
QIU Jian <qiujian@yunionyun.com>
2024-07-02 21:37:45 +0800

    fix: adjust host details storage columes widths

--- containers/Compute/views/host/sidepage/Detail.vue
+++ containers/Compute/views/host/sidepage/Detail.vue
@@ -202,17 +202,18 @@ export default {
         {
           field: 'adapter',
           title: this.$t('compute.text_579'),
+          width: 70,
         },
         {
           field: 'driver',
           title: this.$t('compute.text_378'),
-          width: 80,
+          width: 140,
         },
         {
           field: 'model',
           title: this.$t('compute.text_580'),
           showOverflow: 'ellipsis',
-          minWidth: 100,
+          minWidth: 200,
         },
         {
           field: 'rotate',
@@ -239,7 +240,7 @@ export default {
         {
           field: 'ip_addr',
           title: 'IP',
-          width: 150,
+          width: 160,
         },
         {
           field: 'mac',
