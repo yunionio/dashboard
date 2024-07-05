3d735391adde087f37b67b32d663a9be9b2581ba
GuoLiBin6 <glbin533@163.com>
2024-07-05 10:01:20 +0000

    feat(v3.12/4611): cloudpods support select ios images to create vminstance

--- containers/Compute/views/vminstance/create/form/Private.vue
+++ containers/Compute/views/vminstance/create/form/Private.vue
@@ -327,12 +327,11 @@ export default {
     },
     cloudproviderParamsExtra () {
       const params = {
-        cloud_env: 'private',
         manager_id: this.form.fd.cloudprovider,
         ...this.scopeParams,
       }
-      if (this.form.fd.hypervisor && this.form.fd.hypervisor) {
-        params.provider = HYPERVISORS_MAP[this.form.fd.hypervisor].provider
+      if (this.cloudregionZoneParams.cloudregion) {
+        params.cloudregion_id = this.cloudregionZoneParams.cloudregion
       }
       return params
     },
