f3b4b4eef74970a131f6ad5a2a320aa720508859
GuoLiBin6 <glbin533@163.com>
2024-11-08 17:47:05 +0800

    feat: 容器支持查看源数据操作

---
+++ containers/Compute/views/pod-container/dialogs/ViewInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <base-dialog @cancel="cancelDialog">
+    <div slot="header">{{$t('compute.view_source_data')}}</div>
+    <div slot="body">
+      <code-mirror v-model="yamlInfo" :options="cmOptions" />
+    </div>
+    <div slot="footer">
+      <a-button @click="cancelDialog">{{ $t('dialog.cancel') }}</a-button>
+    </div>
+  </base-dialog>
+</template>
+
+<script>
+import jsYaml from 'js-yaml'
+import DialogMixin from '@/mixins/dialog'
+import WindowsMixin from '@/mixins/windows'
+
+export default {
+  name: 'ContainerViewDataInfoDialog',
+  components: {
+  },
+  mixins: [DialogMixin, WindowsMixin],
+  data () {
+    return {
+      loading: false,
+      cmOptions: {
+        tabSize: 2,
+        styleActiveLine: true,
+        lineNumbers: true,
+        line: true,
+        mode: 'text/x-yaml',
+        theme: 'material',
+      },
+      yamlInfo: jsYaml.safeDump(this.params.data[0]),
+    }
+  },
+  methods: {
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.CodeMirror {
+  height: 500px;
+}
+</style>
--- containers/Compute/views/pod-container/mixins/singleActions.js
+++ containers/Compute/views/pod-container/mixins/singleActions.js
@@ -70,6 +70,16 @@ export default {
                 return ret
               },
             },
+            {
+              label: this.$t('compute.view_source_data'),
+              action: (obj) => {
+                this.createDialog('ContainerViewDataInfoDialog', {
+                  data: [obj],
+                  columns: this.columns,
+                  onManager: this.onManager,
+                })
+              },
+            },
           ]
         },
       },
