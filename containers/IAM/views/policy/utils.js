0665cecbf7be9b8658fc80616608bc8178b90317
GuoLiBin6 <glbin533@163.com>
2024-11-13 18:12:51 +0800

    feat: 扩展权限翻译方法

--- containers/IAM/views/policy/utils.js
+++ containers/IAM/views/policy/utils.js
@@ -2,12 +2,22 @@ import i18n from '@/locales'
 import { PERMISSION as POLICY_MAP } from '@/constants/permission'
 import { RESOURCES_MAP, SERVICES_MAP } from './constants'
 
-export function transformLabel (config) {
+/**
+ * 翻译权限展示名
+ * 1. 优先查找 SERVICES_MAP RESOURCES_MAP 指定的名
+ * 2. 查找 i18n 中 `${service}.${resourceKey}` 的名称
+ * 3. 查找 i18n 中 `dictionary.${resourceKey}` 的名称
+ * 4. 都找不到时，返回 resourceKey
+ * 建议：ce 中涉及的，可在 dictionary 和 SERVICES_MAP、RESOURCES_MAP 添加，ee 和 oem 涉及的，添加至 该模块`${service}.${resourceKey}` 即可
+ */
+export function transformLabel (config, resourceKey, service) {
   if (config) {
     if (config.i18n) return i18n.t(config.i18n)
     return config.label
+  } else if (service && i18n.te(`${service}.${resourceKey}`)) {
+    return i18n.t(`${service}.${resourceKey}`)
   }
-  return null
+  return i18n.te(`dictionary.${resourceKey}`) ? i18n.t(`dictionary.${resourceKey}`) : resourceKey
 }
 
 /**
@@ -25,7 +35,7 @@ export function genPolicyGroups () {
     // 如果没有声明初始化进行
     if (!temp[item[0]]) {
       temp[item[0]] = {
-        label: transformLabel(SERVICES_MAP[item[0]]) || item[0],
+        label: transformLabel(SERVICES_MAP[item[0]], item[0]),
         service: item[0],
         resources: [],
       }
@@ -34,7 +44,7 @@ export function genPolicyGroups () {
     if (!resources.includes(`${item[0]}_${item[1]}`)) {
       resources.push(`${item[0]}_${item[1]}`)
       const res = {
-        label: transformLabel(RESOURCES_MAP[item[1]]) || item[1],
+        label: transformLabel(RESOURCES_MAP[item[1]], item[1], item[0]),
         resource: item[1],
       }
       if (RESOURCES_MAP[item[1]] && RESOURCES_MAP[item[1]].extras) {
