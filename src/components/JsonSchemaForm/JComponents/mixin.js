7a38edd22d74350e6724da1380e42a38e2c87359
GuoLiBin6 <glbin533@163.com>
2024-11-20 20:35:04 +0800

    fix: 虚拟机实例azure套餐和镜像选择添加适配规则

--- src/components/JsonSchemaForm/JComponents/mixin.js
+++ src/components/JsonSchemaForm/JComponents/mixin.js
@@ -2,6 +2,32 @@ export default {
   props: {
     value: {},
   },
+  data () {
+    return {
+      centos_Generation1_ignore_sku_filters: [
+        /^Standard_DC.*v2$/,
+        /^Standard_DC.*as_v5$/,
+        /^Standard_D.*ps_v5$/,
+        /^Standard_EC.*as_v5$/,
+        /^Standard_E.*ps_v5$/,
+        /^Standard_FX.*mds$/,
+        /^Standard_M.*s_v2.*/,
+        /^Standard_NC.*ads_A100_v4$/,
+        /^Standard_ND.*asr_A100_v4$/,
+        /^Standard_ND.*rs_v2$/,
+        /^Standard_ND.*amsr_A100_v4$/,
+      ],
+      centos_Generation2_ignore_sku_filters: [
+        /^Standard_A.*v2$/,
+        /^Standard_D.*v2$/,
+        /^Standard_D.*v3$/,
+        /^Standard_E.*v3.*/,
+        /^Standard_NC[6,12,24,24r]+$/,
+        /^Standard_NV[6,12,24]+$/,
+        /^Standard_NP[10,20,40]+s$/,
+      ],
+    }
+  },
   inject: ['scopeParams', 'formFd', 'extendFd'],
   methods: {
     change (...ret) {
