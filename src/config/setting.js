e7d0aa76593af4a05315741e8fd4378e4c9a7048
GuoLiBin6 <glbin533@163.com>
2024-12-23 10:13:28 +0800

    feat: 允许打包注入当前平台信息

--- src/config/setting.js
+++ src/config/setting.js
@@ -9,6 +9,7 @@ export default {
   themeColor: storage.get('__oc_theme_color__') || process.env.THEME_COLOR || '#1890FF',
   theme: storage.get('__oc_theme__') || process.env.THEME || 'dark',
   brand: process.env.BRAND || { 'zh-CN': '内置虚拟化', en: 'Buiilt-in Virtualization', 'ja-JP': '内蔵バーチャライゼーション' },
+  product: process.env.PRODUCT || (process.env.VUE_APP_IS_PRIVATE ? { 'zh-CN': '云管平台', en: 'Cloud Management Platform', 'ja-JP': 'クラウド管理プラットフォーム' } : { 'zh-CN': 'Cloudpods', en: 'Cloudpods', 'ja-JP': 'Cloudpods' }),
   l2MenuVisible: !R.isNil(_l2MenuVisible) && !R.isNil(_l2MenuVisible) ? _l2MenuVisible : true,
   monitorAlertNotifyTriggerTime: process.env.VUE_APP_MONITOR_ALERT_NOTIFY_TRIGGER_TIME || 1000 * 60 * 60, // 默认值1小时
   oemVersion: process.env.OEM_VERSION || process.env.VUE_APP_OEM_VERSION || '',
