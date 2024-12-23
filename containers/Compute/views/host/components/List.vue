67ea78c992e608b04de929912ac4e6474b9748b6
GuoLiBin6 <glbin533@163.com>
2024-12-23 11:51:25 +0800

    feat: 修改宿主机和存储设置超售比api

--- containers/Compute/views/host/components/List.vue
+++ containers/Compute/views/host/components/List.vue
@@ -301,7 +301,7 @@ export default {
                 },
                 {
                   label: this.$t('compute.text_513'),
-                  permission: 'hosts_update',
+                  permission: 'hosts_update,hosts_perform_set_commit_bound',
                   action: () => {
                     this.createDialog('HostAdjustOversoldRatioDialog', {
                       data: this.list.selectedItems,
