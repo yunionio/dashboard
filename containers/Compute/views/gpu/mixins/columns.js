e6368840514f0c9ddae1d92a0662ec5e74dd6af3
QIU Jian <qiujian@yunionyun.com>
2024-12-30 01:40:56 +0800

    fix: add more columns to isolated-devices

--- containers/Compute/views/gpu/mixins/columns.js
+++ containers/Compute/views/gpu/mixins/columns.js
@@ -1,12 +1,12 @@
-import {
-  getReserveResourceColumn,
-} from '../utils/columns'
+import i18n from '@/locales'
 import {
   getNameDescriptionTableColumn,
   getCopyWithContentTableColumn,
   getRegionTableColumn,
 } from '@/utils/common/tableColumn'
-import i18n from '@/locales'
+import {
+  getReserveResourceColumn,
+} from '../utils/columns'
 
 export default {
   created () {
@@ -28,6 +28,7 @@ export default {
         field: 'dev_type',
         title: i18n.t('compute.text_481'),
         width: 120,
+        showOverflow: 'ellipsis',
       },
       {
         field: 'model',
@@ -47,6 +48,63 @@ export default {
               </div>,
             ]
           },
+        },
+      },
+      {
+        field: 'vendor_device_id',
+        title: 'PCI ID',
+        width: 120,
+        showOverflow: 'ellipsis',
+        slots: {
+          default: ({ row }, h) => {
+            if (row.vendor_device_id) {
+              if (row.vendor_device_id === '0000:0000') {
+                return row.vendor_device_id
+              } else {
+                const url = 'https://admin.pci-ids.ucw.cz/read/PC/' + row.vendor_device_id.replace(':', '/')
+                return [
+                  <list-body-cell-wrap copy hideField={true} field='vendor_device_id' row={row} message={row.vendor_device_id}>
+                    <div class='d-flex'>
+                      <span class='text-truncate'>{ row.vendor_device_id }</span>
+                      <a href={ url } target="pciid"><a-icon type="link" /></a>
+                    </div>
+                  </list-body-cell-wrap>,
+                ]
+              }
+            }
+          },
+        },
+      },
+      {
+        field: 'device_path',
+        title: i18n.t('compute.isolated_devices.device_path.title'),
+        width: 120,
+        showOverflow: 'ellipsis',
+        slots: {
+          default: ({ row }, h) => {
+            const ret = []
+            if (row.device_path) {
+              ret.push(
+                [
+                  <list-body-cell-wrap copy hideField={true} field='device_path' row={row} message={row.device_path}>
+                    {row.device_path}
+                  </list-body-cell-wrap>,
+                ],
+              )
+            }
+            return ret
+          },
+        },
+      },
+      {
+        field: 'numa_node',
+        title: 'NUMA',
+        width: 80,
+        formatter: function ({ row }) {
+          if (row.numa_node >= 0) {
+            return row.numa_node
+          }
+          return '-'
         },
       },
       {
