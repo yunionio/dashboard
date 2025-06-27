97e045efd7b5e86683f463608f62f73fe9f18216
GuoLiBin6 <glbin533@163.com>
2025-06-27 11:13:34 +0800

    feat: nas权限组创建参数

--- containers/Storage/views/access-group/dialogs/Create.vue
+++ containers/Storage/views/access-group/dialogs/Create.vue
@@ -8,6 +8,27 @@
         <a-form-item :label="$t('storage.text_40')">
           <a-input v-decorator="decorators.name" :placeholder="$t('storage.text_56')" />
         </a-form-item>
+        <area-selects
+          class="mb-0"
+          ref="areaSelects"
+          :wrapperCol="formItemLayout.wrapperCol"
+          :labelCol="formItemLayout.labelCol"
+          :names="['provider', 'cloudregion']"
+          :cloudregionParams="cloudregionParams"
+          :isRequired="true"
+          :providerParams="providerParams"
+          filterBrandResource="nas"
+          @change="handleRegionChange" />
+        <a-form-item :label="$t('compute.text_15')">
+        <base-select
+          resource="cloudproviders"
+          v-decorator="decorators.cloudprovider"
+          :params="cloudproviderParams"
+          :isDefaultSelect="true"
+          :showSync="true"
+          :select-props="{ placeholder: $t('compute.text_149') }"
+          @update:item="cloudproviderSelected" />
+        </a-form-item>
         <a-form-item :label="$t('common.description')">
           <a-textarea :auto-size="{ minRows: 1, maxRows: 3 }" v-decorator="decorators.description" :placeholder="$t('common_367')" />
         </a-form-item>
@@ -24,10 +45,12 @@
 import DialogMixin from '@/mixins/dialog'
 import WindowsMixin from '@/mixins/windows'
 import { isRequired } from '@/utils/validate'
+import AreaSelects from '@/sections/AreaSelects'
 
 export default {
   name: 'AccessGroupCreateDialog',
   components: {
+    AreaSelects,
   },
   mixins: [DialogMixin, WindowsMixin],
   data () {
@@ -55,6 +78,12 @@ export default {
             ],
           },
         ],
+        cloudprovider: [
+          'cloudprovider',
+          {
+            rules: [{ required: true, message: this.$t('common.tips.input', [this.$t('storage.text_40')]) }],
+          },
+        ],
         description: ['description'],
       },
       formItemLayout: {
@@ -65,9 +94,42 @@ export default {
           span: 4,
         },
       },
+      cloudRegionParams: {
+        scope: this.$store.getters.scope,
+        limit: 20,
+        usable: false,
+        cloud_env: 'public',
+      },
+      providerParams: {
+        scope: this.$store.getters.scope,
+        usable: false,
+      },
+      regionProvider: '',
+      regionId: '',
     }
   },
+  computed: {
+    cloudproviderParams () {
+      const params = {
+        limit: 20,
+        // brand: this.form.fd.provider,
+        cloudregion: this.regionId,
+        enabled: true,
+        read_only: false,
+        scope: this.$store.getters.scope,
+      }
+      return params
+    },
+  },
   methods: {
+    handleRegionChange (data) {
+      const { cloudregion } = data
+      if (cloudregion) {
+        const { provider } = cloudregion.value
+        this.regionProvider = provider
+        this.regionId = cloudregion.id
+      }
+    },
     doCreate (data) {
       return new this.$Manager('access_groups').create({ data: data })
     },
@@ -75,7 +137,7 @@ export default {
       this.loading = true
       try {
         const values = await this.form.fc.validateFields()
-        await this.doCreate(values)
+        await this.doCreate({ ...values, cloudregion: this.regionId })
         this.loading = false
         this.cancelDialog()
         this.params.refresh && this.params.refresh()
