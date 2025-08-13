247c9e66d079fb8a842a5fdda94c7ca08540957c
GuoLiBin6 <glbin533@163.com>
2025-08-13 19:38:33 +0800

    feat: 自动快照策略支持修改

--- containers/Compute/views/snapshotpolicy/mixins/singleActions.js
+++ containers/Compute/views/snapshotpolicy/mixins/singleActions.js
@@ -3,6 +3,18 @@ export default {
   created () {
     this.singleActions = [
       {
+        label: i18n.t('table.action.modify'),
+        permission: 'snapshotpolicy_update',
+        action: obj => {
+          this.createDialog('CreateSnapshotPolicyDialog', {
+            type: 'update',
+            data: [obj],
+            onManager: this.onManager,
+            columns: this.columns,
+          })
+        },
+      },
+      {
         label: i18n.t('compute.bind_resource'),
         permission: 'disks_perform_bind_snapshotpolicy,servers_perform_bind_snapshotpolicy,snapshotpolicy_perform_bind_resources,snapshotpolicy_perform_unbind_resources',
         action: obj => {
