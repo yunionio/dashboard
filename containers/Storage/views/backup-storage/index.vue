4d4e4426e67fdd9adf9249abcf67d02e18c01670
GuoLiBin6 <glbin533@163.com>
2025-08-25 15:31:16 +0800

    feat: 资源增加列表状态统计

--- containers/Storage/views/backup-storage/index.vue
+++ containers/Storage/views/backup-storage/index.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    <page-header :title="$t('compute.backup_storage')" />
+    <page-header
+      :title="$t('compute.backup_storage')"
+      isShowResStatusTab
+      :status-opts="statusOpts"
+      :status-click-handle="statusClickHandle" />
     <page-body>
       <backup-storage-list
-        :id="listId" />
+        :id="listId"
+        :cloudEnvOptions="cloudEnvOptions"
+        :filterParams="filterParams"
+        statusResKey="backupStorage"
+        @resStatisticsChange="resStatisticsChange" />
     </page-body>
   </div>
 </template>
 
 <script>
+import ResStatisticsV2Mixin from '@/mixins/resStatisticsV2Mixin'
 import BackupStorageList from './components/List'
 
 export default {
@@ -16,9 +25,12 @@ export default {
   components: {
     BackupStorageList,
   },
+  mixins: [ResStatisticsV2Mixin],
   data () {
     return {
       listId: 'BackupStorageList',
+      statusNormalList: ['online', 'offline'],
+      statusHiddenList: ['error'],
     }
   },
 }
--- containers/Storage/views/file-system/index.vue
+++ containers/Storage/views/file-system/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <page-header :title="$t('dictionary.filesystem')" :tabs="cloudEnvOptions" :current-tab.sync="cloudEnv" />
+    <page-header
+      :title="$t('dictionary.filesystem')"
+      :tabs="cloudEnvOptions"
+      :current-tab.sync="cloudEnv"
+      isShowResStatusTab
+      :status-opts="statusOpts"
+      :status-click-handle="statusClickHandle" />
     <page-body>
-      <file-system-list :id="listId" :cloud-env="cloudEnv" />
+      <file-system-list :id="listId" :cloud-env="cloudEnv"
+        :cloudEnvOptions="cloudEnvOptions"
+        :filterParams="filterParams"
+        statusResKey="nas"
+        @resStatisticsChange="resStatisticsChange" />
     </page-body>
   </div>
 </template>
 
 <script>
 import { getCloudEnvOptions } from '@/utils/common/hypervisor'
+import ResStatisticsV2Mixin from '@/mixins/resStatisticsV2Mixin'
 import FileSystemList from './components/List'
 
 export default {
@@ -16,11 +27,13 @@ export default {
   components: {
     FileSystemList,
   },
+  mixins: [ResStatisticsV2Mixin],
   data () {
     return {
       listId: 'FileSystemList',
       cloudEnvOptions: getCloudEnvOptions('object_storage_brands'),
       cloudEnv: '',
+      statusNormalList: ['available'],
     }
   },
 }
