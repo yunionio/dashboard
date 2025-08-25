5aa77f6fc6e18114d800c95ac9c92e732152488b
GuoLiBin6 <glbin533@163.com>
2025-08-25 15:31:16 +0800

    feat: 资源增加列表状态统计

--- containers/Storage/views/file-system/index.vue
+++ containers/Storage/views/file-system/index.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <page-header :title="$t('dictionary.filesystem')" />
+    <page-header
+      :title="$t('dictionary.filesystem')"
+      isShowResStatusTab
+      :status-opts="statusOpts"
+      :status-click-handle="statusClickHandle" />
     <page-body>
-      <file-system-list :id="listId" />
+      <file-system-list
+        :id="listId"
+        :filterParams="filterParams"
+        statusResKey="nas"
+        @resStatisticsChange="resStatisticsChange" />
     </page-body>
   </div>
 </template>
 
 <script>
+import ResStatisticsV2Mixin from '@/mixins/resStatisticsV2Mixin'
 import FileSystemList from './components/List'
 
 export default {
@@ -15,9 +24,11 @@ export default {
   components: {
     FileSystemList,
   },
+  mixins: [ResStatisticsV2Mixin],
   data () {
     return {
       listId: 'FileSystemList',
+      statusNormalList: ['available'],
     }
   },
 }
