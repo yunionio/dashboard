0ce569cd1ebcd4b30bb7dfa3ea8552439044e3a7
GuoLiBin6 <glbin533@163.com>
2025-09-26 14:24:15 +0800

    feat: 虚拟机panicked 展示截图

--- src/sections/DialogManager/components/EventLog.vue
+++ src/sections/DialogManager/components/EventLog.vue
@@ -17,7 +17,16 @@
         <a-icon class="primary-color" type="copy" />
         <a-button type="link" size="small">{{$t('common.text00094')}}</a-button>
       </div>
+      <div v-if="screenShotId" class="float-right mr-3">
+        <a-button type="link" size="small" @click="viewScreenshot">{{ $t('common.view_screenshot') }}</a-button>
+      </div>
       <a-button v-if="sessionId" class="float-right mr-3" type="link" size="small" @click="showLog">{{ $t('common.view_scheduler_log') }}</a-button>
+      <div v-if="screenshotUrl" class="screenshot-wrapper mt-5">
+        <img style="width: 100%" :src="screenshotUrl" />
+        <div class="btn-wrapper">
+          <div class="screen-download" @click="downloadScreenshot">{{ $t('common.download_screenshot') }}</div>
+        </div>
+      </div>
     </div>
     <div slot="footer" class="d-flex justify-content-between" v-if="params.currentData">
       <a-button :disabled="prevDisabled" @click="changeCurrentData('prev')"> <a-icon type="left" />{{ $t('common.prev_one') }}</a-button>
@@ -117,6 +126,8 @@ export default {
           },
         },
       ],
+      screenshotUrl: '',
+      canvas: null,
     }
   },
   computed: {
@@ -143,13 +154,82 @@ export default {
     nextDisabled () {
       return this.currentIndex === this.dataList.length - 1
     },
+    screenShotId () {
+      if (this.currentData?.action === 'guest_panicked') {
+        try {
+          const notes = JSON.parse(this.params.data)
+          return notes.screen_dump_name || ''
+        } catch (e) {
+          return ''
+        }
+      }
+      return ''
+    },
   },
   created () {
     if (this.params.handleSelect) {
       this.params.handleSelect(this.currentData)
     }
+    if (this.screenShotId) {
+      this.viewScreenshot()
+    }
   },
   methods: {
+    ppmToPNGDataURL (base64Data) {
+      // 解码 Base64
+      const binaryStr = atob(base64Data)
+      // 解析 PPM 头信息
+      const lines = binaryStr.split('\n')
+      let width = 720
+      let height = 400
+      if (lines.length > 3) {
+        const size = lines[1].split(' ')
+        if (size.length === 2) {
+          width = parseInt(size[0])
+          height = parseInt(size[1])
+        }
+      }
+      // 创建 Canvas
+      const canvas = document.createElement('canvas')
+      canvas.width = width
+      canvas.height = height
+      const ctx = canvas.getContext('2d')
+      const imageData = ctx.createImageData(width, height)
+      // 找到像素数据开始位置（跳过3行头部）
+      let dataStart = 0
+      let lineCount = 0
+      for (let i = 0; i < binaryStr.length; i++) {
+        if (binaryStr[i] === '\n') {
+          lineCount++
+          if (lineCount === 3) {
+            dataStart = i + 1
+            break
+          }
+        }
+      }
+      // 解析 RGB 像素数据
+      const pixelData = new Uint8Array(binaryStr.length - dataStart)
+      for (let i = 0; i < pixelData.length; i++) {
+        pixelData[i] = binaryStr.charCodeAt(dataStart + i)
+      }
+      // 填充到 ImageData
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          const idx = (y * width + x) * 4
+          const pixelIdx = (y * width + x) * 3
+          if (pixelIdx + 2 < pixelData.length) {
+            imageData.data[idx] = pixelData[pixelIdx] // R
+            imageData.data[idx + 1] = pixelData[pixelIdx + 1] // G
+            imageData.data[idx + 2] = pixelData[pixelIdx + 2] // B
+            imageData.data[idx + 3] = 255 // Alpha
+          }
+        }
+      }
+      ctx.putImageData(imageData, 0, 0)
+      this.canvas = canvas
+      // 转换为 PNG Data URL
+      return canvas.toDataURL('image/png')
+    },
     changeCurrentData (type) {
       const index = this.currentIndex + (type === 'prev' ? -1 : 1)
       this.currentData = this.dataList[index]
@@ -168,6 +248,36 @@ export default {
     },
     copyError () {
       this.$message.error(this.$t('common.text00096'))
+    },
+    async viewScreenshot () {
+      try {
+        const res = await new this.$Manager(`servers/${this.currentData.obj_id}/screen-dump-show`, 'v2').list({
+          params: {
+            object_name: this.screenShotId,
+          },
+        })
+        if (res.data?.screen_dump) {
+          this.screenshotUrl = this.ppmToPNGDataURL(res.data.screen_dump.replace('data:application/octet-stream;base64,', ''))
+        } else {
+          this.$message.error(this.$t('common.screenshot_error'))
+        }
+      } catch (e) {
+        this.$message.error(this.$t('common.screenshot_error'))
+        throw e
+      }
+    },
+    downloadScreenshot () {
+      const that = this
+      if (this.canvas) {
+        this.canvas.toBlob(function (blob) {
+          const url = URL.createObjectURL(blob)
+          const a = document.createElement('a')
+          a.href = url
+          a.download = `server-panicked-screenshot-${that.currentData.obj_id}.png`
+          a.click()
+          URL.revokeObjectURL(url)
+        }, 'image/png')
+      }
     },
   },
 }
@@ -190,6 +300,46 @@ export default {
   }
   .value {
     flex: 1 1 auto;
+  }
+}
+.screenshot-wrapper {
+  position: relative;
+  width: 100%;
+  min-height: 200px;
+  .btn-wrapper {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    z-index: 1000;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 12px;
+    .screen-download {
+      padding: 5px 10px;
+      cursor: pointer;
+      background-color: #fff;
+      border-radius: 5px;
+      margin-right: 10px;
+      display: none;
+    }
+    .screen-view {
+      padding: 5px 10px;
+      cursor: pointer;
+      background-color: #fff;
+      border-radius: 5px;
+      display: none;
+    }
+  }
+}
+.screenshot-wrapper:hover {
+  .btn-wrapper {
+    .screen-download {
+      display: block;
+    }
+    .screen-view {
+      display: block;
+    }
   }
 }
 </style>
