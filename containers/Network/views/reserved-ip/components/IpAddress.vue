5064e3a04f5caa0cb06996861ab7f71cf12380a4
GuoLiBin6 <glbin533@163.com>
2025-11-27 15:51:05 +0800

    feat: Cancel the limit on the number of reserved IP addresses

--- containers/Network/views/reserved-ip/components/IpAddress.vue
+++ containers/Network/views/reserved-ip/components/IpAddress.vue
@@ -9,16 +9,16 @@
       </a-form-item>
       <a-button v-if="networkList.length > 1" shape="circle" icon="minus" size="small" @click="decrease(item.key, i)" class="mt-2 ml-1" />
     </div>
-    <div class="d-flex align-items-center" v-if="networkCountRemaining > 0">
+    <div class="d-flex align-items-center">
       <a-button type="primary" shape="circle" icon="plus" size="small" @click="add" />
       <a-button type="link" @click="add">{{$t('network.text_670')}}</a-button>
-      <span class="network-count-tips">{{$t('network.text_169')}}<span class="remain-num">{{ networkCountRemaining }}</span>{{$t('network.text_170')}}</span>
     </div>
   </div>
 </template>
 
 <script>
 import { uuid } from '@/utils/utils'
+import { validate } from '@/utils/validate'
 
 export default {
   name: 'ReservedIPAddress',
@@ -27,30 +27,85 @@ export default {
       type: Object,
       required: true,
     },
+    form: {
+      type: Object,
+      required: true,
+    },
   },
   data () {
     return {
-      limit: 6,
       networkList: [],
     }
-  },
-  computed: {
-    networkCountRemaining () {
-      return this.limit - this.networkList.length
-    },
   },
   created () {
     this.add()
   },
   methods: {
+    getFormInstance () {
+      return this.form || this.$parent?.form?.fc || this.$parent?.$parent?.form?.fc
+    },
     ipChange (e, i) {
       this.networkList[i].ip = e.target.value
-      // this.$emit('change', e.target.value)
+    },
+    ipToInt (ip) {
+      const REG = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
+      const result = REG.exec(ip)
+      if (!result) return -1
+      const ret = (parseInt(result[1]) << 24 |
+        parseInt(result[2]) << 16 |
+        parseInt(result[3]) << 8 |
+        parseInt(result[4])) >>> 0
+      return ret
+    },
+    intToIp (ipInt) {
+      return [
+        (ipInt >>> 24) & 0xFF,
+        (ipInt >>> 16) & 0xFF,
+        (ipInt >>> 8) & 0xFF,
+        ipInt & 0xFF,
+      ].join('.')
+    },
+    getNextIp (ip) {
+      if (!ip) return ''
+      const ipResult = validate(ip, 'IPv4')
+      if (ipResult === false || ipResult.result === false) {
+        return ''
+      }
+      const ipInt = this.ipToInt(ip)
+      if (ipInt === -1) return ''
+      const nextInt = ipInt + 1
+      if (nextInt > 0xFFFFFFFF) return ''
+      return this.intToIp(nextInt)
     },
     add () {
       const uid = uuid()
+      let nextIp = ''
+      if (this.networkList.length > 0) {
+        const lastIndex = this.networkList.length - 1
+        const lastItem = this.networkList[lastIndex]
+        const form = this.getFormInstance()
+        if (form) {
+          const lastFieldName = `networkIps[${lastItem.key}]`
+          const lastFieldValue = form.getFieldValue(lastFieldName)
+          if (lastFieldValue) {
+            nextIp = this.getNextIp(lastFieldValue)
+          }
+        }
+      }
       this.networkList.push({
         key: uid,
+        ip: nextIp,
+      })
+      this.$nextTick(() => {
+        if (nextIp) {
+          const form = this.getFormInstance()
+          if (form) {
+            const fieldName = `networkIps[${uid}]`
+            form.setFieldsValue({
+              [fieldName]: nextIp,
+            })
+          }
+        }
       })
     },
     decrease (uid, index) {
