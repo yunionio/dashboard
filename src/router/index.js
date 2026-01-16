992fcb7edcb2ff6a7588ec0f559349791103ec73
GuoLiBin6 <glbin533@163.com>
2026-01-16 10:47:39 +0800

    feat: document title show company info

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@
  */
 import Vue from 'vue'
 import Router from 'vue-router'
+import { getProductName } from '@/utils/auth'
 import routes from './routes'
 
 const originalPush = Router.prototype.push
@@ -22,6 +23,10 @@ const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+})
+
+router.afterEach((to, from, next) => {
+  document.title = getProductName()
 })
 
 export default router
