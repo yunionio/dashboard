9dbfabfb39cec015f378a87e75e7ac7adce9c5c5
GuoLiBin6 <glbin533@163.com>
2026-03-10 17:17:30 +0800

    feat: Initialization filtering condition issue when modifying alarm strategy

--- containers/Monitor/views/commonalert/components/new-alert/form/index.vue
+++ containers/Monitor/views/commonalert/components/new-alert/form/index.vue
@@ -695,7 +695,7 @@ export default {
       const conditionRef = this.$refs.conditionRef
       conditionRef.reset(false)
       metric_details.forEach((item, idx) => {
-        conditionRef.add()
+        conditionRef.add({ comparator: item.comparator })
         this.$nextTick(() => {
           const conditionList = conditionRef.conditionList
           this.form.fc.setFieldsValue({
