20d8a6691da27403ec3ea25fb2c97649bdef7e13
GuoLiBin6 <glbin533@163.com>
2026-04-13 16:14:19 +0800

    fix: Fix API in filtering

--- src/utils/common/tableFilter.js
+++ src/utils/common/tableFilter.js
@@ -724,7 +724,8 @@ export const getCustomDistinctFieldFilter = ({ label, multiple = true, fetchMeth
     },
     formatter: val => {
       if (multiple) {
-        return `${field}.in(${val.join(',')})`
+        const list = val.map(item => `'${item}'`)
+        return `${field}.in(${list.join(',')})`
       }
       return `${field}.equals('${val}')`
     },
