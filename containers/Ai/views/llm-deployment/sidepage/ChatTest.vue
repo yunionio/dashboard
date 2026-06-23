8005f4259c119a3d4f6837700ff2621179a06d87
GuoLiBin6 <glbin533@163.com>
2026-06-23 15:56:12 +0800

    feat: add llm deployment chat test

---
+++ containers/Ai/views/llm-deployment/sidepage/ChatTest.vue
@@ -0,0 +1,37 @@
+<template>
+  <ai-chat
+    mode="chatTest"
+    :data="data"
+    :res-id="resId"
+    :initial-virtual-key-id="initialVirtualKeyId"
+    :initial-virtual-key="initialVirtualKey" />
+</template>
+
+<script>
+import AiChat from '@Ai/sections/Chat'
+
+export default {
+  name: 'LlmDeploymentChatTest',
+  components: {
+    AiChat,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+    resId: {
+      type: String,
+      required: true,
+    },
+    initialVirtualKeyId: {
+      type: String,
+      default: '',
+    },
+    initialVirtualKey: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
